b739f65fedf2b1242c892fc7bff08e1c74c97a59
Harri Porten <porten@kde.org>
1999-03-30 12:59:23 +0000

    seteuid() replacement for HP/UX
    svn path=/trunk/kdebase/kscreensaver/; revision=18892

--- kscreensaver/main.cpp
+++ kscreensaver/main.cpp
@@ -311,7 +311,11 @@ bool canReadPasswdDatabase()
 int main( int argc, char *argv[] )
 {
 	// drop root privileges temporarily
+#ifdef HAVE_SETEUID
 	seteuid(getuid());
+#else
+        setreuid(-1, getuid())
+#endif // HAVE_SETEUID
 
 	Window saveWin = 0;
 	int timeout = 600;
@@ -404,7 +408,11 @@ int main( int argc, char *argv[] )
 	}
 
 	// regain root privileges
+#ifdef HAVE_SETEUID
 	seteuid(0);
+#else
+        setreuid(-1, 0)
+#endif // HAVE_SETEUID
 	initPasswd();
 	// ... and drop them again before doing anything important
 	setuid(getuid());
