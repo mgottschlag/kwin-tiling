31724dcafc609356b18d9a65507bc13c42806f95
Simon Hausmann <hausmann@kde.org>
2000-01-22 14:14:02 +0000

    - reverting
    svn path=/trunk/kdebase/kcontrol/; revision=38989

--- kcontrol/kcontrol/main.cpp
+++ kcontrol/kcontrol/main.cpp
@@ -1,28 +1,28 @@
 /*
 
   Copyright (c) 1999 Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
-
+ 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-
+ 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
+ 
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-*/
+ 
+*/                                                                            
 
 
 /**
  * Howto debug:
  *    start "kcontrol --nofork" in a debugger.
- *
+ *    
  * If you want to test with command line arguments you need
  * -after you have started kcontrol in the debugger-
  * open another shell and run kcontrol with the desired
@@ -57,9 +57,9 @@ MyApplication::MyApplication()
 {
   if (isRestored())
     RESTORE(TopLevel)
-  else
+  else 
     toplevel = new TopLevel();
-
+  
   setMainWidget(toplevel);
   toplevel->resize(640,400);
 }
@@ -80,21 +80,21 @@ int MyApplication::newInstance()
 
 int main(int argc, char *argv[])
 {
-  KAboutData aboutData( "kcontrol", I18N_NOOP("KDE Control Centre"),
-    "v2.0pre", "The KDE Control Centre", KAboutData::License_GPL,
+  KAboutData aboutData( "kcontrol", I18N_NOOP("KDE Control Centre"), 
+    "v2.0pre", "The KDE Control Centre", KAboutData::GPL, 
     "(c) 1998-2000, The KDE Control Centre Developers");
   aboutData.addAuthor("Matthias Hoelzer-Kluepfel",0, "hoelzer@kde.org");
   KCmdLineArgs::init( argc, argv, &aboutData );
   KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
-
-
+ 
+  
   if (!MyApplication::start())
     exit(0); // Don't do anything if we are already running
-
+  
   MyApplication app; //&aboutData
-  // TODO: MyApplication (a KUniqueApplication) doesn't
+  // TODO: MyApplication (a KUniqueApplication) doesn't 
   // support KApplication::KApplication(KAboutData *)
-  // So, I'm leaving this alone, and will let the
+  // So, I'm leaving this alone, and will let the 
   // maintainter of kcontrol finish it up!
 
   // show the whole stuff
