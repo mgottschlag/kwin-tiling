90e918e9db5a79ed744cf89640a2fde8459d6e8f
Michael Koch <koch@kde.org>
2000-01-23 20:24:09 +0000

    David, imo THIS is the right fix
    svn path=/trunk/kdebase/kcontrol/; revision=39108

---
+++ kcontrol/display/bgdefaults.h
@@ -0,0 +1,31 @@
+/* vi: ts=8 sts=4 sw=4
+ *
+ * $Id$
+ *
+ * This file is part of the KDE project, module kdesktop.
+ * Copyright (C) 1999 Geert Jansen <g.t.jansen@stud.tue.nl>
+ * 
+ * You can Freely distribute this program under the GNU General Public
+ * License. See the file "COPYING" for the exact licensing terms.
+ */
+#ifndef __BGDefaults_h_Included__
+#define __BGDefaults_h_Included__
+
+
+// Globals
+#define _maxDesktops 8
+
+#define _defCommon false
+#define _defDock true
+#define _defExport false
+#define _defLimitCache true
+#define _defCacheSize 2048
+
+// Per desktop defaults
+#define _defColorA  QColor("#4682B4")
+#define _defColorB  QColor("#000000")
+#define _defBackgroundMode Flat
+#define _defWallpaperMode NoWallpaper
+#define _defMultiMode NoMulti
+
+#endif // __BGDefaults_h_Included__
