1dc1a9644ccad777d2a08266a9b8b6ca6460e062
Dawit Alemayehu <adawit@kde.org>
2000-03-04 03:21:12 +0000

    Port of "ShortURI" filter into a KURIFilterPlugin.  Finally. (DA)
    svn path=/trunk/kdebase/kcontrol/; revision=42465

---
+++ kcontrol/ebrowsing/plugins/shorturi/kshorturifilter.cpp
@@ -0,0 +1,252 @@
+/*
+ * kshorturifilter.cpp
+ * Copyright (c) 1999-2000 Dawit Alemayehu <adawit@earthlink.net>
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/stat.h>
+
+#include <qdir.h>
+#include <qlist.h>
+
+#include <kurl.h>
+#include <klocale.h>
+#include <kprotocolmanager.h>
+#include <kinstance.h>
+#include <kglobal.h>
+
+#include "kshorturifilter.h"
+#include "kshorturifilter.moc"
+
+KInstance *KShortURIFilterFactory::s_instance = 0L;
+
+KShortURIFilter::KShortURIFilter( QObject *parent, const char *name )
+                :KURIFilterPlugin( parent, name ? name : "KShortURIFilter", "shorturi", 1.0),
+                 DCOPObject("KShortURIFilterIface")
+{
+    // TODO : Make this configurable.  Should go into control module...
+    m_urlHints.insert("www", "http://");
+    m_urlHints.insert("ftp", "ftp://");
+    m_urlHints.insert("news", "news://");
+    m_pCmdType = KShortURIFilter::Unknown;
+    m_strError = QString::null;
+}
+
+KShortURIFilter::~KShortURIFilter()
+{
+}
+
+bool KShortURIFilter::filterURI( KURL &uri )
+{
+    if( uri.isMalformed() )
+    {
+        debug( "About to filter the URL" );
+        QString url = uri.url();
+        m_pCmdType = parseCmd( url );
+        if( m_pCmdType != KShortURIFilter::Unknown ||
+            m_pCmdType != KShortURIFilter::Error )
+        {
+            debug( "URL Filtered to : %s", url.latin1() );
+            uri = url;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool KShortURIFilter::isExecutable( const QString &name ) const
+{
+    QStringList path = QStringList::split( ':', getenv("PATH") );
+    for( QStringList::ConstIterator it = path.begin(); it != path.end(); it++ )
+    {
+        if( access(*it + '/' + name, X_OK) == 0 )
+            return true;
+    }
+    return false;
+}
+
+bool KShortURIFilter::isValidShortURL( const QString& cmd ) const
+{
+  // NOTE : By design, this check disqualifies some valid
+  // URL's that contain queries and *nix command characters.
+  // This is an intentional trade off to best match the URL
+  // with a local resource first.  This also allows KShortURIFilter
+  // to behave consistently with the way it does now. (Dawit A.)
+  // Some tests removed since this is now called only if the start of cmd qualifies for a host name (malte)
+
+  // TODO: configurability like always treat foobar/XXX as a shortURL
+  if ( QRegExp( "[ ;<>]" ).match( cmd ) >= 0
+    || QRegExp( "||" ).match( cmd ) >= 0
+    || QRegExp( "&&" ).match( cmd ) >= 0
+    || cmd.at( cmd.length()-1 ) == '&' ) return false;
+
+  return true;
+}
+
+KShortURIFilter::CmdType KShortURIFilter::parseCmd( QString& cmd )
+{
+    // First look if we've got a known protocol prefix
+    // and assume a qualified URL
+    QStringList protocols = KProtocolManager::self().protocols();
+    for( QStringList::ConstIterator it = protocols.begin(); it != protocols.end(); it++ )
+    {
+        if( cmd.left((*it).length()).lower() == *it )
+            return KShortURIFilter::URL;
+    }
+
+    // See if the beginning of cmd looks like a valid FQDN
+    QString host;
+    if( QRegExp("[a-z][a-z0-9-]*\\.[a-z]", false).match(cmd) == 0 )
+    {
+        host = cmd.left(cmd.find('.'));
+        // Check if it's one of the urlHints and qualify the URL
+        QString proto = m_urlHints[host.lower()];
+        if( !proto.isEmpty() )
+        {
+            cmd.insert(0, proto);
+            return KShortURIFilter::URL;
+        }
+    }
+
+    // Assume http if cmd starts with <IP>/
+    // Will QRegExp support ([0-9]{1,3}\.){3}[0-9]{1,3} some time?
+    if ( QRegExp("[0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?/").match(cmd) == 0 )
+    {
+        cmd.insert(0, "http://");
+        return KShortURIFilter::URL;
+    }
+
+    // Is it to be invoked with kdehelp? btw: how to invoke the man/info browser in KDE 2?
+    if( cmd.left(5) == "info:" || cmd.left(4) == "man:" || cmd[0] == '#' )
+    {
+        if( cmd.left(2) == "##" )
+            cmd = "info:" + ( cmd.length() == 2 ? QString("(dir)" ) : cmd.mid(2));
+        else if ( cmd[0] == '#' )
+            cmd = "man" + (cmd.length() == 1 ? QString("(index)") : cmd.mid(1));
+        return KShortURIFilter::Help;
+    }
+
+    // Local file and directory processing.
+    // TODO: also detect executables when arguments are given
+    QString uri = QDir::cleanDirPath( cmd );
+
+    // Strip off "file:/" in order to expand local
+    // URLs if necessary.
+    if( uri.lower().find("file:/") == 0 )
+        uri.remove ( 0, uri[6] == '/' ? 6 : 5 );
+
+    // HOME directory ?
+    if( uri[0] == '~' )
+    {
+        int len = uri.find('/');
+        if( len == -1 )
+            len = uri.length();
+        if( len == 1 )
+            uri.replace ( 0, 1, QDir::homeDirPath() );
+        else
+        {
+            QString user = uri.mid( 1, len-1 );
+            struct passwd *dir = getpwnam(user.latin1());
+            if( dir && strlen(dir->pw_dir) )
+                uri.replace (0, len, dir->pw_dir);
+            else
+            {
+                m_strError = dir ? i18n("%1 doesn't have a home directory!").arg(user) : i18n("There is no user called %1.").arg(user);
+                return KShortURIFilter::Error;
+            }
+        }
+    }
+
+    // Now for any local resource
+    struct stat buff;
+
+    // Determine if "uri" is an absolute path to a local resource
+    if( (uri[0] == '/') && ( stat( uri.latin1() , &buff ) == 0 ) )
+    {
+        if( !S_ISDIR(buff.st_mode) && access (uri.latin1(), X_OK) == 0 )
+        {
+            cmd = uri;
+            return KShortURIFilter::Executable;
+        }
+        // Open "uri" as file:/xxx if it is a non-executable local resource.
+        if( S_ISDIR( buff.st_mode ) || S_ISREG( buff.st_mode ) )
+        {
+            cmd = "file:" + uri;
+            return KShortURIFilter::URL;
+        }
+    }
+
+    // If "uri" is not the absolute path to a file or a directory,
+    // see if it is executable under the user's $PATH variable.
+    if( isExecutable ( uri ) )
+    {
+        cmd = uri;
+        return KShortURIFilter::Executable;
+    }
+
+    // If cmd is NOT a local resource check for a  valid "shortURL"
+    // candidate, append "http://" as the default protocol.
+    // FIXME: Make this option configurable !! (Dawit A.)
+    if( (!host.isEmpty() && isValidShortURL ( cmd )) || cmd == "localhost" )
+    {
+        cmd.insert(0, "http://");
+        return KShortURIFilter::URL;
+    }
+    return KShortURIFilter::Shell;
+}
+
+KCModule *KShortURIFilter::configModule(QWidget *parent, const char *name) const
+{
+    return 0; // TODO : Pending control-panel module.
+}
+
+QString KShortURIFilter::configName() const
+{
+    return i18n("&Short URI");
+}
+
+bool KShortURIFilter::process(const QCString &fun, const QByteArray &data, QCString& replyType, QByteArray &replyData)
+{
+	if ( fun == "configure()" )
+	{
+		replyType = "void";
+		configure( );
+		return TRUE;
+	}
+	if ( DCOPObject::process( fun, data, replyType, replyData ) )
+		return TRUE;
+
+	return FALSE;
+}
+
+KShortURIFilterFactory::KShortURIFilterFactory( QObject *parent, const char *name )
+                       :KLibFactory( parent, name )
+{
+    s_instance = new KInstance( "kshorturifilter" );
+}
+
+KShortURIFilterFactory::~KShortURIFilterFactory()
+{
+    delete s_instance;
+}
+
+QObject *KShortURIFilterFactory::create( QObject *parent, const char *, const char*, const QStringList & )
+{
+    return new KShortURIFilter( parent );
+}
+
+KInstance *KShortURIFilterFactory::instance()
+{
+    return s_instance;
+}
+
+extern "C"
+{
+    void *init_libkshorturifilter()
+    {
+        return new KShortURIFilterFactory;
+    }
+}
+
---
+++ kcontrol/ebrowsing/plugins/shorturi/kshorturifilter.h
@@ -0,0 +1,99 @@
+/*
+    kshorturifilter.h
+
+    This file is part of the KDE project
+    Copyright (C) 2000 Yves Arrouye <yves@realnames.com>
+    Copyright (C) 2000 Dawit Alemayehu <adawit@earthlink.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef _KSHORTURIFILTER_H_
+#define _KSHORTURIFILTER_H_
+
+#include <qmap.h>
+
+#include <dcopobject.h>
+#include <klibloader.h>
+#include <kurifilterplugin.h>
+
+class KInstance;
+
+class KShortURIFilter : public KURIFilterPlugin , public DCOPObject
+{
+    K_DCOP
+
+public:
+    enum CmdType { URL, Executable, Shell, Help, Error, Unknown };
+
+    KShortURIFilter( QObject *parent = 0, const char *name = 0 );
+    virtual ~KShortURIFilter();
+
+    virtual bool filterURI( KURL &uri );
+    CmdType type() const { return m_pCmdType; }
+    QString errorMessage() const { return m_strError; }
+
+    virtual KCModule *configModule( QWidget *parent = 0, const char *name = 0 ) const;
+    virtual QString configName() const;
+
+public:
+k_dcop:
+    virtual void configure() { return; }
+
+protected:
+    /**
+     *  Function determines whether what the user entered in the
+     *  KShortURIFilter box is executable or not.
+     */
+    bool isExecutable ( const QString& ) const;
+
+    bool isValidShortURL ( const QString& ) const;
+
+    /**
+     * Parses cmd to determine its type
+     * Possible return values are:
+     * URL - cmd is a URL
+     * Executable - cmd is a local executable with no parameters (for now)
+     * Shell - anything else should be run through a shell
+     * Help - should be passed to kdehelp
+     * Error - cmd is invald (for now only if it's ~username and no home dir could be found)
+     * in case of an error, cmd is set to the appropriate message, otherwise it's qualified
+     */
+    KShortURIFilter::CmdType parseCmd( QString& );
+
+private:
+    KShortURIFilter::CmdType m_pCmdType;
+    QMap<QString, QString> m_urlHints;
+    QString m_strError;
+};
+
+
+class KShortURIFilterFactory : public KLibFactory
+{
+    Q_OBJECT
+
+public:
+    KShortURIFilterFactory( QObject *parent = 0, const char *name = 0 );
+    ~KShortURIFilterFactory();
+
+    virtual QObject *create( QObject *parent = 0, const char *name = 0, const char* classname = "QObject", const QStringList &args = QStringList() );
+
+    static KInstance *instance();
+
+private:
+    static KInstance *s_instance;
+
+};
+#endif
