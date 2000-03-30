7daeeb1449160c4d5674e677e774f31bc2f8ea17
Dima Rogozin <dima@mercury.co.il>
2000-03-30 02:30:04 +0000

    First version of clock kcontrol module
    svn path=/trunk/kdebase/kcontrol/; revision=45117

---
+++ kcontrol/clock/main.cpp
@@ -0,0 +1,88 @@
+/*
+ *  main.cpp
+ *
+ *  Copyright (C) 1998 Luca Montecchiani <m.luca@usa.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <qtabwidget.h>
+#include <qlayout.h>
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kcmodule.h>
+
+#include "main.h"
+#include "main.moc"
+
+#include "tzone.h"
+#include "dtime.h"
+
+#include <stdio.h>
+
+KclockModule::KclockModule(QWidget *parent, const char *name)
+  : KCModule(parent, name)
+{ 
+  QVBoxLayout *layout = new QVBoxLayout(this);
+  tab = new QTabWidget(this);
+  layout->addWidget(tab);
+  
+  dtime = new Dtime(this);
+  tab->addTab(dtime, i18n("Date && Time"));
+  connect(dtime, SIGNAL(changed(bool)), this, SLOT(moduleChanged(bool)));
+
+  tzone = new Tzone(this);
+  tab->addTab(tzone, i18n("Time Zone"));
+  connect(tzone, SIGNAL(changed(bool)), this, SLOT(moduleChanged(bool)));
+  
+  setButtons(Help|Reset|Cancel|Apply|Ok);
+}
+
+void KclockModule::save()
+{
+  dtime->save();
+  tzone->save();
+}
+
+void KclockModule::load()
+{
+  dtime->load();
+  tzone->load();
+}
+
+QString KclockModule::quickHelp()
+{
+  return i18n("FIXME: add some help");
+}
+
+void KclockModule::moduleChanged(bool state)
+{
+  emit changed(state);
+}
+
+extern "C"
+{
+  
+  KCModule *create_clock(QWidget *parent, const char *name) 
+  { 
+    KGlobal::locale()->insertCatalogue("kcmkclock");
+    return new KclockModule(parent, name);
+  }
+
+}
+
+
---
+++ kcontrol/clock/tzone.cpp
@@ -0,0 +1,114 @@
+/*
+ *  tzone.cpp
+ *
+ *  Copyright (C) 1998 Luca Montecchiani <m.luca@usa.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>    
+
+#include <qlabel.h>
+#include <qmsgbox.h>
+#include <qcombo.h>
+#include <qpixmap.h> 
+#include <qlayout.h>
+
+#include <kconfig.h>
+#include <kglobal.h>
+#include <klocale.h>
+#include <kcmodule.h>
+
+#include "xpm/world.xpm"
+#include "tzone.h"
+#include "tzone.moc"
+
+Tzone::Tzone(QWidget * parent, const char *name)
+  : KCModule (parent, name)
+{
+  // *************************************************************
+  // Start Dialog
+  // *************************************************************
+  
+  QFrame* frame1 = new QFrame( this );
+  frame1->setFrameStyle( QFrame::Sunken | QFrame::Box );
+  
+  QVBoxLayout *v1 = new QVBoxLayout( frame1, 10 );
+  
+  tzonelist = new QComboBox( FALSE, frame1, "ComboBox_1" );
+  connect( tzonelist, SIGNAL(activated(int)), SLOT(zone_changed()) );
+  tzonelist->setAutoResize( FALSE );
+  v1->addWidget( tzonelist );
+  
+  QLabel* worldMap = new QLabel( frame1, "WorldMap" );
+  worldMap->setAlignment( QLabel::AlignCenter );
+  v1->addWidget( worldMap );
+  
+  QHBoxLayout *top = new QHBoxLayout( this, 5 );
+  top->addWidget(frame1, 1);
+  
+  // *************************************************************
+  // End Dialog
+  // *************************************************************
+  
+  fillTimeZones();
+  
+  QPixmap pm( world );
+  worldMap->setPixmap(pm);
+  
+  load();
+}
+
+void Tzone::fillTimeZones()
+{
+  // Needs to be improved (Add zone names)
+  for ( int i=-14 ; i <= 12 ; i++ )
+    {
+      QString s;
+      s.sprintf( "GMT%+d", i);
+      tzonelist->insertItem( s );
+    }
+}
+
+void Tzone::load() 
+{
+  KConfig *config = KGlobal::config();
+  config->setGroup("tzone");
+  pos = config->readNumEntry("TZ", 14);  
+  tzonelist->setCurrentItem(pos);
+}
+
+void Tzone::save()
+{
+  char tz[40];
+  
+  sprintf(tz, "/usr/share/zoneinfo/Etc/%s", (tzonelist->currentText()).data());
+   
+  debug("Set time zone %s", tz);
+  
+  // This is extremely ugly. Who knows the better way?
+  unlink( "/etc/localtime" );
+  symlink( tz, "/etc/localtime" );
+  
+  // write some stuff 
+  KConfig *config = KGlobal::config();
+  config->setGroup("tzone");
+  config->writeEntry("TZ", tzonelist->currentItem() );
+  config->sync();
+}
+
