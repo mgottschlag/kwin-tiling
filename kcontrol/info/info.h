ae5f0be79521b4874aa7408fd0db1a56181037ff
Dirk Mueller <mueller@kde.org>
2000-06-30 00:52:06 +0000

    use Q_OBJECT for meta-unloading
    svn path=/trunk/kdebase/kcontrol/; revision=54924

--- kcontrol/info/info.h
+++ kcontrol/info/info.h
@@ -36,6 +36,8 @@ bool GetInfo_XServer_and_Video( QListView *lBox );
 
 class KInfoListWidget : public KCModule
 {
+  Q_OBJECT
+
 public:
 
   KInfoListWidget(QWidget *parent, const char *name=0, bool _getlistbox (QListView *)=0);
