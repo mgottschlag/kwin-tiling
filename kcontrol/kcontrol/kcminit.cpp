456d54c759c536f8eeca6027d1c4ea2b90541d51
Stephan Kulow <coolo@kde.org>
2000-09-24 12:31:43 +0000

    use the same code in kcminit than in kcmshell to only init one kcontrol module than all at once
    svn path=/trunk/kdebase/kcontrol/; revision=64998

--- kcontrol/kcontrol/kcminit.cpp
+++ kcontrol/kcontrol/kcminit.cpp
@@ -28,7 +28,14 @@
 #include <klibloader.h>
 #include <kdebug.h>
 #include <dcopclient.h>
+#include "global.h"
+#include <iostream.h>
 
+static KCmdLineOptions options[] =
+{
+    { "+module", I18N_NOOP("Configuration module to open."), 0 },
+    KCmdLineLastOption
+};
 
 int main(int argc, char *argv[])
 {
@@ -38,46 +45,79 @@ int main(int argc, char *argv[])
 	I18N_NOOP("KCMInit - runs startups initialisation for Control Modules."));
 
   KCmdLineArgs::init(argc, argv, &aboutData);
+  KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
 
   KApplication app;
   KLocale::setMainCatalogue(0);
 
-  // locate the desktop files
-  KService::List list = KService::allInitServices();
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+  QCString arg;
+  if (args->count() == 1) {
+    arg = args->arg(0);
+  }
 
   // get the library loader instance
   KLibLoader *loader = KLibLoader::self();
+
+  KService::List list;
+
+  if (!arg.isEmpty()) {
+
+    QString path = KCGlobal::baseGroup();
+    path += arg;
+    path += ".desktop";
+
+    KService::Ptr serv = KService::serviceByDesktopPath( path );
+    if (!serv)
+    {
+        // Path didn't work. Trying as a name
+        serv = KService::serviceByDesktopName( arg );
+    }
+
+    if ( !serv || serv->library().isEmpty() || 
+	 serv->init().isEmpty()) {
+      cerr << i18n("Module %1 not found!").arg(arg).local8Bit() << endl;
+      return -1;
+    } else
+      list.append(serv);
+
+  } else {
+
+    // locate the desktop files
+    list = KService::allInitServices();
+
+  }
 
   // look for X-KDE-Init=... entries
   for(KService::List::Iterator it = list.begin();
       it != list.end();
       ++it)
-  {
-     KService::Ptr service = (*it);
-     if (service->library().isEmpty() || service->init().isEmpty())
-        continue; // Skip
-     // try to load the library
-     QString libName = QString("libkcm_%1").arg(service->library());
-     KLibrary *lib = loader->library(QFile::encodeName(libName));
-     if (lib)
-     {
-        // get the init_ function
-	QString factory = QString("init_%1").arg(service->init());
-	void *init = lib->symbol(factory.utf8());
-        if (init)
+    {
+      KService::Ptr service = (*it);
+      if (service->library().isEmpty() || service->init().isEmpty())
+	continue; // Skip
+      // try to load the library
+      QString libName = QString("libkcm_%1").arg(service->library());
+      KLibrary *lib = loader->library(QFile::encodeName(libName));
+      if (lib)
 	{
-	   // initialize the module
-	   kdDebug() << "Initializing " << libName << ": " << factory << endl;
-
-	   void (*func)() = (void(*)())init;
-	   func();
+	  // get the init_ function
+	  QString factory = QString("init_%1").arg(service->init());
+	  void *init = lib->symbol(factory.utf8());
+	  if (init)
+	    {
+	      // initialize the module
+	      kdDebug() << "Initializing " << libName << ": " << factory << endl;
+	      
+	      void (*func)() = (void(*)())init;
+	      func();
+	    }
+	  loader->unloadLibrary(QFile::encodeName(libName));
 	}
-        loader->unloadLibrary(QFile::encodeName(libName));
-     }
-  }
+    }
   
   if ( !kapp->dcopClient()->isAttached() )
-      kapp->dcopClient()->attach();
+    kapp->dcopClient()->attach();
   kapp->dcopClient()->send( "ksplash", "", "upAndRunning(QString)",  QString("kcminit"));
 
   return 0;
