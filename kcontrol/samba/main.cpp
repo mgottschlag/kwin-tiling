eeeab47ffd250a8667de8ac438aff29163ca6cb3
Alexander Neundorf <neundorf@kde.org>
2000-10-31 22:25:38 +0000

    I think the lines "Qt, available at no cost" are not the best
    Alex
    
    P.S. checking out the sources fresh again did help :-)
    
    svn path=/trunk/kdebase/kcontrol/; revision=69529

--- kcontrol/samba/main.cpp
+++ kcontrol/samba/main.cpp
@@ -1,9 +1,6 @@
 /*
  * main.cpp for the samba kcontrol module
  *
- * Requires the Qt widget libraries, available at no cost at
- * http://www.troll.no/
- *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
