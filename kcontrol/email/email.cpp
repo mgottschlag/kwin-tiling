cdc676efd9f61e08cbae5e10911751ef55d8f515
Alex Zepeda <zipzippy@sonic.net>
2001-04-12 23:33:35 +0000

    This isn't really a GPL'd file.  Thanks.
    svn path=/trunk/kdebase/kcontrol/; revision=91760

--- kcontrol/email/email.cpp
+++ kcontrol/email/email.cpp
@@ -5,8 +5,28 @@
  * This file is part of the KDE project, module kcontrol.
  * Copyright (C) 1999-2001 by Alex Zepeda and Daniel Molkentin
  *
- * You can freely distribute this program under the GNU General Public
- * License. See the file "COPYING" for the exact licensing terms.
+ * You can freely distribute this program under the following terms:
+ *
+ * 1.) If you use this program in a product outside of KDE, you must 
+ * credit the authors, and must mention "kcmemail".
+ *
+ * 2.) If this program is used in a product other than KDE, this license 
+ * must be reproduced in its entirety in the documentation and/or some 
+ * other highly visible location (e.g. startup splash screen)
+ * 
+ * 3.) Use of this product implies the following terms:
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  *
  */
 
