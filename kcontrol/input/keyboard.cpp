8383a0784070198c5587c535fd66f3ad00521868
Luboš Luňák <l.lunak@kde.org>
2001-04-30 21:08:33 +0000

    Upgrade to the latest and greatest ( and the last ) NumLockX 1.0 .
    svn path=/trunk/kdebase/kcontrol/; revision=94560

--- kcontrol/input/keyboard.cpp
+++ kcontrol/input/keyboard.cpp
@@ -48,11 +48,6 @@
 #include "keyboard.h"
 #include <X11/Xlib.h>
 
-#ifdef HAVE_XTEST
-#include <X11/extensions/XTest.h>
-#include <X11/keysym.h>
-#endif
-
 KeyboardConfig::KeyboardConfig (QWidget * parent, const char *name)
     : KCModule (parent, name)
 {
@@ -97,7 +92,7 @@ KeyboardConfig::KeyboardConfig (QWidget * parent, const char *name)
   ( void ) new QRadioButton( i18n( "Leave unchanged" ), numlockGroup );
   connect(numlockGroup, SIGNAL(released(int)), this, SLOT(changed()));
   lay->addWidget( numlockGroup );
-#ifndef HAVE_XTEST
+#if !defined(HAVE_XTEST) && !defined(HAVE_XKB)
   numlockGroup->setDisabled( true );
 #endif
   wtstr = i18n("If supported, this option allows you to setup"
@@ -212,18 +207,124 @@ void KeyboardConfig::changed()
 
 /*
  Originally comes from NumLockX http://dforce.sh.cvut.cz/~seli/en/numlockx
- Copyright (C) 2000 Lubos Lunak <l.lunak@email.cz>
-*/
+
+ NumLockX
+ 
+ Copyright (C) 2000-2001 Lubos Lunak        <l.lunak@kde.org>
+ Copyright (C) 2001      Oswald Buddenhagen <ossi@kde.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+****************************************************************************/
+
+#include <X11/Xlib.h>
 
 #ifdef HAVE_XTEST
-int numlockx_get_numlock_state()
+#include <X11/extensions/XTest.h>
+#endif
+
+#ifdef HAVE_XKB
+#include <X11/XKBlib.h>
+#endif
+
+#include <X11/keysym.h>
+
+#if defined(HAVE_XTEST) || defined(HAVE_XKB)
+
+/* the XKB stuff is based on code created by Oswald Buddenhagen <ossi@kde.org> */
+#ifdef HAVE_XKB
+int xkb_init()
+    {
+    int xkb_opcode, xkb_event, xkb_error;
+    int xkb_lmaj = XkbMajorVersion;
+    int xkb_lmin = XkbMinorVersion;
+    return XkbLibraryVersion( &xkb_lmaj, &xkb_lmin )
+        && XkbQueryExtension( qt_xdisplay(), &xkb_opcode, &xkb_event, &xkb_error,
+			       &xkb_lmaj, &xkb_lmin );
+    }
+    
+unsigned int xkb_mask_modifier( XkbDescPtr xkb, const char *name )
+    {
+    int i;
+    if( !xkb || !xkb->names )
+	return 0;
+    for( i = 0;
+         i < XkbNumVirtualMods;
+	 i++ )
+	{
+	char* modStr = XGetAtomName( xkb->dpy, xkb->names->vmods[i] );
+	if( modStr != NULL && strcmp(name, modStr) == 0 )
+	    {
+	    unsigned int mask;
+	    XkbVirtualModsToReal( xkb, 1 << i, &mask );
+	    return mask;
+	    }
+	}
+    return 0;
+    }
+
+unsigned int xkb_numlock_mask()
+    {
+    XkbDescPtr xkb;
+    if(( xkb = XkbGetKeyboard( qt_xdisplay(), XkbAllComponentsMask, XkbUseCoreKbd )) != NULL )
+	{
+        unsigned int mask = xkb_mask_modifier( xkb, "NumLock" );
+        XkbFreeKeyboard( xkb, 0, True );
+        return mask;
+        }
+    return 0;
+    }
+        
+int xkb_set_on()
+    {
+    unsigned int mask;
+    if( !xkb_init())
+        return 0;
+    mask = xkb_numlock_mask();
+    if( mask == 0 )
+        return 0;
+    XkbLockModifiers ( qt_xdisplay(), XkbUseCoreKbd, mask, mask);
+    return 1;
+    }
+    
+int xkb_set_off()
+    {
+    unsigned int mask;
+    if( !xkb_init())
+        return 0;
+    mask = xkb_numlock_mask();
+    if( mask == 0 )
+        return 0;
+    XkbLockModifiers ( qt_xdisplay(), XkbUseCoreKbd, mask, 0);
+    return 1;
+    }
+#endif
+
+#ifdef HAVE_XTEST
+int xtest_get_numlock_state()
     {
     int i;
     int numlock_mask = 0;
     Window dummy1, dummy2;
     int dummy3, dummy4, dummy5, dummy6;
     unsigned int mask;
-    XModifierKeymap* map = XGetModifierMapping( qt_xdisplay());
+    XModifierKeymap* map = XGetModifierMapping( qt_xdisplay() );
     KeyCode numlock_keycode = XKeysymToKeycode( qt_xdisplay(), XK_Num_Lock );
     if( numlock_keycode == NoSymbol )
         return 0;
@@ -234,41 +335,62 @@ int numlockx_get_numlock_state()
 	if( map->modifiermap[ map->max_keypermod * i ] == numlock_keycode )
 		numlock_mask = 1 << i;
 	}
-    XQueryPointer( qt_xdisplay(), DefaultRootWindow( qt_xdisplay()), &dummy1, &dummy2,
+    XQueryPointer( qt_xdisplay(), DefaultRootWindow( qt_xdisplay() ), &dummy1, &dummy2,
         &dummy3, &dummy4, &dummy5, &dummy6, &mask );
     XFreeModifiermap( map );
     return mask & numlock_mask;
     }
 
-void numlockx_change_numlock()
+void xtest_change_numlock()
     {
-    XTestFakeKeyEvent( qt_xdisplay(), XKeysymToKeycode( qt_xdisplay(), XK_Num_Lock ), True,
-        CurrentTime );
-    XTestFakeKeyEvent( qt_xdisplay(), XKeysymToKeycode( qt_xdisplay(), XK_Num_Lock ), False,
-        CurrentTime );
+    XTestFakeKeyEvent( qt_xdisplay(), XKeysymToKeycode( qt_xdisplay(), XK_Num_Lock ), True, CurrentTime );
+    XTestFakeKeyEvent( qt_xdisplay(), XKeysymToKeycode( qt_xdisplay(), XK_Num_Lock ), False, CurrentTime );
     }
 
-void numlockx_set_on()
+void xtest_set_on()
     {
-    if( !numlockx_get_numlock_state())
-        numlockx_change_numlock();
+    if( !xtest_get_numlock_state())
+        xtest_change_numlock();
     }
 
-void numlockx_set_off()
+void xtest_set_off()
     {
-    if( numlockx_get_numlock_state())
-        numlockx_change_numlock();
+    if( xtest_get_numlock_state())
+        xtest_change_numlock();
+    }
+#endif
+
+void numlock_set_on()
+    {
+#ifdef HAVE_XKB
+    if( xkb_set_on())
+        return;
+#endif
+#ifdef HAVE_XTEST
+    xtest_set_on();
+#endif
+    }
+
+void numlock_set_off()
+    {
+#ifdef HAVE_XKB
+    if( xkb_set_off())
+        return;
+#endif
+#ifdef HAVE_XTEST
+    xtest_set_off();
+#endif
     }
 
 void numlockx_change_numlock_state( bool set_P )
     {
     if( set_P )
-        numlockx_set_on();
+        numlock_set_on();
     else
-        numlockx_set_off();
+        numlock_set_off();
     }
 #else
 void numlockx_change_numlock_state( bool ) {} // dummy
-#endif
+#endif // defined(HAVE_XTEST) || defined(HAVE_XKB)
 
 #include "keyboard.moc"
