a7467e557d0a302bcb801f0118e61ad292697c01
Andrea Rizzi <rizzi@kde.org>
2001-05-23 12:11:12 +0000

    Added ui files, cpp and h files removed as Ralf asked. It compile here. Copyright notice added to files.
    svn path=/trunk/kdebase/kcontrol/; revision=98394

--- kcontrol/konsole/kcmkonsole.h
+++ kcontrol/konsole/kcmkonsole.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          kcmkonsole.h 
+                             -------------------
+    begin                : mar apr 17 16:44:59 CEST 2001
+    copyright            : (C) 2001 by Andrea Rizzi
+    email                : rizzi@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef KCMKONSOLE_H
 #define KCMKONSOLE_H
