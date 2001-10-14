6eb9aa0028ddce77237844326fdd751d22a647db
Gregory Mokhin <mokhin@bog.msu.ru>
2001-10-14 10:42:46 +0000

    message fix
    svn path=/trunk/kdebase/kcontrol/; revision=117863

--- kcontrol/kfontinst/kfontinst/InstalledFontListWidget.cpp
+++ kcontrol/kfontinst/kfontinst/InstalledFontListWidget.cpp
@@ -60,12 +60,12 @@ CInstalledFontListWidget::CInstalledFontListWidget(QWidget *parent, const char *
 
     setCfgButtonState(CKfiGlobal::cfg().getModifiedDirs().count()>0 || CKfiGlobal::cfg().firstTime() ? true : false);
 
-    QWhatsThis::add(itsButton2, i18n("Installing and uninstalling the fonts only"
-                                     "copies them to / removes them from the X"
-                                     "fonts folder. In order to make X, Ghostscript,"
-                                     "and StarOffice aware of the changes you"
-                                     "must \"configure\" your system - this will"
-                                     "create the neccessary configuration files"
+    QWhatsThis::add(itsButton2, i18n("Installing and uninstalling the fonts only "
+                                     "copies them to / removes them from the X "
+                                     "fonts folder. In order to make X, Ghostscript, "
+                                     "and StarOffice aware of the changes you "
+                                     "must \"configure\" your system - this will "
+                                     "create the neccessary configuration files "
                                      "needed by the various programs."));
 
     itsFontsPopup=new QPopupMenu(this);
