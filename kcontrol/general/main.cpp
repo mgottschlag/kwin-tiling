c3da3ae6204cf8c380e1368b0fdb22608b86daff
Daniel Molkentin <molkentin@kde.org>
2001-12-09 11:24:34 +0000

    this commit belongs to the modifications in Makefile.am
    svn path=/trunk/kdebase/kcontrol/; revision=126239

--- kcontrol/general/main.cpp
+++ kcontrol/general/main.cpp
@@ -3,7 +3,7 @@
 #include "widgetsettings.h"
 
 extern "C" {
-    KCModule *create_general(QWidget *parent, const char *name) {
+    KCModule *create_widgetsettings(QWidget *parent, const char *name) {
       KGlobal::locale()->insertCatalogue(QString::fromLatin1("kcmwidgetsettings"));
       return new KWidgetSettingsModule(parent, name);
     }
