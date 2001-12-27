221633bd755eddb63c40c0b9cbda36faa8ee0f5b
Stephan Binner <binner@kde.org>
2001-12-27 10:52:58 +0000

    s/kcmwidgetsetting/kcmwidgetsettings/
    svn path=/trunk/kdebase/kcontrol/; revision=128760

--- kcontrol/general/widgetsettings.ui.h
+++ kcontrol/general/widgetsettings.ui.h
@@ -180,7 +180,7 @@ void  KWidgetSettingsModule::save()
 const KAboutData* KWidgetSettingsModule::aboutData() const
 {
 	KAboutData *about =
-	new KAboutData(I18N_NOOP("kcmwidgetsetting"), I18N_NOOP("KDE Widgetsettings Module"),  0, 0, KAboutData::License_GPL,
+	new KAboutData(I18N_NOOP("kcmwidgetsettings"), I18N_NOOP("KDE Widgetsettings Module"),  0, 0, KAboutData::License_GPL,
 						I18N_NOOP("(c) 2001 Daniel Molkentin"));
 
 	about->addAuthor("Daniel Molkentin", 0, "molkentin@kde.org");
