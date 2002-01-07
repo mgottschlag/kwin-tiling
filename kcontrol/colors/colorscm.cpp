03048241b6cd486cbf19b10b804f8af6dcf69c39
Lukáš Tinkl <lukas@kde.org>
2002-01-07 10:37:02 +0000

    ummm, much better :-)
    Danimo: I'll fix the rest as well
    
    svn path=/trunk/kdebase/kcontrol/; revision=130338

--- kcontrol/colors/colorscm.cpp
+++ kcontrol/colors/colorscm.cpp
@@ -50,7 +50,7 @@
 
 /**** DLL Interface ****/
 typedef KGenericFactory<KColorScheme , QWidget> KolorFactory;
-K_EXPORT_COMPONENT_FACTORY( libkcm_colors, KolorFactory );
+K_EXPORT_COMPONENT_FACTORY( libkcm_colors, KolorFactory("kcmcolors") );
 
 /*
 extern "C" {
