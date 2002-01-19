92b6f03b07439712990b3a22791ddd799f813bdd
Ellis Whitehead <kde@ellisw.net>
2002-01-19 04:42:35 +0000

    insertAction() => insert()
    svn path=/trunk/kdebase/kwin/; revision=132460

--- klipper/klipperbindings.cpp
+++ klipper/klipperbindings.cpp
@@ -1,12 +1,12 @@
 #ifndef NOSLOTS
 # define DEF( name, key3, key4, fnSlot ) \
-   keys->insertAction( name, i18n(name), QString::null, key3, key4, this, SLOT(fnSlot) )
+   keys->insert( name, i18n(name), QString::null, key3, key4, this, SLOT(fnSlot) )
 #else
 # define DEF( name, key3, key4, fnSlot ) \
-   keys->insertAction( name, i18n(name), QString::null, key3, key4 )
+   keys->insert( name, i18n(name), QString::null, key3, key4 )
 #endif
 
-	keys->insertLabel( "Program:klipper", i18n("Clipboard") );
+	keys->insert( "Program:klipper", i18n("Clipboard") );
 
 	DEF( I18N_NOOP("Show Klipper Popup-Menu"), "Alt+Ctrl+V", "Meta+Ctrl+V", slotPopupMenu() );
 	DEF( I18N_NOOP("Manually Invoke Action on Current Clipboard"), "Alt+Ctrl+R", "Meta+Ctrl+R", slotRepeatAction() );
