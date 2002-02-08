b325c97145da5567bfc14e70a38cfd929d7c0d4b
Dirk Mueller <mueller@kde.org>
2002-02-08 13:49:55 +0000

    IRIX compile fixlets
    svn path=/trunk/kdebase/kcontrol/; revision=135399

--- kcontrol/info/memory_sgi.cpp
+++ kcontrol/info/memory_sgi.cpp
@@ -27,7 +27,8 @@ void KMemoryWidget::update()
   Memory_Info[FREE_MEM]     = MEMORY(rmi.freemem*pagesize); // total free physical memory (without swaps)
   Memory_Info[BUFFER_MEM]   = MEMORY(rmi.bufmem*pagesize);
 
-#warning "FIXME: Memory_Info[CACHED_MEM]"
+
+  //FIXME: Memory_Info[CACHED_MEM]"
   Memory_Info[CACHED_MEM] = NO_MEMORY_INFO; // cached memory in ram
   
   long val;
