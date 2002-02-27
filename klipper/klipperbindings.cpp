d6ce0a660461d620d8ddda2906abe1fb01a31d39
Ellis Whitehead <kde@ellisw.net>
2002-02-27 21:35:22 +0000

    Use qt keycodes instead of strings when constructing the global shortcuts. This way they get compile-time checked and the relatively expensive string to key conversion is skipped.
    svn path=/trunk/kdebase/kwin/; revision=139253

--- klipper/klipperbindings.cpp
+++ klipper/klipperbindings.cpp
@@ -5,11 +5,13 @@
 # define DEF( name, key3, key4, fnSlot ) \
    keys->insert( name, i18n(name), QString::null, key3, key4 )
 #endif
+#define WIN KKey::QtWIN
 
 	keys->insert( "Program:klipper", i18n("Clipboard") );
 
-	DEF( I18N_NOOP("Show Klipper Popup-Menu"), "Alt+Ctrl+V", "Meta+Ctrl+V", slotPopupMenu() );
-	DEF( I18N_NOOP("Manually Invoke Action on Current Clipboard"), "Alt+Ctrl+R", "Meta+Ctrl+R", slotRepeatAction() );
-	DEF( I18N_NOOP("Enable/Disable Clipboard Actions"), "Alt+Ctrl+X", "Meta+Ctrl+X", toggleURLGrabber() );
+	DEF( I18N_NOOP("Show Klipper Popup-Menu"), ALT+CTRL+Qt::Key_V, WIN+CTRL+Qt::Key_V, slotPopupMenu() );
+	DEF( I18N_NOOP("Manually Invoke Action on Current Clipboard"), ALT+CTRL+Qt::Key_R, WIN+CTRL+Qt::Key_R, slotRepeatAction() );
+	DEF( I18N_NOOP("Enable/Disable Clipboard Actions"), ALT+CTRL+Qt::Key_X, WIN+CTRL+Qt::Key_X, toggleURLGrabber() );
 
 #undef DEF
+#undef WIN
