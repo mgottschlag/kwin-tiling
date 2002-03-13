366dd89c0f8ce4e43e2b447b5cc27972df3c3dd7
Malte Starostik <malte@kde.org>
2002-03-13 21:50:04 +0000

    CVS_SILENT updated email address
    svn path=/trunk/kdebase/kcontrol/; revision=142821

--- kcontrol/ebrowsing/plugins/ikws/searchprovider.cpp
+++ kcontrol/ebrowsing/plugins/ikws/searchprovider.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000 Malte Starostik <malte.starostik@t-online.de>
+ * Copyright (c) 2000 Malte Starostik <malte@kde.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
