93fa6974b4a59b973c918038dbb16ba8c9b3c239
Ryan Cumming <ryan@kde.org>
2002-04-02 02:48:08 +0000

    GUI: Capitialization
    svn path=/trunk/kdebase/kcontrol/; revision=146508

--- kcontrol/background/backgnd.cpp
+++ kcontrol/background/backgnd.cpp
@@ -143,7 +143,7 @@ KBackground::KBackground(QWidget *parent, const char *name, const QStringList &/
     m_pDeskList = new QListBox(group);
     connect(m_pDeskList, SIGNAL(highlighted(int)), SLOT(slotSelectDesk(int)));
     vbox->addWidget(m_pDeskList);
-    m_pCBCommon = new QCheckBox(i18n("&Common Background"), group);
+    m_pCBCommon = new QCheckBox(i18n("&Common background"), group);
     vbox->addWidget(m_pCBCommon);
     connect(m_pCBCommon, SIGNAL(toggled(bool)), SLOT(slotCommonDesk(bool)));
     QWhatsThis::add( m_pDeskList, i18n("Choose the desktop whose background"
@@ -246,9 +246,9 @@ KBackground::KBackground(QWidget *parent, const char *name, const QStringList &/
     connect( m_WallpaperType, SIGNAL(clicked(int)), SLOT(slotWallpaperType(int)) );
     grid->addMultiCellWidget( m_WallpaperType, 0, 0, 0, 2 );
 
-    QRadioButton *rb = new QRadioButton( i18n("&No Wallpaper"), m_WallpaperType );
-    rb = new QRadioButton( i18n("&Single Wallpaper"), m_WallpaperType );
-    rb = new QRadioButton( i18n("&Multiple Wallpapers"), m_WallpaperType );
+    QRadioButton *rb = new QRadioButton( i18n("&No wallpaper"), m_WallpaperType );
+    rb = new QRadioButton( i18n("&Single wallpaper"), m_WallpaperType );
+    rb = new QRadioButton( i18n("&Multiple wallpapers"), m_WallpaperType );
 
     lbl = new QLabel(i18n("M&ode:"), m_pTab2);
     lbl->setFixedSize(lbl->sizeHint());
@@ -341,13 +341,13 @@ KBackground::KBackground(QWidget *parent, const char *name, const QStringList &/
     QWhatsThis::add( m_pReverseBlending, i18n("For some types of blending, you can"
 					      " reverse the background and wallpaper layers by checking this option.") );
 
-    m_pCBLimit = new QCheckBox(i18n("&Limit Pixmap Cache"), m_pTab3);
+    m_pCBLimit = new QCheckBox(i18n("&Limit pixmap cache"), m_pTab3);
     QWhatsThis::add( m_pCBLimit, i18n( "Checking this box limits the amount of memory that KDE will use to save pixmap (raster graphics). It is advisable to do this, especially if you do not have a lot of memory." ) );
     grid->addMultiCellWidget(m_pCBLimit, 2, 2, 0, 1);
     connect(m_pCBLimit, SIGNAL(toggled(bool)), SLOT(slotLimitCache(bool)));
     hbox = new QHBoxLayout();
     grid->addLayout(hbox, 3, 0);
-    lbl = new QLabel(i18n("Cache &Size"), m_pTab3);
+    lbl = new QLabel(i18n("Cache &size"), m_pTab3);
     lbl->setFixedSize(lbl->sizeHint());
     hbox->addSpacing(20); hbox->addWidget(lbl); hbox->addStretch();
     m_pCacheBox = new QSpinBox(m_pTab3);
