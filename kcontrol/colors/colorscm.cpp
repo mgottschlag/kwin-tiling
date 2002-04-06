0018c1bea58db1cc5076d53b8c2dd7364163500d
Ryan Cumming <ryan@kde.org>
2002-04-06 21:01:42 +0000

    GUI: Remove fullstop
    svn path=/trunk/kdebase/kcontrol/; revision=147514

--- kcontrol/colors/colorscm.cpp
+++ kcontrol/colors/colorscm.cpp
@@ -231,7 +231,7 @@ KColorScheme::KColorScheme(QWidget *parent, const char *name, const QStringList 
     label->setText(i18n("High Contrast", "High"));
     groupLayout->addWidget( label );
 
-    cbExportColors = new QCheckBox(i18n("Apply colors to &non-KDE applications."), this);
+    cbExportColors = new QCheckBox(i18n("Apply colors to &non-KDE applications"), this);
     topLayout->addMultiCellWidget( cbExportColors, 2, 2, 0, 1 );
     connect(cbExportColors, SIGNAL(toggled(bool)), this, SLOT(slotChanged()));
 
