2d8abe5fd0653b620916f30ffe6b0c2b21a671b4
Oswald Buddenhagen <ossi@kde.org>
2002-07-12 17:10:25 +0000

    - renamed kdmdesktop to krootimage - now expecting the name of the config file on the command line
    svn path=/trunk/kdebase/kdm/; revision=166276

--- kdm/kfrontend/krootimage.cpp
+++ kdm/kfrontend/krootimage.cpp
@@ -1,57 +1,79 @@
-#include <unistd.h>
+/* This file is part of the KDE project
+   Copyright (C) ???
+   Copyright (C) 2002 Oswald Buddenhagen <ossi@kde.org>
 
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License version 2 as published by the Free Software Foundation.
 
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <kcmdlineargs.h>
 #include <ksimpleconfig.h>
 #include <klocale.h>
-#include <kcmdlineargs.h>
+
+#include <qfile.h>
+
+#include "krootimage.h"
 
 #include <X11/Xlib.h>  
 
-
-#include "bgrender.h"
-#include "kdmdesktop.moc"
-
-#include <config.h>
 #include <stdlib.h>
 
 static const char *description = 
 	I18N_NOOP("Fancy desktop background for kdm");
 
-static const char *version = "v1.4";
+static const char *version = "v1.5";
+
+static KCmdLineOptions options[] = {
+    { "+config", I18N_NOOP("Name of the configuration file"), 0 }
+};
 
 
-
-MyApplication::MyApplication()
-  : KApplication(), 
-    renderer(0, new KSimpleConfig( 
-	QString::fromLatin1 (KDE_CONFDIR "/kdm/kdmrc") ))
+MyApplication::MyApplication( const char *conf )
+  : KApplication(),
+    renderer( 0, new KSimpleConfig( QFile::decodeName( conf ) ) )
 {
-  connect(&renderer, SIGNAL(imageDone(int)), this, SLOT(renderDone()));
+  connect( &renderer, SIGNAL(imageDone(int)), this, SLOT(renderDone()) );
   renderer.start();
 }
 
 
 void MyApplication::renderDone()
 {
-  QPixmap pm = *renderer.pixmap();
-  desktop()->setBackgroundPixmap(pm);
-  desktop()->repaint(true);
+  desktop()->setBackgroundPixmap( *renderer.pixmap() );
+  desktop()->repaint( true );
   quit();
 }
 
 
 int main(int argc, char *argv[])
 {
-  setenv("HOME", "/tmp", 1);	/* for QSettings */
+  if (!getenv( "HOME" ))
+    setenv( "HOME", "/tmp", 1 );	/* for QSettings */
   KApplication::disableAutoDcopRegistration();
 
-  KLocale::setMainCatalogue("kdesktop");
-  KCmdLineArgs::init(argc, argv, "kdmdesktop", description, version);
+  KLocale::setMainCatalogue( "kdesktop" );
+  KCmdLineArgs::init( argc, argv, "krootimage", description, version );
+  KCmdLineArgs::addCmdLineOptions( options );
 
-  MyApplication app;
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+  if (!args->count())
+    args->usage();
+  MyApplication app( args->arg( 0 ) );
+  args->clear();
   
   // Keep color resources after termination
-  XSetCloseDownMode(qt_xdisplay(), RetainTemporary);
+  XSetCloseDownMode( qt_xdisplay(), RetainTemporary );
 
   app.exec();
 
@@ -59,3 +81,5 @@ int main(int argc, char *argv[])
   
   return 0;
 }
+
+#include "krootimage.moc"
--- kdm/kfrontend/krootimage.h
+++ kdm/kfrontend/krootimage.h
@@ -1,11 +1,29 @@
+/* This file is part of the KDE project
+   Copyright (C) ???
+   Copyright (C) 2002 Oswald Buddenhagen <ossi@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
 #ifndef __KDMDESKTOP_H__
 #define __KDMDESKTOP_H__
 
 
 #include <kapplication.h>
 
-
-#include "bgrender.h"
+#include <bgrender.h>
 
 
 class MyApplication : public KApplication
@@ -13,19 +31,13 @@ class MyApplication : public KApplication
   Q_OBJECT
 
 public:
- 
-  MyApplication();
-
+  MyApplication( const char * );
 
 private slots:
-
   void renderDone();
 
 private:
-  
   KBackgroundRenderer renderer;
-
 };
-
 
 #endif
