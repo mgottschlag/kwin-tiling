c61b445a816078f48bdd21490c5b72e72f461845
Craig Drummond <craig@kde.org>
2002-07-17 22:07:35 +0000

    Change email address
    svn path=/trunk/kdebase/kcontrol/; revision=167230

--- kcontrol/fonts/kxftconfig.cpp
+++ kcontrol/fonts/kxftconfig.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2002 Craig Drummond <cpdrummond@uklinux.net>
+   Copyright (c) 2002 Craig Drummond <Craig.Drummond@gmx.net>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
