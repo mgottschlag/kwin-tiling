846882b89a12f28cceea821681f12b9802c4b9f8
Chris Howells <howells@kde.org>
2002-08-31 20:12:59 +0000

    I declare a war on the overuse of exclmation marks
    svn path=/trunk/kdebase/kcontrol/; revision=174913

--- kcontrol/kcontrol/proxywidget.cpp
+++ kcontrol/kcontrol/proxywidget.cpp
@@ -93,7 +93,7 @@ RootInfoWidget::RootInfoWidget(QWidget *parent, const char *name = 0)
     setFrameShape(QFrame::Box);
     setFrameShadow(QFrame::Raised);
 
-    setText(i18n("<b>Changes in this module require root access!</b><br>"
+    setText(i18n("<b>Changes in this module require root access.</b><br>"
                       "Click the \"Administrator Mode\" button to "
                       "allow modifications in this module."));
 
