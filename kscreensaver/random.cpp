f96596e9eb18be82d01de5930832eb0a5d73852c
Dirk Mueller <mueller@kde.org>
2003-01-12 12:45:50 +0000

    no format strings..
    svn path=/trunk/kdebase/kscreensaver/; revision=199206

--- kscreensaver/random.cpp
+++ kscreensaver/random.cpp
@@ -31,9 +31,9 @@
 
 void usage(char *name)
 {
-    printf(i18n("Usage: %1 [-setup] [args]\n").arg(name).local8Bit());
-    printf(i18n("Starts a random screen saver.\n").local8Bit());
-    printf(i18n("Any arguments (except -setup) are passed on to the screen saver.\n").local8Bit());
+    puts(i18n("Usage: %1 [-setup] [args]\n").arg(name).local8Bit());
+    puts(i18n("Starts a random screen saver.\n").local8Bit());
+    puts(i18n("Any arguments (except -setup) are passed on to the screen saver.\n").local8Bit());
 }
 
 static const char *appName = "random";
