906ea98a686cce4e773575683d40fc4b8eac709a
Waldo Bastian <bastian@kde.org>
2003-05-13 15:34:44 +0000

    s^QUriDrag^KURLDrag^
    svn path=/trunk/kdebase/kcontrol/; revision=225643

--- kcontrol/thememgr/installer.cpp
+++ kcontrol/thememgr/installer.cpp
@@ -65,7 +65,7 @@ ThemeListBox::ThemeListBox(QWidget *parent)
 
 void ThemeListBox::dragEnterEvent(QDragEnterEvent* event)
 {
-   event->accept((event->source() != this) && QUriDrag::canDecode(event));
+   event->accept((event->source() != this) && KURLDrag::canDecode(event));
 }
 
 void ThemeListBox::dropEvent(QDropEvent* event)
@@ -103,7 +103,7 @@ void ThemeListBox::mouseMoveEvent(QMouseEvent *e)
          url.setPath(mDragFile);
          KURL::List urls;
          urls.append(url);
-         QUriDrag *d = KURLDrag::newDrag(urls, this);
+         KURLDrag *d = KURLDrag::newDrag(urls, this);
          d->dragCopy();
       }
    }
