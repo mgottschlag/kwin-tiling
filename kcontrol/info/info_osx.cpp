ffc98fd21e46aeed91b82c1753b2df3370c01154
Benjamin Reed <ranger@befunk.com>
2003-06-29 20:16:54 +0000

    license
    svn path=/trunk/kdebase/kcontrol/; revision=234594

--- kcontrol/info/info_osx.cpp
+++ kcontrol/info/info_osx.cpp
@@ -1,9 +1,24 @@
 /*
- * kdebase3-3.0.1-3.patch,v 1.2 2002/06/24 01:48:12 ranger Exp
+ *  $Id$
  *
- * info_fbsd.cpp is part of the KDE program kcminfo.  This displays
- * various information about the system (hopefully a FreeBSD system)
- * it's running on.
+ *  Copyright (c) 2003 Benjamin Reed <ranger@befunk.com>
+ *
+ *  info_osx.cpp is part of the KDE program kcminfo.  Copied wholesale
+ *  from info_fbsd.cpp =)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 #define INFO_CPU_AVAILABLE
