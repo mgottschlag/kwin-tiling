a1613ed5a3b2a5fff58b88bcf31059f18f106dd9
Oswald Buddenhagen <ossi@kde.org>
2003-08-06 16:02:28 +0000

    security fix: bail out when pam_setcred fails. thx to SuSE
    svn path=/trunk/kdebase/kdm/; revision=241294

--- kdm/backend/client.c
+++ kdm/backend/client.c
@@ -1008,7 +1008,11 @@ StartClient (struct display *d,
 	    exit (1);
 # endif
 # ifdef USE_PAM
-	pam_setcred(pamh, 0);
+	if (pam_setcred(pamh, 0) != PAM_SUCCESS) {
+	    LogError("pam_setcred for %\"s failed, errno=%d\n",
+		     name, errno);
+	    exit (1);
+	}
 	/* pass in environment variables set by libpam and modules it called */
 	pam_env = pam_getenvlist(pamh);
 	ReInitErrorLog ();
@@ -1278,7 +1282,9 @@ SessionExit (struct display *d, int status)
 #ifdef USE_PAM
 	if (pamh) {
 	    /* shutdown PAM session */
-	    pam_setcred(pamh, PAM_DELETE_CRED);
+	    if (pam_setcred(pamh, PAM_DELETE_CRED) != PAM_SUCCESS)
+		LogError("pam_setcred(DELETE_CRED) for %\"s failed, errno=%d\n",
+			 d->verify->user, errno);
 	    pam_close_session(pamh, 0);
 	    pam_end(pamh, PAM_SUCCESS);
 	    pamh = NULL;
