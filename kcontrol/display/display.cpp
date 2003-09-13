e8f84cefded16d163b1aa22c4b27c9b34997fb0b
Nadeem Hasan <nhasan@kde.org>
2003-09-13 22:42:41 +0000

    New unified "Display" module. It combines "randr", "kgamma" and "xinerama".
    I don't know yet how to hide the individual modules from Control Centre.
    
    svn path=/trunk/kdebase/kcontrol/; revision=250853

---
+++ kcontrol/display/display.cpp
@@ -0,0 +1,85 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Nadeem Hasan <nhasan@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "display.h"
+
+#include <kcmoduleloader.h>
+#include <kdialog.h>
+#include <kgenericfactory.h>
+
+#include <qlayout.h>
+#include <qtabwidget.h>
+
+typedef KGenericFactory<KCMDisplay, QWidget> DisplayFactory;
+K_EXPORT_COMPONENT_FACTORY ( kcm_display, DisplayFactory( "kcmdisplay" ) )
+
+KCMDisplay::KCMDisplay( QWidget *parent, const char *name, const QStringList& )
+    : KCModule( parent, name )
+{
+  m_tabs = new QTabWidget( this );
+
+  m_randr = addTab( "randr", i18n(  "Size && Orientation" ) );
+  m_gamma = addTab( "kgamma", i18n( "Monitor Gamma" ) );
+  m_xiner = addTab( "xinerama", i18n( "Multiple Monitors" ) );
+
+  QVBoxLayout *top = new QVBoxLayout( this, 0, KDialog::spacingHint() );
+  top->addWidget( m_tabs );
+
+  setButtons( Apply|Help );
+  load();
+}
+
+KCModule* KCMDisplay::addTab( const QString &name, const QString &label )
+{
+  QWidget *page = new QWidget( m_tabs, name.latin1() );
+  QVBoxLayout *top = new QVBoxLayout( page, KDialog::marginHint() );
+
+  KCModule *kcm = KCModuleLoader::loadModule( name, page );
+
+  if ( kcm )
+  {
+    top->addWidget( kcm );
+    m_tabs->addTab( page, label );
+
+    connect( kcm, SIGNAL( changed(bool) ), SLOT( setChanged(bool) ) );
+  }
+
+  return kcm;
+}
+
+void KCMDisplay::load()
+{
+  if ( m_randr )
+    m_randr->load();
+  if ( m_gamma )
+    m_gamma->load();
+  if ( m_xiner )
+    m_xiner->load();
+}
+
+void KCMDisplay::save()
+{
+  if ( m_randr && m_randr->changed() )
+    m_randr->save();
+  if ( m_gamma && m_gamma->changed() )
+    m_gamma->save();
+  if ( m_xiner && m_xiner->changed() )
+    m_xiner->save();
+}
+
---
+++ kcontrol/display/display.h
@@ -0,0 +1,45 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Nadeem Hasan <nhasan@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <kcmodule.h>
+
+class QTabWidget;
+
+class KCMDisplay : public KCModule
+{
+  public:
+    KCMDisplay( QWidget *parent, const char *name, const QStringList& );
+    void load();
+    void save();
+
+  protected:
+    KCModule* addTab( const QString &name, const QString &label );
+
+  private:
+    QTabWidget *m_tabs;
+    KCModule *m_randr;
+    KCModule *m_gamma;
+    KCModule *m_xiner;
+};
+
+#endif // DISPLAY_H
+
