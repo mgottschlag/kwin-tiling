471fe8f2986bfa11d7179522641b76e264b580f0
Lukáš Tinkl <lukas@kde.org>
2003-11-14 14:50:25 +0000

    i18n fixes
    svn path=/trunk/kdebase/khotkeys/; revision=267062

--- khotkeys/shared/khotkeysglobal.h
+++ khotkeys/shared/khotkeysglobal.h
@@ -1,11 +1,11 @@
 /****************************************************************************
 
  KHotKeys
- 
+
  Copyright (C) 1999-2001 Lubos Lunak <l.lunak@kde.org>
 
  Distributed under the terms of the GNU General Public License version 2.
- 
+
 ****************************************************************************/
 
 #ifndef _KHOTKEYSGLOBAL_H_
@@ -19,6 +19,8 @@
 //#endif
 
 #include <qstring.h>
+
+#include <klocale.h>
 
 class KConfig;
 class QObject;
@@ -43,7 +45,7 @@ QString get_menu_entry_from_path( const QString& path_P );
 
 void init_global_data( bool active_P, QObject* owner_P );
 
-const char* const MENU_EDITOR_ENTRIES_GROUP_NAME = "Menu Editor entries";
+const char* const MENU_EDITOR_ENTRIES_GROUP_NAME = I18N_NOOP( "Menu Editor entries" );
 
 //***************************************************************************
 // Inline
