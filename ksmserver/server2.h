d1206793d690495447b7e8666cc458543b189b98
Luboš Luňák <l.lunak@kde.org>
2004-11-16 14:00:39 +0000

    cvs add - for #58721
    svn path=/trunk/kdebase/ksmserver/; revision=363424

---
+++ ksmserver/server2.h
@@ -0,0 +1,14 @@
+// This is in a separate file only because dcopidl doesn't handle
+// the extern "C" { ... } construct.
+
+#define INT32 QINT32
+#include <X11/Xlib.h>
+#include <X11/Xmd.h>
+#include <X11/ICE/ICElib.h>
+extern "C" {
+#include <X11/ICE/ICEutil.h>
+#include <X11/ICE/ICEmsg.h>
+#include <X11/ICE/ICEproto.h>
+#include <X11/SM/SM.h>
+#include <X11/SM/SMlib.h>
+}
