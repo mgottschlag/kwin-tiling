76f63aff5f6ace3da20da3045cf7d6e234a8f614
Maks Orlovich <maksim@kde.org>
2005-04-12 14:54:51 +0000

    Proper product name for bugzy BUG:88730
    svn path=/trunk/kdebase/kcontrol/; revision=405072

--- kcontrol/randr/main.cpp
+++ kcontrol/randr/main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char **argv)
 	KAboutData aboutData("randr", I18N_NOOP("Resize and Rotate"), krandrtrayVersion, I18N_NOOP("Resize and Rotate System Tray App"), KAboutData::License_GPL, "(c) 2002,2003 Hamish Rodda", 0L, "");
 	aboutData.addAuthor("Hamish Rodda",I18N_NOOP("Maintainer"), "rodda@kde.org");
 	aboutData.addCredit("Lubos Lunak",I18N_NOOP("Many fixes"), "l.lunak@suse.cz");
+	aboutData.setProductName("krandr/krandrtray");
 	KGlobal::locale()->setMainCatalogue("krandr");
 
 	KCmdLineArgs::init(argc,argv,&aboutData);
