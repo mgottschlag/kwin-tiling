d3facc4a3df25f37f4fbfdb317f584d4e24365a5
Markus Brueffer <markus@brueffer.de>
2005-05-06 22:23:18 +0000

    Fix build on FreeBSD 4.9 and below
    svn path=/trunk/KDE/kdebase/kcontrol/; revision=410179

--- kcontrol/usbview/usbdevices.cpp
+++ kcontrol/usbview/usbdevices.cpp
@@ -26,6 +26,7 @@
 
 #ifdef Q_OS_FREEBSD
 #include <sys/ioctl.h>
+#include <sys/param.h>
 #endif
 
 QPtrList<USBDevice> USBDevice::_devices;
@@ -260,11 +261,15 @@ void USBDevice::collectData( int fd, int level, usb_device_info &di, int parent)
 	_channels     = di.udi_nports;
 	
 	// determine the speed
+#if __FreeBSD_version > 490102
 	switch (di.udi_speed) {
 		case USB_SPEED_LOW:  _speed = 1.5;   break;
 		case USB_SPEED_FULL: _speed = 12.0;  break;
 		case USB_SPEED_HIGH: _speed = 480.0; break;
 	}
+#else
+	_speed = di.udi_lowspeed ? 1.5 : 12.0;
+#endif
 
 	// Get all attached devicenodes
 	for ( int i = 0; i < USB_MAX_DEVNAMES; ++i )
