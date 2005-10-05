2031f68d8b1d47e2ebb2075d565aa68209ee574b
Dirk Mueller <mueller@kde.org>
2005-10-05 08:44:32 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdebase/kdm/; revision=467416

--- kcheckpass/kcheckpass.h
+++ kcheckpass/kcheckpass.h
@@ -22,7 +22,7 @@
  *
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  *	Copyright (C) 1998, Caldera, Inc.
  *	Released under the GNU General Public License
--- kcontrol/arts/arts.cpp
+++ kcontrol/arts/arts.cpp
@@ -15,7 +15,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
     Permission is also granted to link this program with the Qt
     library, treating Qt like a library that normally accompanies the
