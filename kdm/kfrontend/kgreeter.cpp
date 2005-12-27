3f84451e402ca49e4f59ddfd58b3e839757ef616
Laurent Montel <montel@kde.org>
2005-12-27 16:47:34 +0000

    Compile
    svn path=/trunk/KDE/kdebase/kdm/; revision=491800

--- kdm/kfrontend/kgreeter.cpp
+++ kdm/kfrontend/kgreeter.cpp
@@ -65,6 +65,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <Q3ValueList>
 #include <QVBoxLayout>
 #include <QAbstractEventDispatcher>
+#include <q3dict.h>
 
 #include <pwd.h>
 #include <grp.h>
