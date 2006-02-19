d7436b846716584d52f8389fe293cbd8ad10eb37
Kevin Ottens <ervin@kde.org>
2006-02-19 17:35:30 +0000

    Make the solidshell more useful. In particular sync with the framework and allow for mount, unmount and eject using solidshell.
    svn path=/branches/work/kdehw/apps/solidshell/; revision=511377

--- solid/solidshell/main.cpp
+++ solid/solidshell/main.cpp
@@ -1,40 +1,293 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2006 Kevin Ottens <ervin@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+*/
+
+#include "main.h"
+
 #include <iostream>
-#include <kapplication.h>
+using namespace std;
+
+#include <QString>
+#include <QStringList>
+
+#include <kinstance.h>
 #include <kcmdlineargs.h>
+#include <klocale.h>
+
 #include <kdehw/devicemanager.h>
 #include <kdehw/device.h>
-#include <kdehw/ifaces/storage.h>
-#include <QString>
+#include <kdehw/ifaces/volume.h>
+
+
+static const char appName[] = "solidshell";
+static const char programName[] = I18N_NOOP("solidshell");
+
+static const char description[] = I18N_NOOP("KDE tool for querying and controlling your hardware from the command line");
+
+static const char version[] = "0.1";
+
+static const KCmdLineOptions options[] =
+{
+   { "commands", I18N_NOOP("Show available commands by domains"), 0},
+   { "+domain", I18N_NOOP("Domain (see --commands)"), 0},
+   { "+command", I18N_NOOP("Command (see --commands)"), 0},
+   { "+[arg(s)]", I18N_NOOP("Arguments for command"), 0},
+   KCmdLineLastOption
+};
+
+std::ostream &operator<<( std::ostream &out, const QString &msg )
+{
+    return ( out << msg.toLocal8Bit().constData() );
+}
+
+std::ostream &operator<<( std::ostream &out, const QMap<QString,QVariant> &properties )
+{
+    foreach( QString key, properties.keys() )
+    {
+        out << "  " << key << " = ";
+
+        QVariant property = properties[key];
+
+        switch ( property.type() )
+        {
+        case QVariant::StringList:
+        {
+            out << "{";
+
+            QStringList list = property.toStringList();
+
+            QStringList::ConstIterator it = list.begin();
+            QStringList::ConstIterator end = list.end();
+
+            for ( ; it!=end; ++it )
+            {
+                out << "'" << *it << "'";
+
+                if ( it+1!=end )
+                {
+                    out << ", ";
+                }
+            }
+
+            out << "}  (string list)" << endl;
+            break;
+        }
+        case QVariant::Bool:
+            out << ( property.toBool()?"true":"false" ) << "  (bool)" << endl;
+            break;
+        case QVariant::Int:
+            out << property.toString()
+                << "  (0x" << QString::number( property.toInt(), 16 ) << ")  (int)" << endl;
+            break;
+        default:
+            out << "'" << property.toString() << "'  (string)" << endl;
+            break;
+        }
+    }
+
+    return out;
+}
+
+void checkArgumentCount( int min, int max )
+{
+    int count = KCmdLineArgs::parsedArgs()->count();
+
+    if ( count < min )
+    {
+        cerr << i18n( "Syntax Error: Not enough arguments" ) << endl;
+        ::exit( 1 );
+    }
+
+    if ( ( max > 0 ) && ( count > max ) )
+    {
+        cerr << i18n( "Syntax Error: Too many arguments" ) << endl;
+        ::exit( 1 );
+    }
+}
 
 int main(int argc, char **argv)
 {
-    KCmdLineArgs::init(argc, argv, "solidshell", 0, 0, 0);
-    KApplication app(false);
+  KCmdLineArgs::init(argc, argv, appName, programName, description, version, false);
 
+  KCmdLineArgs::addCmdLineOptions( options );
+  KCmdLineArgs::addTempFileOption();
+
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+  KInstance instance( appName );
+
+  if ( args->isSet( "commands" ) )
+  {
+      KCmdLineArgs::enable_i18n();
+
+      cout << endl << i18n( "Syntax:" ) << endl << endl;
+
+      cout << "  solidshell hardware list [details]" << endl;
+      cout << i18n( "             # List the hardware available in the system.\n"
+                    "             # If the details option is specified, the device properties are\n"
+                    "             # listed (be careful, property names are backend dependent),\n"
+                    "             # otherwise only device UDIs are listed.\n" ) << endl;
+
+      cout << "  solidshell hardware properties 'udi'" << endl;
+      cout << "  solidshell hardware mount 'udi'" << endl;
+      cout << "  solidshell hardware unmount 'udi'" << endl;
+      cout << "  solidshell hardware eject 'udi'" << endl;
+
+      return 0;
+  }
+
+  return SolidShell::doIt() ? 0 : 1;
+}
+
+bool SolidShell::doIt()
+{
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    checkArgumentCount( 2, 0 );
+
+    QByteArray domain = args->arg( 0 );
+    QByteArray command = args->arg( 1 );
+
+    int fake_argc = 0;
+    char **fake_argv = 0;
+    SolidShell shell( fake_argc, fake_argv );
+
+    if ( domain == "hardware" )
+    {
+        if ( command == "list" )
+        {
+            checkArgumentCount( 2, 3 );
+            QByteArray extra( args->count()==3 ? args->arg( 2 ) : "" );
+            return shell.hwList( extra=="details" );
+        }
+        else if ( command == "properties" )
+        {
+            checkArgumentCount( 3, 3 );
+            QString udi( args->arg( 2 ) );
+            return shell.hwProperties( udi );
+        }
+        else if ( command == "mount" )
+        {
+            checkArgumentCount( 3, 3 );
+            QString udi( args->arg( 2 ) );
+            return shell.hwVolumeCall( Mount, udi );
+        }
+        else if ( command == "unmount" )
+        {
+            checkArgumentCount( 3, 3 );
+            QString udi( args->arg( 2 ) );
+            return shell.hwVolumeCall( Unmount, udi );
+        }
+        else if ( command == "eject" )
+        {
+            checkArgumentCount( 3, 3 );
+            QString udi( args->arg( 2 ) );
+            return shell.hwVolumeCall( Eject, udi );
+        }
+    }
+
+
+    return false;
+}
+
+bool SolidShell::hwList( bool details )
+{
     KDEHW::DeviceManager &manager = KDEHW::DeviceManager::self();
 
     KDEHW::DeviceList all = manager.allDevices();
 
     foreach ( KDEHW::Device device, all )
     {
-        std::cout << "udi = " << device.udi().toLatin1().constData() << std::endl;
+        cout << "udi = '" << device.udi() << "'" << endl;
 
-        if ( device.is<KDEHW::Ifaces::Storage>() )
-	{
-            KDEHW::Ifaces::Storage *storage = device.as<KDEHW::Ifaces::Storage>();
-            std::cout << "Storage vendor" << storage->vendor().toLatin1().constData() << std::endl;
-            std::cout << "Storage model " << storage->product().toLatin1().constData() << std::endl;
-        }
-
-        QMap<QString,QVariant> properties = device.allProperties();
-
-        foreach( QString key, properties.keys() )
+        if ( details )
         {
-            std::cout << "    " << key.toLatin1().constData() << " = "
-                      << "'" << properties[key].toString().toLatin1().constData()
-                      << "'" << std::endl;
+            QMap<QString,QVariant> properties = device.allProperties();
+            cout << properties << endl;
         }
     }
 
-    return 0;
+    return true;
 }
+
+bool SolidShell::hwProperties( const QString &udi )
+{
+    KDEHW::DeviceManager &manager = KDEHW::DeviceManager::self();
+    KDEHW::Device device = manager.findDevice( udi );
+
+    cout << "udi = '" << device.udi() << "'" << endl;
+    QMap<QString,QVariant> properties = device.allProperties();
+    cout << properties << endl;
+
+    return true;
+}
+
+bool SolidShell::hwVolumeCall( SolidShell::VolumeCallType type, const QString &udi )
+{
+    KDEHW::DeviceManager &manager = KDEHW::DeviceManager::self();
+    KDEHW::Device device = manager.findDevice( udi );
+
+    if ( !device.is<KDEHW::Ifaces::Volume>() )
+    {
+        cerr << i18n( "Error: %1 doesn't have the capability Volume." ).arg( udi ) << endl;
+        return false;
+    }
+
+    KIO::Job *job;
+
+    switch( type )
+    {
+    case Mount:
+        job = device.as<KDEHW::Ifaces::Volume>()->mount();
+        break;
+    case Unmount:
+        job = device.as<KDEHW::Ifaces::Volume>()->unmount();
+        break;
+    case Eject:
+        job = device.as<KDEHW::Ifaces::Volume>()->eject();
+        break;
+    }
+
+    connect( job, SIGNAL( result( KIO::Job* ) ),
+             this, SLOT( slotResult( KIO::Job* ) ) );
+    m_loop.exec();
+
+    if ( m_error )
+    {
+        cerr << i18n( "Error: %1" ).arg( m_errorString ) << endl;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void SolidShell::slotResult( KIO::Job *job )
+{
+    m_error = 0;
+
+    if ( job->error() )
+    {
+        m_error = job->error();
+        m_errorString = job->errorString();
+    }
+
+    m_loop.exit();
+}
+
+#include "main.moc"
---
+++ solid/solidshell/main.h
@@ -0,0 +1,50 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2006 Kevin Ottens <ervin@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <QCoreApplication>
+#include <QEventLoop>
+#include <kio/jobclasses.h>
+
+class SolidShell : public QCoreApplication
+{
+    Q_OBJECT
+public:
+    SolidShell( int &argc, char **argv ) : QCoreApplication( argc, argv ) {}
+
+    static bool doIt();
+
+    bool hwList( bool details );
+    bool hwProperties( const QString &udi );
+
+    enum VolumeCallType { Mount, Unmount, Eject };
+    bool hwVolumeCall( VolumeCallType type, const QString &udi );
+
+private:
+    QEventLoop m_loop;
+    int m_error;
+    QString m_errorString;
+private slots:
+    void slotResult( KIO::Job *job );
+};
+
+
+#endif
