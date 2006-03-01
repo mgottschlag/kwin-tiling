d194132885b8bfc4d189a2cdf86b9e8c1df46d04
Alexander Neundorf <neundorf@kde.org>
2006-03-01 20:32:04 +0000

    -fail if kde4 libs haven't been found
    Alex
    
    
    svn path=/trunk/KDE/kdebase/cmake/; revision=514892

--- cmake/modules/FindKDE4.cmake
+++ cmake/modules/FindKDE4.cmake
@@ -2,9 +2,6 @@
 # then ask kde-config for the kde data dirs
 # then check the data dirs for FindKDE4.cmake
 
-#this line includes FindQt.cmake, which searches the Qt library and headers
-FIND_PACKAGE(Qt4 REQUIRED)
-
 MACRO(_MACRO_GETENV_WIN_PATH var name)
    set(${var} $ENV{${name}})
    STRING( REGEX REPLACE "\\\\" "/" ${var} "${${var}}" )
@@ -19,24 +16,6 @@ FIND_PROGRAM(KDE4_KDECONFIG_EXECUTABLE NAMES kde-config
    /opt/kde4/bin
    /opt/kde
    )
-
-
-# these are not good
-# they should be named KDE4_KIO_LIBRARIES etc
-# and moved to kdelibs/cmake/modules/FindKDE4Internal.cmake
-# Alex
-
-set(LIB_KDECORE ${QT_AND_KDECORE_LIBS} ${QT_QTGUI_LIBRARY} ${X11_X11_LIB} DCOP ${ZLIB_LIBRARY})
-
-set(LIB_KDEUI ${LIB_KDECORE} kdeui )
-
-set(LIB_KIO ${LIB_KDEUI} kio)
-
-set(LIB_KPARTS ${LIB_KIO} kparts)
-
-set(LIB_KUTILS ${LIB_KPARTS} kutils)
-
-set(LIB_KDE3SUPPORT ${QT_QT3SUPPORT_LIBRARY} ${LIB_KUTILS})
 
 
 IF (KDE4_KDECONFIG_EXECUTABLE)
@@ -70,4 +49,28 @@ IF (KDE4_KDECONFIG_EXECUTABLE)
 
 
 ENDIF (KDE4_KDECONFIG_EXECUTABLE)
+
+
+IF (KDE4_FIND_REQUIRED AND NOT KDE4_FOUND)
+   MESSAGE(FATAL_ERROR "Could not find KDE4")
+ENDIF (KDE4_FIND_REQUIRED AND NOT KDE4_FOUND)
+
+
+# these are not good
+# they should be named KDE4_KIO_LIBRARIES etc
+# and moved to kdelibs/cmake/modules/FindKDE4Internal.cmake
+# Alex
+
+set(LIB_KDECORE ${QT_AND_KDECORE_LIBS} ${QT_QTGUI_LIBRARY} ${X11_X11_LIB} DCOP ${ZLIB_LIBRARY})
+
+set(LIB_KDEUI ${LIB_KDECORE} kdeui )
+
+set(LIB_KIO ${LIB_KDEUI} kio)
+
+set(LIB_KPARTS ${LIB_KIO} kparts)
+
+set(LIB_KUTILS ${LIB_KPARTS} kutils)
+
+set(LIB_KDE3SUPPORT ${QT_QT3SUPPORT_LIBRARY} ${LIB_KUTILS})
+
 
