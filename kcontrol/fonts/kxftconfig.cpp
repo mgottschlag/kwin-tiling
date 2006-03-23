91ab62ad5d166f4e6a1f63a2dd32211f239a3d99
Olivier Goffart <ogoffart@kde.org>
2006-03-23 09:39:02 +0000

    compile
    svn path=/trunk/KDE/kdebase/workspace/; revision=521684

--- kcontrol/fonts/kxftconfig.cpp
+++ kcontrol/fonts/kxftconfig.cpp
@@ -1536,7 +1536,7 @@ void KXftConfig::setAntiAliasing( bool set )
 
 bool KXftConfig::getAntiAliasing() const
 {
-  return QSettings().readEntry("/qt/useXft", QVariant(false)).toBool();
+  return QSettings().value("/qt/useXft", QVariant(false)).toBool();
 }
 
 
