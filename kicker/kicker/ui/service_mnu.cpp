7edfaa3d6bd65accf3f1146f6536ca3e51b3bf56
Adrian Page <adrian@pagenet.plus.com>
2006-03-25 18:22:28 +0000

    Fix compile.
    svn path=/trunk/KDE/kdebase/workspace/; revision=522480

--- kicker/kicker/ui/service_mnu.cpp
+++ kicker/kicker/ui/service_mnu.cpp
@@ -446,7 +446,7 @@ void PanelServiceMenu::mousePressEvent(QMouseEvent * ev)
 
 void PanelServiceMenu::mouseReleaseEvent(QMouseEvent * ev)
 {
-    if (ev->button() == Qt::RightButton && !Kicker::the()->isKioskImmutable())
+    if (ev->button() == Qt::RightButton && !Kicker::self()->isKioskImmutable())
     {
         int id = static_cast<QMenuItem*>(actionAt(ev->pos()))->id();
 
