95ff99a365a7e9cdd96124ba25e94d72ad01e3a8
Stephan Kulow <coolo@kde.org>
2006-05-01 20:28:17 +0000

    moves messages target
    svn path=/trunk/KDE/kdebase/workspace/; revision=536328

--- kcontrol/kicker/Messages.sh
+++ kcontrol/kicker/Messages.sh
@@ -1,7 +1,7 @@
-#! /usr/bin/env bash
+#! /bin/sh
 # Translation of tiles is used by lookandfeeltab_impl.cpp
 # Tile names are transformed to words with title case
 ### FIXME: Where are the tiles?
 true || (cd ../../../kicker/data/tiles ; ls *_tiny_up.png) | perl -p -e \
-  's/(.*)_tiny_up\.png/i18n\(\"\u$1\"\)\;/; s/[_ ]+(.)/ \u$1/g' >> rc.cpp
+'s/(.*)_tiny_up\.png/i18n\(\"\u$1\"\)\;/; s/[_ ]+(.)/ \u$1/g' >> rc.cpp
 $XGETTEXT *.cpp -o $podir/kcmkicker.pot
