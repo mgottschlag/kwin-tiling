38d0130bcf335d69b2bd320605b0c2de332735b3
David Faure <faure@kde.org>
2006-05-03 14:15:17 +0000

    Use the cache when we have already checked for those things and found them.
    svn path=/trunk/KDE/kdebase/cmake/; revision=536941

--- cmake/modules/FindFontconfig.cmake
+++ cmake/modules/FindFontconfig.cmake
@@ -2,39 +2,46 @@
 # Once done this will define
 #
 #  FONTCONFIG_FOUND - system has Fontconfig
-#  FONTCONFIG_INCLUDE_DIR - the FONTCONFIG include directory
 #  FONTCONFIG_LIBRARIES - Link these to use FONTCONFIG
 #  FONTCONFIG_DEFINITIONS - Compiler switches required for using FONTCONFIG
 #
 
+if (FONTCONFIG_LIBRARIES AND FONTCONFIG_DEFINITIONS)
 
-# use pkg-config to get the directories and then use these values
-# in the FIND_PATH() and FIND_LIBRARY() calls
-INCLUDE(UsePkgConfig)
+  # in cache already
+  set(FONTCONFIG_FOUND TRUE)
 
-PKGCONFIG(fontconfig _FONTCONFIGIncDir _FONTCONFIGLinkDir _FONTCONFIGLinkFlags _FONTCONFIGCflags)
-set(FONTCONFIG_DEFINITIONS ${_FONTCONFIGCflags})
+else (FONTCONFIG_LIBRARIES AND FONTCONFIG_DEFINITIONS)
 
-FIND_LIBRARY(FONTCONFIG_LIBRARIES NAMES fontconfig
-  PATHS
-  ${_FONTCONFIGLinkDir}
-  /usr/lib
-  /usr/local/lib
-)
+  # use pkg-config to get the directories and then use these values
+  # in the FIND_PATH() and FIND_LIBRARY() calls
+  INCLUDE(UsePkgConfig)
+  
+  PKGCONFIG(fontconfig _FONTCONFIGIncDir _FONTCONFIGLinkDir _FONTCONFIGLinkFlags _FONTCONFIGCflags)
 
-if (FONTCONFIG_LIBRARIES)
-   set(FONTCONFIG_FOUND TRUE)
-endif (FONTCONFIG_LIBRARIES)
+  set(FONTCONFIG_DEFINITIONS ${_FONTCONFIGCflags} CACHE INTERNAL "The compilation flags for fontconfig")
 
-if (FONTCONFIG_FOUND)
-  if (NOT FONTCONFIG_FIND_QUIETLY)
-    message(STATUS "Found FONTCONFIG: ${FONTCONFIG_LIBRARIES}")
-  endif (NOT FONTCONFIG_FIND_QUIETLY)
-else (FONTCONFIG_FOUND)
-  if (FONTCONFIG_FIND_REQUIRED)
-    message(FATAL_ERROR "Could NOT find FONTCONFIG")
-  endif (FONTCONFIG_FIND_REQUIRED)
-endif (FONTCONFIG_FOUND)
+  FIND_LIBRARY(FONTCONFIG_LIBRARIES NAMES fontconfig
+    PATHS
+    ${_FONTCONFIGLinkDir}
+    /usr/lib
+    /usr/local/lib
+  )
 
-MARK_AS_ADVANCED(FONTCONFIG_LIBRARIES)
+  if (FONTCONFIG_LIBRARIES)
+     set(FONTCONFIG_FOUND TRUE)
+  endif (FONTCONFIG_LIBRARIES)
 
+  if (FONTCONFIG_FOUND)
+    if (NOT FONTCONFIG_FIND_QUIETLY)
+      message(STATUS "Found FONTCONFIG: ${FONTCONFIG_LIBRARIES}")
+    endif (NOT FONTCONFIG_FIND_QUIETLY)
+  else (FONTCONFIG_FOUND)
+    if (FONTCONFIG_FIND_REQUIRED)
+      message(FATAL_ERROR "Could NOT find FONTCONFIG")
+    endif (FONTCONFIG_FIND_REQUIRED)
+  endif (FONTCONFIG_FOUND)
+
+  MARK_AS_ADVANCED(FONTCONFIG_LIBRARIES)
+
+endif (FONTCONFIG_LIBRARIES AND FONTCONFIG_DEFINITIONS)
