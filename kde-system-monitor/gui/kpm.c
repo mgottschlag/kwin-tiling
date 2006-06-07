717e03df2962b7569a2de05d8739e199e3538be3
Greg Martyn <greg.martyn@gmail.com>
2006-06-07 01:47:35 +0000

    GUI: so much for human readable names.. not that it compiled anyway though :)\nkde-system-monitor->ksysguard
    svn path=/trunk/KDE/kdebase/workspace/; revision=548992

--- kde-system-monitor/gui/kpm.c
+++
@@ -1,8 +0,0 @@
-/* A small wrapper to call kde-system-monitor --showprocesses */
-
-#include <unistd.h>
-
-int main()
-{
-  return execlp( "kde-system-monitor", "kde-system-monitor", "--showprocesses", 0 );
-}
