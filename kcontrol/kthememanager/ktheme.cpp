8e91f65793c31ceb0fd27985b5e31b84191e60b1
Laurent Montel <montel@kde.org>
2006-06-13 20:01:46 +0000

    kdatastream.h doesn't exit it fixes ambigous compile (for me)
    svn path=/trunk/KDE/kdebase/workspace/; revision=551150

--- kcontrol/kthememanager/ktheme.cpp
+++ kcontrol/kthememanager/ktheme.cpp
@@ -29,7 +29,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <kapplication.h>
 #include <kbuildsycocaprogressdialog.h>
 #include <kconfig.h>
-#include <kdatastream.h>
 #include <kdebug.h>
 #include <kglobal.h>
 #include <kiconloader.h>
