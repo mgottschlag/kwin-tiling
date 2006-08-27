79c6c8277ee43f9f3fb0595f7a3cb528354a4158
Will Entriken <kde.org@phor.net>
2006-08-27 15:53:25 +0000

    kill warning
    svn path=/trunk/KDE/kdebase/workspace/; revision=577813

--- kdm/backend/ctrl.c
+++ kdm/backend/ctrl.c
@@ -185,7 +185,7 @@ closeCtrl( struct display *d )
 	CtrlRec *cr = d ? &d->ctrl : &ctrl;
 
 	if (cr->fd >= 0) {
-		char *c;
+		//char *c;
 		UnregisterInput( cr->fd );
 		CloseNClearCloseOnFork( cr->fd );
 		cr->fd = -1;
