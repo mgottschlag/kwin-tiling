03642582a71745d65d41e94fc6c522611941bfca
Script Kiddy <scripty@kde.org>
2006-09-05 15:08:02 +0000

    - Extract the user-visible messages of the UI files - Do no run xgettext 2 times on rc.cpp (one time by the find, one time explicit) (goutte)
    svn path=/trunk/KDE/kdebase/workspace/; revision=581172

--- kpersonalizer/Messages.sh
+++ kpersonalizer/Messages.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
+$EXTRACTRC *.ui >> rc.cpp
 LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
- $XGETTEXT $LIST rc.cpp -o $podir/kpersonalizer.pot
+ $XGETTEXT $LIST -o $podir/kpersonalizer.pot
 fi
