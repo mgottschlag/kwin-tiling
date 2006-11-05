aab3ba6ac471ade5a849c45631b84db92eb8bdac
Alexander Neundorf <neundorf@kde.org>
2006-11-05 17:00:20 +0000

    -add a check to FindSamba.cmake for smbc_set_context(), evaluate this one and compile kioslave/smb/ only if it exists -display the features at the end of the cmake run
    Alex
    
    
    svn path=/trunk/KDE/kdebase/cmake/; revision=602295

--- cmake/modules/FindSamba.cmake
+++ cmake/modules/FindSamba.cmake
@@ -4,34 +4,47 @@
 #  SAMBA_FOUND - system has SAMBA
 #  SAMBA_INCLUDE_DIR - the SAMBA include directory
 #  SAMBA_LIBRARIES - The libraries needed to use SAMBA
+#  SAMBA_HAVE_SMBC_SET_CONTEXT - true if libsmbclient has smbc_set_context()
+
+# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 if(SAMBA_INCLUDE_DIR AND SAMBA_LIBRARIES)
     # Already in cache, be silent
     set(SAMBA_FIND_QUIETLY TRUE)
 endif(SAMBA_INCLUDE_DIR AND SAMBA_LIBRARIES)
 
-FIND_PATH(SAMBA_INCLUDE_DIR libsmbclient.h
-   /usr/include
-   /usr/local/include
-)
+find_path(SAMBA_INCLUDE_DIR NAMES libsmbclient.h )
 
-FIND_LIBRARY(SAMBA_LIBRARIES NAMES smbclient
-   PATHS
-   /usr/lib
-   /usr/local/lib
-)
+find_library(SAMBA_LIBRARIES NAMES smbclient )
 
 
 if(SAMBA_INCLUDE_DIR AND SAMBA_LIBRARIES)
    set(SAMBA_FOUND TRUE)
+   # check whether libsmbclient has smbc_set_context()
+   include(CheckSymbolExists)
+   include(MacroPushRequiredVars)
+   macro_push_required_vars()
+   set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${SAMBA_LIBRARIES})
+   check_symbol_exists(smbc_set_context "libsmbclient.h" SAMBA_HAVE_SMBC_SET_CONTEXT)
+   macro_pop_required_vars()
+else(SAMBA_INCLUDE_DIR AND SAMBA_LIBRARIES)
+   set(SAMBA_FOUND FALSE)
+   set(SAMBA_HAVE_SMBC_SET_CONTEXT FALSE)
 endif(SAMBA_INCLUDE_DIR AND SAMBA_LIBRARIES)
 
 
 if(SAMBA_FOUND)
-   if(NOT SAMBA_FIND_QUIETLY)
+   if(NOT Samba_FIND_QUIETLY)
       message(STATUS "Found samba: ${SAMBA_LIBRARIES}")
-   endif(NOT SAMBA_FIND_QUIETLY)
+   endif(NOT Samba_FIND_QUIETLY)
+else(SAMBA_FOUND)
+   if (Samba_FIND_REQUIRED)
+      message(FATAL_ERROR "Could not find Samba library")
+   endif (Samba_FIND_REQUIRED)
 endif(SAMBA_FOUND)
 
-MARK_AS_ADVANCED(SAMBA_INCLUDE_DIR SAMBA_LIBRARIES)
+mark_as_advanced(SAMBA_INCLUDE_DIR SAMBA_LIBRARIES)
 
