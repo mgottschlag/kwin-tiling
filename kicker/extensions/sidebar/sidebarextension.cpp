83dff9dc93defae1566d9554a726891885031d56
David Faure <faure@kde.org>
2007-02-20 22:30:11 +0000

    Can't see what this was used for?
    svn path=/trunk/KDE/kdebase/workspace/; revision=635790

--- kicker/extensions/sidebar/sidebarextension.cpp
+++ kicker/extensions/sidebar/sidebarextension.cpp
@@ -50,7 +50,8 @@ SidebarExtension::SidebarExtension(const QString& configFile,
      m_resizing(false),
      m_expandedSize(200)
 {
-    new KonqHistoryManager(0);
+    // DF: what was this for?
+    //new KonqHistoryManager(0);
     m_layout=new QHBoxLayout(this);
     m_layout->activate();
     m_sbWrapper=new KVBox(this);
