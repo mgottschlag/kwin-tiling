a11ec27ef32b1e6572142d8cae8679eb357f2362
Stephan Kulow <coolo@kde.org>
2007-03-05 12:15:05 +0000

    one moved :)
    svn path=/trunk/KDE/kdebase/workspace/; revision=639585

--- kdmlib/Messages.sh
+++ kdmlib/Messages.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-### TODO: why do we need 3 POT files for one directory?
+### TODO: why do we need 2 POT files for one directory?
 $XGETTEXT kgreet_classic.cpp rc.cpp -o $podir/kgreet_classic.pot
 $XGETTEXT kgreet_winbind.cpp rc.cpp -o $podir/kgreet_winbind.pot
-$XGETTEXT dmctl.cpp rc.cpp -o $podir/libdmctl.pot
