2f0e835b16fed981b3f64901defdfe85be9bf04f
Laurent Montel <montel@kde.org>
2007-03-10 11:56:12 +0000

    Not necessary
    svn path=/trunk/KDE/kdebase/workspace/; revision=641180

--- plasma/plasma/clock.h
+++ plasma/plasma/clock.h
@@ -36,10 +36,8 @@
 
 #include <X11/Xlib.h>
 #include <X11/extensions/composite.h>
-#include <X11/extensions/Xcomposite.h>
 #include <X11/extensions/Xrender.h>
 #include <X11/extensions/shape.h>
-#include <X11/extensions/Xdamage.h>
 
 #include <kdemacros.h>
 
