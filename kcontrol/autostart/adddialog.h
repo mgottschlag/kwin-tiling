e2f0379f53ef9fd66f700cca4048401732ca8949
Stephen Leaf <smileaf@gmail.com>
2007-03-15 15:37:06 +0000

    Fix lisense address/Add lisense where missing. GUI: Remove the group box around 'Add' and 'Cancel' and rename 'Add' to 'Add Program' to avoid confusion.
    svn path=/trunk/kdereview/autostart/; revision=642843

--- kcontrol/autostart/adddialog.h
+++ kcontrol/autostart/adddialog.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006-2007 by Stephen Leaf                               *
+ *   Copyright (C) 2007 by Stephen Leaf                                    *
  *   smileaf@smileaf.org                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -15,9 +15,8 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA          *
  ***************************************************************************/
-
 
 #ifndef _ADDDIALOG_H_
 #define _ADDDIALOG_H_
