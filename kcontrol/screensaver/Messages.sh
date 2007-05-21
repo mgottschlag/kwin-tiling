93cd80e62d9a61d5682a3d2fb9e7a55084887a25
Chusslove Illich <caslav.ilic@gmx.net>
2007-05-21 01:53:50 +0000

    Fix message extraction of screensaver categories.
    svn path=/trunk/KDE/kdebase/workspace/; revision=666842

--- kcontrol/screensaver/Messages.sh
+++ kcontrol/screensaver/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-sed 's/X-KDE-Category=\(.*\)/i18n\("Screen saver category","\1"\);/' < category_list > category_list.cpp
+sed 's/X-KDE-Category=\(.*\)/i18nc\("Screen saver category","\1"\);/' < category_list > category_list.cpp
 $XGETTEXT *.cpp -o $podir/kcmscreensaver.pot
 rm -f category_list.cpp
