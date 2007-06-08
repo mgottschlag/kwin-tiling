cf2e46ea66a1d4d5578fa7f3dd5ac041c6539252
Stephan Kulow <coolo@kde.org>
2007-06-08 12:46:23 +0000

    simplify Messages
    svn path=/trunk/KDE/kdebase/workspace/; revision=672909

--- kcontrol/kxkb/Messages.sh
+++ kcontrol/kxkb/Messages.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT kxkb.cpp extension.cpp rc.cpp -o $podir/kxkb.pot
-$XGETTEXT kcmlayout.cpp kcmmisc.cpp rc.cpp -o $podir/kcmlayout.pot
-$XGETTEXT rules.cpp pixmap.cpp kxkbbindings.cpp rc.cpp -o $podir/kxkb_common.pot
+$XGETTEXT *.cpp -o $podir/kxkb.pot
