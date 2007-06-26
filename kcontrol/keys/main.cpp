53eb06947fe9556cf0a3f06687a0dc73e15fe9b4
Dirk Mueller <mueller@kde.org>
2007-06-26 12:43:01 +0000

    fix includes
    svn path=/trunk/KDE/kdebase/workspace/; revision=680516

--- kcontrol/keys/main.cpp
+++ kcontrol/keys/main.cpp
@@ -126,12 +126,12 @@ extern "C"
 	QAction* a = 0L;
 // this should match the included files above
 #define NOSLOTS
-#include "../../../workspace/klipper/klipperbindings.cpp"
+#include "../..//klipper/klipperbindings.cpp"
 #include "../../kwin/kwinbindings.cpp"
 #define KICKER_ALL_BINDINGS
-#include "../../../workspace/kicker/kicker/core/kickerbindings.cpp"
-#include "../../../workspace/kicker/taskbar/taskbarbindings.cpp"
-#include "../../../workspace/kdesktop/kdesktopbindings.cpp"
+#include "../../kicker/kicker/core/kickerbindings.cpp"
+#include "../../kicker/taskbar/taskbarbindings.cpp"
+#include "../../kdesktop/kdesktopbindings.cpp"
 #include "../kxkb/kxkbbindings.cpp"
 
   // Write all the global keys to kdeglobals.
