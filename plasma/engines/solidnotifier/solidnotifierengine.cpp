f3bb5fed59385bc8c383e89205543f3561fd4691
Alexis Ménard <menard@kde.org>
2007-07-06 22:33:54 +0000

    Add destructor and change library into software in licence header..sorry ;)
    svn path=/trunk/KDE/kdebase/workspace/; revision=684565

--- plasma/engines/solidnotifier/solidnotifierengine.cpp
+++ plasma/engines/solidnotifier/solidnotifierengine.cpp
@@ -1,13 +1,12 @@
 /*
  *   Copyright (C) 2007 Menard Alexis <darktears31@gmail.com>
  *
- *
- * This library is free software; you can redistribute it and/or
+ * This program is free software you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
@@ -28,6 +27,11 @@
 SolidNotifierEngine::SolidNotifierEngine(QObject* parent, const QStringList& args)
     : Plasma::DataEngine(parent)
 {
+}
+
+SolidNotifierEngine::~SolidNotifierEngine()
+{
+
 }
 
 #include "solidnotifierengine.moc"
--- plasma/engines/solidnotifier/solidnotifierengine.h
+++ plasma/engines/solidnotifier/solidnotifierengine.h
@@ -1,11 +1,12 @@
 /*
- *   Copyright (C) 2007 Menard Alexis <darktears31@gmail.com>
- * This library is free software; you can redistribute it and/or
+ * Copyright (C) 2007 Menard Alexis <darktears31@gmail.com>
+ *
+ * This program is free software you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
@@ -35,6 +36,7 @@ class SolidNotifierEngine : public Plasma::DataEngine
 
     public:
         SolidNotifierEngine( QObject* parent, const QStringList& args);
+        ~SolidNotifierEngine();
 };
 
 K_EXPORT_PLASMA_DATAENGINE(solidnotifier, SolidNotifierEngine)
