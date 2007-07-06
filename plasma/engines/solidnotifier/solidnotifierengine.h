0ee4b8f03e71b0de724d9afa3ad84789e0afe7f5
Alexis Ménard <menard@kde.org>
2007-07-06 19:20:18 +0000

    Add new solidnotifier interface for the future notification with solid...
    -> do nothing yet :=)
    
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=684520

---
+++ plasma/engines/solidnotifier/solidnotifierengine.h
@@ -0,0 +1,41 @@
+/*
+ *   Copyright (C) 2007 Menard Alexis <darktears31@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SOLIDNOTIFIERENGINE_H
+#define SOLIDNOTIFIERENGINE_H
+
+#include <QObject>
+#include <QString>
+#include <QList>
+
+#include "plasma/dataengine.h"
+
+/**
+ * This class evaluates the basic expressions given in the interface.
+ */
+class SolidNotifierEngine : public Plasma::DataEngine
+{
+    Q_OBJECT
+
+    public:
+        SolidNotifierEngine( QObject* parent, const QStringList& args);
+};
+
+K_EXPORT_PLASMA_DATAENGINE(solidnotifier, SolidNotifierEngine)
+
+#endif
