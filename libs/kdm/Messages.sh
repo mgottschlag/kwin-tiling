9ab6146d0cfbfc00363c504ba2877d90ee87350a
Laurent Montel <montel@kde.org>
2007-08-01 11:45:45 +0000

    SVN_SILENT: comment
    svn path=/trunk/KDE/kdebase/workspace/; revision=695113

--- libs/kdm/Messages.sh
+++ libs/kdm/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 ### TODO: why do we need 2 POT files for one directory?
+# Where is generated rc.cpp ?
 $XGETTEXT kgreet_classic.cpp rc.cpp -o $podir/kgreet_classic.pot
 $XGETTEXT kgreet_winbind.cpp rc.cpp -o $podir/kgreet_winbind.pot
