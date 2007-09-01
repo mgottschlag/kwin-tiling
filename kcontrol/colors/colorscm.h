a4473e98a03d3efed53da243ef15cbbcfdb43862
Matthew Woehlke <mw_triad@users.sourceforge.net>
2007-09-01 01:37:09 +0000

    initial framework for totally redesigned color kcm (in part, to demonstrate that I'm not copying anything more than the absolute bare framework from the old version)
    svn path=/trunk/playground/base/colors2/; revision=707090

--- kcontrol/colors/colorscm.h
+++ kcontrol/colors/colorscm.h
@@ -1,81 +1,43 @@
-//-----------------------------------------------------------------------------
-//
-// KDE Display color scheme setup module
-//
-// Copyright (c)  Mark Donohoe 1997
-//
+/* KDE Display color scheme setup module
+ * Copyright (C) 2007 Matthew Woehlke <mw_triad@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #ifndef __COLORSCM_H__
 #define __COLORSCM_H__
 
-#include <QColor>
-#include <QObject>
-
-
 #include <kcmodule.h>
-#include <kdialog.h>
-
-#include "widgetcanvas.h"
 
 class QSlider;
 class QPushButton;
 class QCheckBox;
 class QPalette;
 class KListWidget;
-class KColorSchemeList;
-class KColorTreeWidget;
 
 /**
  * The Desktop/Colors tab in kcontrol.
  */
-class KColorScheme: public KCModule
+class KColorCm : public KCModule
 {
     Q_OBJECT
 
 public:
-    KColorScheme(QWidget *parent, const QVariantList &);
-    ~KColorScheme();
-
-    virtual void load();
-    virtual void save();
-    virtual void defaults();
-
-private Q_SLOTS:
-    void sliderValueChanged(int val);
-    void slotSave();
-    void slotAdd();
-    void slotRemove();
-    void slotImport();
-    void slotColorForWidget(int, const QColor &);
-    void slotPreviewScheme(int);
-    void slotShadeSortColumnChanged(bool);
-    void slotColorChanged(int, const QColor &);
-
-private:
-    void setColorName( const QString &name, int id , int id2 );
-    void readScheme(int index=0);
-    void readSchemeNames();
-	void insertEntry(const QString &sFile, const QString &sName);
-    int findSchemeByName(const QString &scheme);
-    QPalette createPalette();
-    
-    QColor &color(int index);
-
-    int nSysSchemes;
-    bool useRM;
-
-    QColor colorPushColor;
-    QSlider *sb;
-    KColorTreeWidget *mColorTreeWidget;
-    QPushButton *addBt, *removeBt, *importBt;
-    KListWidget *sList;
-    KColorSchemeList *mSchemeList;
-    QString sCurrentScheme;
-
-    WidgetCanvas *cs;
-    
-    QCheckBox *cbExportColors;
-    QCheckBox *cbShadeList;
+    KColorCm(QWidget *parent, const QVariantList &);
+    ~KColorCm();
 };
 
 #endif
