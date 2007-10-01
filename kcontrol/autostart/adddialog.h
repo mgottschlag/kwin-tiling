ebb9a0e6f30fb623de23d57c83990a42411773fd
Stephen Leaf <smileaf@gmail.com>
2007-10-01 03:23:34 +0000

    Fix email address
    svn path=/trunk/playground/base/autostart/; revision=719372

--- kcontrol/autostart/adddialog.h
+++ kcontrol/autostart/adddialog.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2007 by Stephen Leaf                                    *
- *   smileaf@smileaf.org                                                   *
+ *   smileaf@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
