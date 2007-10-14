68498fc31ea6be95344694df483a126bbe637f3e
Richard J. Moore <rich@kde.org>
2007-10-14 20:10:02 +0000

    - bc code is toast
    svn path=/trunk/KDE/kdebase/workspace/; revision=725170

--- plasma/runners/calculator/calculatorrunner.cpp
+++ plasma/runners/calculator/calculatorrunner.cpp
@@ -20,16 +20,12 @@
 #include "calculatorrunner.h"
 
 #include <QAction>
-#include <QFile>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QWidget>
 #include <QScriptEngine>
 
 #include <KIcon>
-#include <KLocale>
-#include <kshell.h> //TODO: replace with KShell after 31/7/2007
-#include <KStandardDirs>
 
 CalculatorRunner::CalculatorRunner( QObject* parent, const QVariantList &args )
     : Plasma::AbstractRunner( parent ),
@@ -73,43 +69,11 @@ bool CalculatorRunner::exec(QAction* action, const QString& term)
     return true;
 }
 
-#ifndef NO_QSCRIPT
-
 QString CalculatorRunner::calculate( const QString& term )
 {
     QScriptEngine eng;
     QScriptValue result = eng.evaluate( term );
     return result.toString();
 }
-
-#else
-
-// function taken from kdesktop/minicli.cpp
-QString CalculatorRunner::calculate( const QString& term )
-{
-    QString result, cmd;
-    const QString bc = KStandardDirs::findExe( "bc" );
-    if ( !bc.isEmpty() ) {
-        cmd = QString( "echo %1 | %2" )
-                     .arg(KShell::quoteArg(QString("scale=8; ") + term),
-                          KShell::quoteArg(bc));
-    }
-    else {
-        cmd = QString( "echo $((%1))" ).arg(term);
-    }
-
-    FILE *fs = popen(QFile::encodeName(cmd).data(), "r");
-    if (fs)
-    {
-        { // scope for QTextStream
-            QTextStream ts(fs, QIODevice::ReadOnly);
-            result = ts.readAll().trimmed();
-        }
-        pclose(fs);
-    }
-    return result;
-}
-
-#endif
 
 #include "calculatorrunner.moc"
