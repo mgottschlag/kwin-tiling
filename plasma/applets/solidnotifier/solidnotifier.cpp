9417756fac4f65dd91ee96f5af02508901f3153f
Alexis Ménard <menard@kde.org>
2007-10-22 06:20:23 +0000

    moved into kdebase as resquested by aaron
    svn path=/trunk/KDE/kdebase/workspace/; revision=727989

---
+++ plasma/applets/solidnotifier/solidnotifier.cpp
@@ -0,0 +1,208 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Alexis Ménard <darktears31@gmail.com>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "solidnotifier.h"
+
+#include <QPainter>
+#include <QColor>
+#include <QApplication>
+
+#include <plasma/widgets/hboxlayout.h>
+#include <plasma/widgets/label.h>
+#include <plasma/widgets/widget.h>
+#include <plasma/widgets/icon.h>
+
+#include <KDialog>
+
+#include <QtDBus/QDBusInterface>
+#include <QtDBus/QDBusReply>
+
+using namespace Plasma;
+
+
+SolidNotifier::SolidNotifier(QObject *parent, const QVariantList &args)
+    : Plasma::Applet(parent, args),m_dialog(0)
+{
+    setDrawStandardBackground(true);
+    setHasConfigurationInterface(true);
+    m_layout = new Plasma::HBoxLayout(this);
+    m_layout->setMargin(0);
+    m_layout->setSpacing(0);
+    test =new QString("Welcome to Device Notifier");
+    m_label=new Plasma::Label(this);
+    m_label->setText(*test);
+    m_label->setPen(QPen(Qt::white));    
+
+    m_time=5;
+    m_height=150;
+
+    SolidEngine = dataEngine("hotplug");
+    m_udi="";
+    icon = false;
+    first=true;
+    
+    m_font = QApplication::font();
+    m_font.setWeight(QFont::Bold);
+
+
+    //connect the timer to MoveDown Animation
+    t=new QTimer(this);
+    connect(t,SIGNAL(timeout()),this,SLOT(moveDown()));
+
+    //connect to engine when a device is plug
+    connect(SolidEngine, SIGNAL(newSource(const QString&)),this, SLOT(SourceAdded(const QString&)));
+
+}
+
+SolidNotifier::~SolidNotifier()
+{
+}
+
+void SolidNotifier::paintInterface(QPainter *p, const QStyleOptionGraphicsItem *option, const QRect &contentsRect)
+{
+    Q_UNUSED(option);
+    Q_UNUSED(p);
+    Q_UNUSED(contentsRect);
+    if(icon)
+    {
+	m_layout->addItem(m_icon);
+    }
+    m_layout->addItem(m_label);
+    kDebug()<<"SolidNotifier:: geometry "<<geometry().width();
+    //hide();
+}
+
+void SolidNotifier::hideNotifier(QGraphicsItem * item)
+{
+    icon=false; 
+    item->hide();
+}
+
+void SolidNotifier::moveUp()
+{
+    t->start(m_time*1000);
+    disconnect(Phase::self(),SIGNAL(movementComplete(QGraphicsItem *)),this,SLOT(hideNotifier(QGraphicsItem *)));
+    show();
+    Phase::self()->moveItem(this, Phase::SlideIn,QPoint(geometry().x(),geometry().y()-m_height));
+}
+
+
+void SolidNotifier::moveDown()
+{
+    t->stop();
+    Phase::self()->moveItem(this, Phase::SlideOut,QPoint(geometry().x(),geometry().y()+m_height));
+    connect(Phase::self(),SIGNAL(movementComplete(QGraphicsItem *)),this,SLOT(hideNotifier(QGraphicsItem *)));
+}
+
+
+void SolidNotifier::updated(const QString &source, Plasma::DataEngine::Data data)
+{
+    if(data.size()>0)
+    {
+		kDebug()<<"SolidNotifier:: "<<data[source].toString();
+		desktop_files=data["predicateFiles"].toStringList();
+		kDebug()<<data["icon"].toString();
+		QString icon_temp = data["icon"].toString();
+		
+		m_layout->removeItem(m_label);
+		delete m_label;
+
+		if(first)
+		{
+		  origin_size=geometry();
+		  first=false;
+		}
+		else
+		{
+		  m_layout->removeItem(m_icon);
+		  delete m_icon;
+		}
+		
+		delete m_layout;
+		m_layout = new Plasma::HBoxLayout(this);
+		m_layout->setMargin(0);
+		m_layout->setSpacing(0);
+    
+
+		k_icon = new KIcon(icon_temp);
+		m_icon=new Plasma::Icon(*k_icon,"",this);
+		
+		m_label=new Plasma::Label(this);
+		icon = true;
+		device_name=i18n("A new device has been detected : \n");
+		device_name+=data["text"].toString();
+		m_label->setPen(QPen(Qt::white));
+		m_label->setText(device_name);
+
+
+		float size_h=origin_size.height();
+		float size_w=origin_size.width();
+		size_h+=m_icon->iconSize().height();
+		size_w+=m_label->geometry().width();
+		QRectF * temp= new QRectF(origin_size.x(),origin_size.y(),size_w,size_h);
+		setGeometry(*temp);
+		update();
+		moveUp();
+     }
+}
+
+void SolidNotifier::SourceAdded(const QString& source)
+{
+    kDebug()<<"SolidNotifier:: source added"<<source;
+    m_udi = source;
+    SolidEngine->connectSource(source, this);
+}
+
+void SolidNotifier::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    Q_UNUSED(event);
+    if(icon)
+    {
+	kDebug()<<"SolidNotifier:: call Solid Ui Server with params :"<<m_udi<<","<<desktop_files;
+	QDBusInterface soliduiserver("org.kde.kded", "/modules/soliduiserver", "org.kde.kded.SolidUiServer");
+	QDBusReply<void> reply = soliduiserver.call("showActionsDialog", m_udi,desktop_files);
+    }
+}
+
+void SolidNotifier::showConfigurationInterface()
+{
+     if (m_dialog == 0) 
+     {
+		kDebug()<<"SolidNotifier:: Enter in configuration interface";
+     	m_dialog = new KDialog;
+        m_dialog->setCaption( i18n("Configure New Device Notifier") );
+
+        ui.setupUi(m_dialog->mainWidget());
+        m_dialog->setButtons( KDialog::Ok | KDialog::Cancel | KDialog::Apply );
+        connect( m_dialog, SIGNAL(applyClicked()), this, SLOT(configAccepted()) );
+        connect( m_dialog, SIGNAL(okClicked()), this, SLOT(configAccepted()) );
+		ui.spinTime->setValue(m_time);
+        ui.spinHeight->setValue(m_height);
+      }
+      m_dialog->show();
+}
+
+void SolidNotifier::configAccepted()
+{
+    kDebug()<<"SolidNotifier:: Config Accepted with params"<<ui.spinTime->value()<<","<<ui.spinHeight->value();
+	m_time=ui.spinTime->value();
+    m_height=ui.spinHeight->value();
+}
+
+#include "solidnotifier.moc"
---
+++ plasma/applets/solidnotifier/solidnotifier.h
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Alexis Ménard <darktears31@gmail.com>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef SOLIDNOTIFIER_H
+#define SOLIDNOTIFIER_H
+
+#include <plasma/applet.h>
+#include <plasma/dataengine.h>
+#include <plasma/phase.h>
+#include <KIcon>
+#include <QTimer>
+
+#include "ui_solidNotifierConfig.h"
+
+namespace Plasma
+{
+    class Svg;
+    class Widget;
+    class Label;
+    class Icon;
+    class HBoxLayout;
+    class ProgressBar;
+} // namespace Plasma
+
+class KDialog;
+
+class SolidNotifier : public Plasma::Applet
+{
+    Q_OBJECT
+
+
+    public:
+        SolidNotifier(QObject *parent, const QVariantList &args);
+        ~SolidNotifier();
+
+        void paintInterface(QPainter *painter, const QStyleOptionGraphicsItem *option, const QRect &contentsRect);
+        void mousePressEvent(QGraphicsSceneMouseEvent *event);
+	void moveUp();
+
+    public slots:
+        void SourceAdded(const QString &name);
+        void updated(const QString &source, Plasma::DataEngine::Data data);
+        void moveDown();
+        void hideNotifier(QGraphicsItem * item);
+	void showConfigurationInterface();
+	void configAccepted();
+
+    private:
+        Plasma::Label * m_label;
+	Plasma::Icon * m_icon;
+        QFont m_font;
+	QRectF origin_size;
+		
+        Plasma::HBoxLayout *m_layout;
+        bool icon;
+	bool first;
+
+        QTimer *t;
+        Plasma::DataEngine* SolidEngine;
+        QStringList desktop_files;
+        QString m_udi;
+        QString * test;
+        QString device_name;
+        KIcon * k_icon;
+	int m_pixelSize;
+	int m_height;
+	int m_time;
+	KDialog *m_dialog;
+        /// Designer Config file
+        Ui::solidNotifierConfig ui;
+
+};
+
+K_EXPORT_PLASMA_APPLET(solidnotifier, SolidNotifier)
+
+#endif
