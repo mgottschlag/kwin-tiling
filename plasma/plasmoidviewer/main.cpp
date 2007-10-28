a0b1992b02bcf2661e3d049cef4177dfd9251b8c
Frerich Raabe <raabe@kde.org>
2007-10-28 22:20:32 +0000

    - A little program for running plasma applets in a window of their own; makes   developing plasma applets nicer since this makes it unnecessary to restart   the desktop all the time when installing a new version of an applet. Invoke   this like 'plasmoidviewer clock' or 'plasmoidviewer knewsticker'.
      Two known issues:
      1.) Plasma::Containment::addApplet seems to return something no matter what.
      I would have thought it returns a null pointer when given some bogus plugin
      name, so that I can do my own error handling. Instead I get an ugly 'The
      object couldn't be loaded' dummy applet.
    
      2.) The size of the viewer window is wrong for many plasma applets; this is
      because they appear in an animation (which starts very small). It works
      correctly for KNewsTicker since KNT simply hardcodes a size hint :-}
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=730489

---
+++ plasma/plasmoidviewer/main.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2007 Frerich Raabe <raabe@kde.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <plasma/containment.h>
+#include <plasma/corona.h>
+
+#include <KApplication>
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KIconLoader>
+#include <KLocale>
+#include <KMessageBox>
+#include <KStandardDirs>
+
+#include <QGraphicsView>
+#include <QIcon>
+
+using namespace Plasma;
+
+static const char description[] = I18N_NOOP( "Run Plasma applets in their own window" );
+static const char version[] = "1.0";
+
+int main(int argc, char **argv)
+{
+    KAboutData aboutData( "plasmoidviewer", 0, ki18n( "Plasma Applet Viewer" ),
+                          version, ki18n( description ), KAboutData::License_BSD,
+                         ki18n( "(C) 2007, The KDE Team" ) );
+    aboutData.addAuthor( ki18n( "Frerich Raabe" ),
+                         ki18n( "Original author" ),
+                        "raabe@kde.org" );
+
+    KCmdLineArgs::init( argc, argv, &aboutData );
+
+    KCmdLineOptions options;
+    options.add( "+applet", ki18n( "Name of applet to add" ) );
+    KCmdLineArgs::addCmdLineOptions( options );
+
+    KApplication app;
+
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs() ;
+    if ( args->count() == 0 ) {
+        return 1;
+    }
+
+    Corona corona;
+    corona.setBackgroundBrush( QPixmap( KStandardDirs::locate( "appdata", "checker.png" ) ) );
+    Containment *containment = corona.addContainment( "null" );
+
+    Applet *applet = containment->addApplet( args->arg( 0 ) );
+    if ( !applet ) {
+        // XXX Can we give a better error message somehow?
+        KMessageBox::error( 0, i18n( "Failed to load applet '%1'.", args->arg( 0 ) ) );
+        return 2;
+    }
+
+    // An Applet::setPosition call which takes the border width (if any) into
+    // account and then calls QGraphicsItem::setPos would be nice here.
+    const qreal borderWidth = ( applet->sizeHint().width() - applet->contentSizeHint().width() ) / 2;
+    applet->setPos( borderWidth, borderWidth );
+    applet->setFlag( QGraphicsItem::ItemIsMovable, false );
+
+    QGraphicsView view( &corona );
+    view.setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+    view.setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+    view.setAlignment( Qt::AlignLeft | Qt::AlignTop );
+    view.setSceneRect( 0, 0, applet->sizeHint().width(), applet->sizeHint().height() );
+    view.resize( view.sceneRect().size().toSize() );
+    view.setWindowTitle( applet->name() );
+    view.setWindowIcon( SmallIcon( applet->icon() ) );
+    view.show();
+
+    return app.exec();
+}
+
