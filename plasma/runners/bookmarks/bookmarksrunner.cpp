65c03a9ef4761d19f6f5b717016c536ff94da06b
Sebastian Kügler <sebas@kde.org>
2007-11-03 11:12:03 +0000

    Bookmark runner by Glenn Ergeerts <glenn.ergeerts@telenet.be>
    svn path=/trunk/KDE/kdebase/workspace/; revision=732265

---
+++ plasma/runners/bookmarks/bookmarksrunner.cpp
@@ -0,0 +1,90 @@
+/*
+ *   Copyright 2007 Glenn Ergeerts <glenn.ergeerts@telenet.be>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "bookmarksrunner.h"
+
+#include <QAction>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QWidget>
+#include <QScriptEngine>
+#include <QList>
+
+#include <KIcon>
+#include <KBookmarkManager>
+#include <KToolInvocation>
+#include <KUrl>
+
+
+BookmarksRunner::BookmarksRunner( QObject* parent, const QVariantList &args )
+    : Plasma::AbstractRunner(parent)
+{
+    Q_UNUSED(args)
+
+    setObjectName(i18n("Bookmarks"));
+}
+
+BookmarksRunner::~BookmarksRunner()
+{
+}
+
+void BookmarksRunner::match(Plasma::SearchContext *search)
+{
+    if (search->searchTerm().length() < 3) {
+        return;
+    }
+
+    KBookmarkManager *bookmarkMgr = KBookmarkManager::userBookmarksManager();
+    KBookmarkGroup bookmarkGrp = bookmarkMgr->root();
+
+    QList<KBookmark> matchingBookmarks = searchBookmarks(bookmarkGrp, search->searchTerm());
+    foreach (KBookmark bookmark, matchingBookmarks) {
+        kDebug() << "Found bookmark: " << bookmark.text() << " (" << bookmark.url().prettyUrl() << ")";
+        Plasma::SearchAction *action = search->addPossibleMatch(this);
+        action->setIcon(KIcon("bookmark"));
+        action->setText(bookmark.text());
+        action->setData(bookmark.url().url());
+    }
+}
+
+QList<KBookmark> BookmarksRunner::searchBookmarks(const KBookmarkGroup &bookmarkGrp, const QString &query)
+{
+    QList<KBookmark> matchingBookmarks;
+    KBookmark currentBookmark = bookmarkGrp.first();
+    while(!currentBookmark.isNull()) {
+        if(currentBookmark.isGroup()) { //recurse
+            matchingBookmarks += searchBookmarks(currentBookmark.toGroup(), query);
+        } else {
+            if(currentBookmark.text().contains(query, Qt::CaseInsensitive)) {
+                matchingBookmarks.append(currentBookmark);
+            }
+        }
+        currentBookmark = bookmarkGrp.next(currentBookmark);
+    }
+    return matchingBookmarks;
+}
+
+void BookmarksRunner::exec(Plasma::SearchAction *action)
+{
+    KUrl url = (KUrl)action->data().toString();
+    kDebug() << "BookmarksRunner::exec opening: " << url.url();
+    KToolInvocation::invokeBrowser(url.url());
+}
+
+#include "bookmarksrunner.moc"
---
+++ plasma/runners/bookmarks/bookmarksrunner.h
@@ -0,0 +1,50 @@
+/*
+ *   Copyright 2007 Glenn Ergeerts <glenn.ergeerts@telenet.be>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef BOOKMARKSRUNNER_H
+#define BOOKMARKSRUNNER_H
+
+#include <KGenericFactory>
+
+#include <plasma/abstractrunner.h>
+
+class QWidget;
+
+class KBookmark;
+class KBookmarkGroup;
+
+
+class BookmarksRunner : public Plasma::AbstractRunner
+{
+    Q_OBJECT
+
+    public:
+        BookmarksRunner(QObject* parent, const QVariantList &args);
+        ~BookmarksRunner();
+
+        void match(Plasma::SearchContext *context);
+        void exec(Plasma::SearchAction *action);
+
+    private:
+        QList<KBookmark> searchBookmarks(const KBookmarkGroup &bookmarkGrp, const QString &query);
+};
+
+K_EXPORT_PLASMA_RUNNER(bookmarksrunner, BookmarksRunner)
+
+#endif
