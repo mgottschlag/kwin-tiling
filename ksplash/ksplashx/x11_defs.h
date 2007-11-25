d54ccb86d4b1dce4d3e89f51d679508647b908fd
Tom Albers <toma@kde.org>
2007-11-25 13:15:52 +0000

    relicense gpl2->gpl2/3 for xcursortheme and change wording for ksplash, so it's picked up by the checker.
    svn path=/trunk/KDE/kdebase/workspace/; revision=741328

--- ksplash/ksplashx/x11_defs.h
+++ ksplash/ksplashx/x11_defs.h
@@ -1,9 +1,10 @@
 /*
  *   Copyright (C) 2007 Luboš Luňák <l.lunak@kde.org>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2
- *   or later as published by the Free Software Foundation
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
