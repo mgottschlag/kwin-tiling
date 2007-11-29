868f5f085e3ef862ec79cfaa216fb6f58446f3d0
Aaron J. Seigo <aseigo@kde.org>
2007-11-29 07:23:39 +0000

    ok, let's try a server side copy this time
    svn path=/trunk/KDE/kdebase/workspace/; revision=742885

---
+++ plasma/scriptengines/qscript/qtgui/timer.cpp
@@ -0,0 +1,34 @@
+#include <QtScript/QScriptValue>
+#include <QtScript/QScriptEngine>
+#include <QtScript/QScriptContext>
+#include <QtScript/QScriptable>
+#include <QtCore/QTimer>
+#include "../backportglobal.h"
+
+Q_DECLARE_METATYPE(QTimer*)
+
+static QScriptValue newTimer(QScriptEngine *eng, QTimer *timer)
+{
+    return eng->newQObject(timer, QScriptEngine::AutoOwnership);
+}
+
+static QScriptValue ctor(QScriptContext *ctx, QScriptEngine *eng)
+{
+    return newTimer(eng, new QTimer(qscriptvalue_cast<QObject*>(ctx->argument(0))));
+}
+
+static QScriptValue toString(QScriptContext *ctx, QScriptEngine *eng)
+{
+    DECLARE_SELF(QTimer, toString);
+    return QScriptValue(eng, QString::fromLatin1("QTimer(interval=%0)")
+                        .arg(self->interval()));
+}
+
+QScriptValue constructTimerClass(QScriptEngine *eng)
+{
+    QScriptValue proto = newTimer(eng, new QTimer());
+    ADD_METHOD(proto, toString);
+    eng->setDefaultPrototype(qMetaTypeId<QTimer*>(), proto);
+
+    return eng->newFunction(ctor, proto);
+}
