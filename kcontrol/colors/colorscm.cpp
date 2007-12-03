bca053f8c8606ea6d5f04516d3ae1c5c1b01bcc1
Matthew Woehlke <mw_triad@users.sourceforge.net>
2007-12-03 22:20:07 +0000

    Move colors to their own tab, so the main tab is now scheme management only, plus a new options list (that may be expanding as time goes on). New string OK'd by Thomas Reitelbach TODO: 'no inactive effects' actually does something (but get the string checked in for translation)
    svn path=/trunk/KDE/kdebase/workspace/; revision=744592

--- kcontrol/colors/colorscm.cpp
+++ kcontrol/colors/colorscm.cpp
@@ -120,6 +120,7 @@ void KColorCm::populateSchemeList()
 void KColorCm::updatePreviews()
 {
     schemePreview->setPalette(m_config);
+    colorPreview->setPalette(m_config);
     inactivePreview->setPalette(m_config, QPalette::Inactive);
     disabledPreview->setPalette(m_config, QPalette::Disabled);
 }
