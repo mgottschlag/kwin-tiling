6da021219e91225e0377caf470af3e7fc5215dfb
Riccardo Iaconelli <riccardo@kde.org>
2008-01-05 20:52:46 +0000

    Be able to configure the desktop even if widgets are locked.
    svn path=/trunk/KDE/kdebase/workspace/; revision=757763

--- plasma/containments/desktop/desktop.cpp
+++ plasma/containments/desktop/desktop.cpp
@@ -125,7 +125,6 @@ void DefaultDesktop::constraintsUpdated(Plasma::Constraints constraints)
         // we need to update the menu items that have already been created
         bool locked = isImmutable();
         m_appletBrowserAction->setVisible(!locked);
-        m_setupDesktopAction->setVisible(!locked);
         if (locked) {
             m_lockDesktopAction->setIcon(KIcon("object-unlocked"));
             m_lockDesktopAction->setText(i18n("Unlock Widgets"));
