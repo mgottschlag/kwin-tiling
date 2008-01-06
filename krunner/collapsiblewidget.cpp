b93a3f6d06bae8926eb04764f3418aa50d8f7591
Laurent Montel <montel@kde.org>
2008-01-06 15:52:00 +0000

    Don't start QTimeLine if QTimeLine alreary started
    svn path=/trunk/KDE/kdebase/workspace/; revision=757947

--- krunner/collapsiblewidget.cpp
+++ krunner/collapsiblewidget.cpp
@@ -145,7 +145,8 @@ void CollapsibleWidget::setExpanded(bool expanded)
 
     d->timeline->setDirection( expanded ? QTimeLine::Forward
                                         : QTimeLine::Backward );
-    d->timeline->start();
+    if(d->timeline->state() != QTimeLine::Running)
+       d->timeline->start();
 }
 
 void CollapsibleWidget::animateCollapse( qreal showAmount )
