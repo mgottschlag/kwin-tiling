mgottschlag/kwin-tiling

commit 0daf676f21a98d53b161f3c3656e00ea4f852326

KUrlify kickoff
svn path=/trunk/KDE/kdebase/workspace/; revision=771023

plasma/applets/kickoff/simpleapplet/simpleapplet.cpp
@@ -305,11 +305,11 @@ void MenuLauncherApplet::toggleMenu(bool pressed)
 
                 d->menuview->addSeparator();
                 QAction *switchaction = d->menuview->addAction(KIcon("system-switch-user"),i18n("Switch User"));
-                switchaction->setData(QUrl("leave:/switch"));
+                switchaction->setData(KUrl("leave:/switch"));
                 QAction *lockaction = d->menuview->addAction(KIcon("system-lock-screen"),i18n("Lock"));
-                lockaction->setData(QUrl("leave:/lock"));
+                lockaction->setData(KUrl("leave:/lock"));
                 QAction *logoutaction = d->menuview->addAction(KIcon("system-log-out"),i18n("Logout"));
-                logoutaction->setData(QUrl("leave:/logout"));
+                logoutaction->setData(KUrl("leave:/logout"));
             } break;
             case Favorites: {
                 Kickoff::MenuView *favview = d->createMenuView(new Kickoff::FavoritesModel(d->menuview));
@@ -344,12 +344,12 @@ void MenuLauncherApplet::toggleMenu(bool pressed)
 void MenuLauncherApplet::actionTriggered(QAction *action)
 {
     if (action->data().type() == QVariant::Url) {
-        QUrl url = action->data().toUrl();
+        KUrl url = action->data().toUrl();
         if (url.scheme() == "leave") {
             if ( ! d->launcher ) {
                 d->launcher = new Kickoff::UrlItemLauncher(d->menuview);
             }
-            d->launcher->openUrl(url.toString());
+            d->launcher->openUrl(url.url());
         }
     }
     else {

plasma/applets/kickoff/core/itemhandlers.cpp
@@ -21,13 +21,13 @@
 #include "core/itemhandlers.h"
 
 // Qt
-#include <QUrl>
 #include <QTimer>
 
 // KDE
 #include <KDebug>
 #include <KService>
 #include <KToolInvocation>
+#include <KUrl>
 #include <solid/powermanagement.h>
 
 // KDE Base
@@ -42,12 +42,12 @@
 
 using namespace Kickoff;
 
-bool ServiceItemHandler::openUrl(const QUrl& url)
+bool ServiceItemHandler::openUrl(const KUrl& url)
 {
-    int result = KToolInvocation::startServiceByDesktopPath(url.toString(),QStringList(),0,0,0,"",true);
+    int result = KToolInvocation::startServiceByDesktopPath(url.pathOrUrl(),QStringList(),0,0,0,"",true);
 
     if (result == 0) {
-        KService::Ptr service = KService::serviceByDesktopPath(url.toString());
+        KService::Ptr service = KService::serviceByDesktopPath(url.pathOrUrl());
 
         if (!service.isNull()) {
             RecentApplications::self()->add(service);
@@ -60,7 +60,7 @@ bool ServiceItemHandler::openUrl(const QUrl& url)
     return result == 0;
 }
 
-bool LeaveItemHandler::openUrl(const QUrl& url)
+bool LeaveItemHandler::openUrl(const KUrl& url)
 {
     m_logoutAction = url.path().remove('/');
 

plasma/applets/kickoff/core/itemhandlers.h
@@ -30,13 +30,13 @@ namespace Kickoff
 class ServiceItemHandler : public UrlItemHandler
 {
 public:
-    virtual bool openUrl(const QUrl& url);
+    virtual bool openUrl(const KUrl& url);
 };
 class LeaveItemHandler : public QObject, public UrlItemHandler
 {
     Q_OBJECT
 public:
-    virtual bool openUrl(const QUrl& url);
+    virtual bool openUrl(const KUrl& url);
 
 private Q_SLOTS:
     void logout();

plasma/applets/kickoff/core/leavemodel.cpp
@@ -39,7 +39,7 @@ class LeaveModel::Private
 
 QStandardItem* LeaveModel::createStandardItem(const QString& url)
 {
-    //Q_ASSERT(QUrl(url).scheme() == "leave");
+    //Q_ASSERT(KUrl(url).scheme() == "leave");
     QStandardItem *item = new QStandardItem();
     const QString basename = QFileInfo(url).baseName();
     if (basename == "logout") {

plasma/applets/kickoff/core/models.cpp
@@ -24,7 +24,6 @@
 // Qt
 #include <QFileInfo>
 #include <QStandardItem>
-#include <QUrl>
 #include <QDir>
 
 // KDE

plasma/applets/kickoff/core/urlitemlauncher.cpp
@@ -25,11 +25,11 @@
 #include <QFileInfo>
 #include <QHash>
 #include <QModelIndex>
-#include <QUrl>
 
 // KDE
 #include <KDebug>
 #include <KRun>
+#include <KUrl>
 #include <Solid/Device>
 #include <Solid/StorageAccess>
 
@@ -50,7 +50,7 @@ public:
 class GenericItemHandler : public UrlItemHandler
 {
 public:
-    virtual bool openUrl(const QUrl& url)
+    virtual bool openUrl(const KUrl& url)
     {
         new KRun(url,0);
         return true;
@@ -67,7 +67,7 @@ public:
     {
         kDebug() << "Opening item with URL" << urlString;
 
-        QUrl url(urlString);
+        KUrl url(urlString);
         HandlerInfo protocolHandler = globalHandlers[url.scheme()];
         if (protocolHandler.type == ProtocolHandler && protocolHandler.handler != 0) {
             return protocolHandler.handler->openUrl(url);

plasma/applets/kickoff/core/urlitemlauncher.h
@@ -25,7 +25,7 @@
 #include <solid/storageaccess.h>
 
 class QModelIndex;
-class QUrl;
+class KUrl;
 
 namespace Kickoff
 {
@@ -40,7 +40,7 @@ class UrlItemHandler
 {
 public:
     virtual ~UrlItemHandler() {}
-    virtual bool openUrl(const QUrl& url) = 0;
+    virtual bool openUrl(const KUrl& url) = 0;
 };
 
 /**

plasma/applets/kickoff/simpleapplet/menuview.cpp
@@ -23,7 +23,9 @@
 // Qt
 #include <QtCore/QAbstractItemModel>
 #include <QtCore/QStack>
-#include <QtCore/QUrl>
+
+// KDE
+#include <KUrl>
 
 // Local
 #include "core/models.h"

commit 7566c40517e2cfeed17b9ab3fedfe296f345f3b0

fix regression introduced with r771023
Please be carefull here since KUrl!=QUrl cause they have different typeid's.

CC_MAIL:begert@gmail.com

svn path=/trunk/KDE/kdebase/workspace/; revision=771227

plasma/applets/kickoff/simpleapplet/simpleapplet.cpp
@@ -305,11 +305,11 @@ void MenuLauncherApplet::toggleMenu(bool pressed)
 
                 d->menuview->addSeparator();
                 QAction *switchaction = d->menuview->addAction(KIcon("system-switch-user"),i18n("Switch User"));
-                switchaction->setData(KUrl("leave:/switch"));
+                switchaction->setData(QUrl("leave:/switch"));
                 QAction *lockaction = d->menuview->addAction(KIcon("system-lock-screen"),i18n("Lock"));
-                lockaction->setData(KUrl("leave:/lock"));
+                lockaction->setData(QUrl("leave:/lock"));
                 QAction *logoutaction = d->menuview->addAction(KIcon("system-log-out"),i18n("Logout"));
-                logoutaction->setData(KUrl("leave:/logout"));
+                logoutaction->setData(QUrl("leave:/logout"));
             } break;
             case Favorites: {
                 Kickoff::MenuView *favview = d->createMenuView(new Kickoff::FavoritesModel(d->menuview));