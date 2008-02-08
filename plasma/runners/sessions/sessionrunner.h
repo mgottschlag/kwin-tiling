758446bdf83fcfdb49d8070afa03a9e53454a4fd
Aaron J. Seigo <aseigo@kde.org>
2008-02-08 17:23:57 +0000

    make shell, services and sessions all plugins. also put sessions as a lower priority.
    svn path=/trunk/KDE/kdebase/workspace/; revision=772408

---
+++ plasma/runners/sessions/sessionrunner.h
@@ -0,0 +1,46 @@
+/*
+ *   Copyright (C) 2006 Aaron Seigo <aseigo@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SESSIONRUNNER_H
+#define SESSIONRUNNER_H
+
+#include <plasma/abstractrunner.h>
+
+class QAction;
+
+/**
+ * This class provides matches for running sessions as well as
+ * an action to start a new session, etc.
+ */
+class SessionRunner : public Plasma::AbstractRunner
+{
+    Q_OBJECT
+
+    public:
+        SessionRunner(QObject *parent, const QVariantList &args);
+        ~SessionRunner();
+
+    protected:
+        void match(Plasma::SearchContext *context);
+        void exec(Plasma::SearchMatch* action);
+};
+
+K_EXPORT_PLASMA_RUNNER(sessions, SessionRunner)
+
+#endif
+
