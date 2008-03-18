15caa128d63f9fda2a116f8ee9657e49f9995e28
Anne-Marie Mahfouf <annma@kde.org>
2008-03-18 19:30:29 +0000

    add autostart start of doc enable doc in .desktop file enable Help button
    svn path=/trunk/kdereview/doc/autostart/; revision=787204

--- kcontrol/autostart/autostart.cpp
+++ kcontrol/autostart/autostart.cpp
@@ -59,8 +59,8 @@ K_PLUGIN_FACTORY(AutostartFactory, registerPlugin<Autostart>();)
     QStringList lstHeader;
     lstHeader<<i18n( "Name" )<< i18n( "Command" )<< i18n( "Status" )<<i18n( "Run On" );
     widget->listCMD->setHeaderLabels(lstHeader);
-
-    setButtons(NoAdditionalButton);
+    setButtons(Help);
+    //setButtons(NoAdditionalButton);
     connect( widget->btnAddScript, SIGNAL(clicked()), SLOT(slotAddCMD()) );
     connect( widget->btnAddProgram, SIGNAL(clicked()), SLOT(slotAddProgram()) );
     connect( widget->btnRemove, SIGNAL(clicked()), SLOT(slotRemoveCMD()) );
