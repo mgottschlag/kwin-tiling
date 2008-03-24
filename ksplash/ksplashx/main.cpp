86ee3b762e973f0a5ed7cb4817708e36c72bc0c5
Luboš Luňák <l.lunak@kde.org>
2008-03-24 14:44:43 +0000

    Fork only when --pid is given, otherwise error output is always disabled.
    svn path=/trunk/KDE/kdebase/workspace/; revision=789540

--- ksplash/ksplashx/main.cpp
+++ ksplash/ksplashx/main.cpp
@@ -48,37 +48,40 @@ int main( int argc, char* argv[] )
             usage( argv[ 0 ] );
         }
     char* theme = argv[ 1 ];
-    int pipes[ 2 ];
-    if( pipe( pipes ) < 0 )
-        {
-        perror( "pipe()" );
-        abort();
-        }
     int parent_pipe = -1;
-    pid_t pid = fork();
-    if( pid < 0 )
+    if( print_pid )
         {
-        perror( "fork()" );
-        abort();
-        }
-    if( pid == 0 )
-        { // child
-        close( pipes[ 0 ] );
-        parent_pipe = pipes[ 1 ];
-        close( 0 ); // close stdin,stdout,stderr, otherwise startkde will block
-        close( 1 );
-        close( 2 );
-        }
-    else
-        { // parent
-        close( pipes[ 1 ] );
-        char buf;
-        while( read( pipes[ 0 ], &buf, 1 ) < 0
-            && ( errno == EINTR || errno == EAGAIN || errno == ECHILD ))
-            ;
-        if( print_pid )
-            printf( "%d\n", pid );
-        return 0;
+        int pipes[ 2 ];
+        if( pipe( pipes ) < 0 )
+            {
+            perror( "pipe()" );
+            abort();
+            }
+        pid_t pid = fork();
+        if( pid < 0 )
+            {
+            perror( "fork()" );
+            abort();
+            }
+        if( pid == 0 )
+            { // child
+            close( pipes[ 0 ] );
+            parent_pipe = pipes[ 1 ];
+            close( 0 ); // close stdin,stdout,stderr, otherwise startkde will block
+            close( 1 );
+            close( 2 );
+            }
+        else
+            { // parent
+            close( pipes[ 1 ] );
+            char buf;
+            while( read( pipes[ 0 ], &buf, 1 ) < 0
+                && ( errno == EINTR || errno == EAGAIN || errno == ECHILD ))
+                ;
+            if( print_pid )
+                printf( "%d\n", pid );
+            return 0;
+            }
         }
     if( !openDisplay())
         return 2;
