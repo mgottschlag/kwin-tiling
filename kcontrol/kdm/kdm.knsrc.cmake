ffca74579b800ac8f81a5d288f4f401e9a15db33
Laurent Montel <montel@kde.org>
2008-04-23 11:37:24 +0000

    Use AbsoluteInstallPath
    svn path=/trunk/KDE/kdebase/workspace/; revision=800115

--- kcontrol/kdm/kdm.knsrc.cmake
+++ kcontrol/kdm/kdm.knsrc.cmake
@@ -1,6 +1,6 @@
 [KNewStuff2]
 ProvidersUrl=http://download.kde.org/khotnewstuff/kdmthemes-providers.xml
 # becomes $KDEHOME/share/apps/kdm/themes
-TargetDir=kdm/themes
+AbsoluteInstallPath=@DATA_INSTALL_DIR@/kdm/themes/
 # unpack the downloaded archive
 Uncompress=true
