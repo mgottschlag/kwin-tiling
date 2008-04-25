mgottschlag/kwin-tiling

commit dc6093b6f803cfe54255e7615309f6364d598a9f

Part of the new svg theme support: now task icons have a margin decided from PanelSvg, but only when the resized icon is still bigger that Liconloader::Small
svn path=/trunk/KDE/kdebase/workspace/; revision=800959

plasma/applets/tasks/abstracttaskitem.cpp
@@ -37,6 +37,7 @@
 #include <KColorUtils>
 #include <KGlobalSettings>
 #include <KDebug>
+#include <KIconLoader>
 
 // Plasma
 #include "plasma/plasma.h"
@@ -458,6 +459,22 @@ QRectF AbstractTaskItem::iconRect() const
     QSizeF bounds = boundingRect().size();
     //leave enough space for the text. usefull in vertical panel
     bounds.setWidth(qMax(bounds.width() / 3, qMin(minimumSize().height(), bounds.width())));
+
+    //add the margins in the svg if the icon wouldn't become too small
+    if (s_taskItemBackground) {
+        //using always the "normal" element to not make the icon size jump
+        s_taskItemBackground->setElementPrefix("normal");
+
+        const int height = bounds.height() - s_taskItemBackground->marginSize(Plasma::TopMargin) - s_taskItemBackground->marginSize(Plasma::BottomMargin);
+        const int width = bounds.width() - s_taskItemBackground->marginSize(Plasma::LeftMargin) - s_taskItemBackground->marginSize(Plasma::RightMargin);
+
+        const int minIconSize = IconSize(KIconLoader::Small);
+
+        if (height > minIconSize && width > minIconSize) {
+            bounds = QSize(width, height);
+        }
+    }
+
     QSize iconSize = _icon.actualSize(bounds.toSize());
 
     return QStyle::alignedRect(QApplication::layoutDirection(), Qt::AlignLeft | Qt::AlignVCenter,

commit 749bc8e903eca1e3ab6144c2e6171fb59c4a8dfa

correcting a problem introduced with r800959 present when background svgs are used still not perfect but further developements will wait until the taskbar is in a more sane situation
svn path=/trunk/KDE/kdebase/workspace/; revision=801070

plasma/applets/tasks/abstracttaskitem.cpp
@@ -462,9 +462,6 @@ QRectF AbstractTaskItem::iconRect() const
 
     //add the margins in the svg if the icon wouldn't become too small
     if (s_taskItemBackground) {
-        //using always the "normal" element to not make the icon size jump
-        s_taskItemBackground->setElementPrefix("normal");
-
         const int height = bounds.height() - s_taskItemBackground->marginSize(Plasma::TopMargin) - s_taskItemBackground->marginSize(Plasma::BottomMargin);
         const int width = bounds.width() - s_taskItemBackground->marginSize(Plasma::LeftMargin) - s_taskItemBackground->marginSize(Plasma::RightMargin);
 