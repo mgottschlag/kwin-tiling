5b4dd98809102ab8f421d45cc5a9f2c3ee1e77ea
Jonathan Riddell <jr@jriddell.org>
2008-05-01 07:44:52 +0000

    move printer-applet to kdeutils to stop circular dependency on kdebindings
    svn path=/trunk/KDE/kdebase/workspace/; revision=802902

--- printer-applet/Messages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-$EXTRACTRC *.rc *.ui >> ./rc.py
-$XGETTEXT --language=Python *.py -o $podir/printer-applet.pot
-rm -f rc.py
--- printer-applet/cmake-modules/FindPyCups.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2008, Jonathan Riddell <jriddell@ubuntu.com>
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-
-try:
-    import cups
-except:
-    exit(1)
-print "Groovy"
