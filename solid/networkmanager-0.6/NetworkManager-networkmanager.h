73d502a9c8fc1e91ae915ff3ca80d79ebf308433
Pino Toscano <pino@kde.org>
2008-05-15 00:23:06 +0000

    add my copyright to the files where i contributed in a significant way
    svn path=/trunk/KDE/kdebase/workspace/; revision=807893

--- solid/networkmanager-0.6/NetworkManager-networkmanager.h
+++ solid/networkmanager-0.6/NetworkManager-networkmanager.h
@@ -1,5 +1,6 @@
 /*  This file is part of the KDE project
     Copyright (C) 2007 Will Stephenson <wstephenson@kde.org>
+    Copyright (C) 2008 Pino Toscano <pino@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
