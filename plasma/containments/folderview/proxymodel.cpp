cfe5a2a92e928de03847f3ea05f8564b10ffceab
Fredrik Höglund <fredrik@kde.org>
2008-05-19 04:06:12 +0000

    Moving the folderview applet to kdebase before the freeze. I'm moving it to containments since it will be changed to inherit Containment instead of Applet shortly.
    svn path=/trunk/KDE/kdebase/workspace/; revision=809546

---
+++ plasma/containments/folderview/proxymodel.cpp
@@ -0,0 +1,61 @@
+/*
+ *   Copyright © 2008 Fredrik Höglund <fredrik@kde.org>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public License
+ *   along with this library; see the file COPYING.LIB.  If not, write to
+ *   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *   Boston, MA 02110-1301, USA.
+ */
+
+#include "proxymodel.h"
+
+#include <KDirModel>
+
+
+ProxyModel::ProxyModel(QObject *parent)
+    : QSortFilterProxyModel(parent)
+{
+}
+
+ProxyModel::~ProxyModel()
+{
+}
+
+QModelIndex ProxyModel::indexForUrl(const KUrl &url) const
+{
+    const KDirModel *dirModel = static_cast<KDirModel*>(sourceModel());
+    return mapFromSource(dirModel->indexForUrl(url));
+}
+
+KFileItem ProxyModel::itemForIndex(const QModelIndex &index) const
+{
+    const KDirModel *dirModel = static_cast<KDirModel*>(sourceModel());
+    return dirModel->itemForIndex(mapToSource(index));
+}
+
+bool ProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
+{
+    const KDirModel *dirModel = static_cast<KDirModel*>(sourceModel());
+    const KFileItem item1 = dirModel->itemForIndex(left);
+    const KFileItem item2 = dirModel->itemForIndex(right);
+
+    // Sort directories first
+    if (item1.isDir() && !item2.isDir())
+        return true;
+
+    if (!item1.isDir() && item2.isDir())
+        return false;
+
+    return QSortFilterProxyModel::lessThan(left, right);
+}
+
