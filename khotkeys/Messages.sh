74020ae35316dc9b2864f5636aa303bde782a64d
Laurent Montel <montel@kde.org>
2008-07-02 07:04:51 +0000

    Remove temporary file
    svn path=/trunk/KDE/kdebase/workspace/; revision=827042

--- khotkeys/Messages.sh
+++ khotkeys/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.khotkeys` >> rc.cpp || exit 11
 $XGETTEXT rc.cpp `find . -name \*.cpp -o -name \*.h` -o $podir/khotkeys.pot
+rm -f rc.cpp
