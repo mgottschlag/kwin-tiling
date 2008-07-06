3fb7ef4291fab8ea09140d0ace35a8a50ac46d31
Aaron J. Seigo <aseigo@kde.org>
2008-07-06 09:21:44 +0000

    missed one in my previous relicensing brigade
    svn path=/trunk/KDE/kdebase/workspace/; revision=828623

--- plasma/plasma/plasmaapp.cpp
+++ plasma/plasma/plasmaapp.cpp
@@ -1,10 +1,10 @@
 /*
  *   Copyright 2006 Aaron Seigo <aseigo@kde.org>
- *   Copyright 2007 Matt Broadstone <mbroadst@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
