0ff227b517767ac84b4fa847d4e9d452f55e0e61
Aaron J. Seigo <aseigo@kde.org>
2008-07-14 07:19:35 +0000

    match the license change from plasma itself
    svn path=/trunk/KDE/kdebase/workspace/; revision=832152

--- krunner/lock/plasma/plasmaapp.cpp
+++ krunner/lock/plasma/plasmaapp.cpp
@@ -1,11 +1,12 @@
 /*
  *   Copyright 2006 Aaron Seigo <aseigo@kde.org>
- *   Copyright 2007 Matt Broadstone <mbroadst@gmail.com>
  *   Copyright 2008 Chani Armitage <chanika@gmail.com>
  *
+ *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
+ *   it under the terms of the GNU General Public License as
+ *   published by the Free Software Foundation; either version 2,
+ *   or (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
