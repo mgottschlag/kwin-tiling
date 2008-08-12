cb2f368cea267dd434915aedd633cc33a0e3811b
Alex Merry <kde@randomguy3.me.uk>
2008-08-12 18:45:17 +0000

    Backport fix: update the entire pager when something changed on one desktop, so that dragging windows between desktops with Active Borders enabled shows up in the pager properly.
    CCBUG: 166619
    
    
    svn path=/branches/KDE/4.1/kdebase/workspace/; revision=845986

--- plasma/applets/pager/pager.cpp
+++ plasma/applets/pager/pager.cpp
@@ -360,11 +360,7 @@ void Pager::recalculateWindowRects()
         }
     }
 
-    if (m_dirtyDesktop < 0 || m_dirtyDesktop >= m_rects.count()) {
-        update();
-    } else {
-        update(m_rects[m_dirtyDesktop]);
-    }
+    update();
 }
 
 void Pager::configAccepted()
