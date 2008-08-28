12e43b2922fc46a8b2b643ade1a2098ddd94a46b
Aaron J. Seigo <aseigo@kde.org>
2008-08-28 21:16:19 +0000

    avoid trusting the windowing environment to always be sane (*sigh* having to support things like people switching window managers mid-session is just so .... what's the word for it .. inane.) BUG:170003
    svn path=/trunk/KDE/kdebase/workspace/; revision=854126

--- plasma/applets/pager/pager.cpp
+++ plasma/applets/pager/pager.cpp
@@ -407,8 +407,11 @@ void Pager::configAccepted()
 
 void Pager::currentDesktopChanged(int desktop)
 {
-    m_currentDesktop = desktop;
+    if (desktop < 1) {
+        return; // bogus value, don't accept it
+    }
 
+    m_currentDesktop = desktop;
     m_dirtyDesktop = -1;
 
     if (!m_timer->isActive()) {
@@ -454,6 +457,10 @@ void Pager::activeWindowChanged(WId id)
 
 void Pager::numberOfDesktopsChanged(int num)
 {
+    if (num < 1) {
+        return; // refuse to update to zero desktops
+    }
+
     m_dirtyDesktop = -1;
 
     m_desktopCount = num;
@@ -559,12 +566,9 @@ void Pager::wheelEvent(QGraphicsSceneWheelEvent *e)
        if (m_kwin->numberOfViewports(0).width() * m_kwin->numberOfViewports(0).height() > 1 )
        desktops = m_kwin->numberOfViewports(0).width() * m_kwin->numberOfViewports(0).height();
        */
-    if (e->delta() < 0)
-    {
+    if (e->delta() < 0) {
         newDesk = m_currentDesktop % desktops + 1;
-    }
-    else
-    {
+    } else {
         newDesk = (desktops + m_currentDesktop - 2) % desktops + 1;
     }
 
@@ -882,7 +886,7 @@ void Pager::paintInterface(QPainter *painter, const QStyleOptionGraphicsItem *op
     for (int i = 0; i < m_windowRects.count(); i++) {
         for (int j = 0; j < m_windowRects[i].count(); j++) {
             QRect rect = m_windowRects[i][j].second;
-            if (m_rects[m_currentDesktop-1].contains(rect)) {
+            if (!m_rects.isEmpty() && m_rects[m_currentDesktop-1].contains(rect)) {
                 if (m_activeWindows.contains(rect)) {
                     painter->setBrush(activeWindowBrushActiveDesk);
                     painter->setPen(activeWindowPen);
