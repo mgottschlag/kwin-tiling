f9557e1f42bf016b819af0e31fb8168d54b0ac3f
Aaron J. Seigo <aseigo@kde.org>
2008-09-20 06:44:08 +0000

    ditch the windows code which broke dbus stuff because dbus gets confused with WId's and must use uints instead; note that thisoverlay doesn't make much sense on windows anyways as its fairly entangled with x11 stuff
    svn path=/trunk/KDE/kdebase/workspace/; revision=862858

--- plasma/shells/screensaver/plasmaapp.h
+++ plasma/shells/screensaver/plasmaapp.h
@@ -88,7 +88,7 @@ public Q_SLOTS:
     /**
      * @return the window id of our view, or 0 if there is none
      */
-    WId viewWinId();
+    uint viewWinId();
 
 private Q_SLOTS:
     void cleanup();
