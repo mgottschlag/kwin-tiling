4c7ac99bfdcf209184161e4627752aa23ddf691e
Simon Edwards <simon@simonzone.com>
2008-10-20 20:01:55 +0000

    fix the directory name for my test python applet.
    svn path=/trunk/KDE/kdebase/workspace/; revision=874124

---
+++ plasma/scriptengines/python/test/plasma-applet-pyclock/contents/ui/updateui.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+pykdeuic4 analog_clock_config.ui > ../code/analog_clock_config_ui.py
+pykdeuic4 calendar.ui > ../code/calendar_ui.py
+pykdeuic4 timezonesConfig.ui > ../code/timezonesConfig_ui.py
