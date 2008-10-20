b2b88623823b25642a91d99833277564faa5c545
Aaron J. Seigo <aseigo@kde.org>
2008-10-20 04:15:14 +0000

    move to kdebase
    svn path=/trunk/KDE/kdebase/workspace/; revision=873730

---
+++ plasma/dataengines/favicons/faviconprovider.cpp
@@ -0,0 +1,106 @@
+/*
+ *   Copyright (C) 2007 Tobias Koenig <tokoe@kde.org>
+ *   Copyright (C) 2008 Marco Martin <notmart@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#include "faviconprovider.h"
+
+#include <QtGui/QImage>
+#include <QtCore/QFile>
+
+#include <KUrl>
+#include <KIO/Job>
+#include <KIO/StoredTransferJob>
+#include <KMimeType>
+#include <KDebug>
+#include <kstandarddirs.h>
+
+class FaviconProvider::Private
+{
+    public:
+        Private( FaviconProvider *parent )
+          : q(parent)
+        {
+        }
+
+        void imageRequestFinished( KJob *job );
+
+        FaviconProvider *q;
+        QImage image;
+        QString cachePath;
+};
+
+void FaviconProvider::Private::imageRequestFinished(KJob *job)
+{
+    if (job->error()) {
+        emit q->error(q);
+        return;
+    }
+
+    KIO::StoredTransferJob *storedJob = qobject_cast<KIO::StoredTransferJob*>(job);
+    image = QImage::fromData(storedJob->data());
+    if (!image.isNull()) {
+        image.save(cachePath, "PNG");
+    }
+    emit q->finished(q);
+}
+
+
+FaviconProvider::FaviconProvider(QObject *parent, const QString &url)
+    : QObject(parent),
+      m_url(url),
+      d(new Private(this))
+{
+    KUrl faviconUrl(url);
+    if (faviconUrl.protocol().isEmpty()) {
+        QString host = faviconUrl.host();
+        faviconUrl = KUrl("http://" + url);
+    }
+
+    QString fileName = KMimeType::favIconForUrl(faviconUrl.url());
+
+    if (!fileName.isEmpty()) {
+        d->cachePath = KStandardDirs::locateLocal("cache",  fileName + ".png");
+        d->image.load(d->cachePath, "PNG");
+    } else {
+        d->cachePath = KStandardDirs::locateLocal("cache",  "favicons/" + faviconUrl.host() + ".png");
+        faviconUrl.setPath("/favicon.ico");
+
+        if (faviconUrl.isValid()) {
+            KIO::StoredTransferJob *job = KIO::storedGet(faviconUrl, KIO::NoReload, KIO::HideProgressInfo);
+            //job->setProperty("uid", id);
+            connect(job, SIGNAL(result(KJob*)), this, SLOT(imageRequestFinished(KJob*)));
+        }
+    }
+}
+
+FaviconProvider::~FaviconProvider()
+{
+}
+
+QImage FaviconProvider::image() const
+{
+    return d->image;
+}
+
+QString FaviconProvider::identifier() const
+{
+    return m_url;
+}
+
+#include "faviconprovider.moc"
---
+++ plasma/dataengines/favicons/faviconprovider.h
@@ -0,0 +1,85 @@
+/*
+ *   Copyright (C) 2007 Tobias Koenig <tokoe@kde.org>
+ *   Copyright (C) 2008 Marco Martin <notmart@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef FAVICONPROVIDER_H
+#define FAVICONPROVIDER_H
+
+#include <QtCore/QObject>
+
+class QImage;
+
+/**
+ * This class provides a favicon for a given url
+ */
+class FaviconProvider : public QObject
+{
+    Q_OBJECT
+
+    public:
+        /**
+         * Creates a new favicon provider.
+         *
+         * @param parent The parent object.
+         */
+        FaviconProvider( QObject *parent, const QString &url);
+
+        /**
+         * Destroys the favicon provider.
+         */
+        ~FaviconProvider();
+
+        /**
+         * Returns the requested image.
+         *
+         * Note: This method returns only a valid image after the
+         *       finished() signal has been emitted.
+         */
+        QImage image() const;
+
+        /**
+         * Returns the identifier of the comic request (name + date).
+         */
+        QString identifier() const;
+
+    Q_SIGNALS:
+        /**
+         * This signal is emitted whenever a request has been finished
+         * successfully.
+         *
+         * @param provider The provider which emitted the signal.
+         */
+        void finished( FaviconProvider *provider );
+
+        /**
+         * This signal is emitted whenever an error has occured.
+         *
+         * @param provider The provider which emitted the signal.
+         */
+        void error( FaviconProvider *provider );
+
+    private:
+      QString m_url;
+
+      class Private;
+      Private* const d;
+
+      Q_PRIVATE_SLOT( d, void imageRequestFinished(KJob *job) )
+};
+
+#endif
