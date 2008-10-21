b074d7bb8ff39ece6995155b01f24d46d370940c
Aaron J. Seigo <aseigo@kde.org>
2008-10-21 01:05:12 +0000

    adjust to ToolTipManager API
    svn path=/trunk/KDE/kdebase/workspace/; revision=874235

--- plasma/libs/libplasmaclock/clockapplet.cpp
+++ plasma/libs/libplasmaclock/clockapplet.cpp
@@ -98,7 +98,7 @@ void ClockApplet::toolTipAboutToShow()
 
 void ClockApplet::toolTipHidden()
 {
-    Plasma::ToolTipManager::self()->clearToolTipContent(this);
+    Plasma::ToolTipManager::self()->setToolTipContent(this);
 }
 
 void ClockApplet::updateToolTipContent()
