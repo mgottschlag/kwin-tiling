d922369f5bbccd68a1fc0c0c692ce33d377a98fe
Chani Armitage <chanika@gmail.com>
2008-10-25 20:11:22 +0000

    fix the "screensaver goes away and then plasma-overlay flickers past a few seconds later" bug I'm surprised at how little code this required in the end. :)
    svn path=/trunk/KDE/kdebase/workspace/; revision=875844

--- plasma/shells/screensaver/plasmaapp.cpp
+++ plasma/shells/screensaver/plasmaapp.cpp
@@ -380,11 +380,22 @@ void PlasmaApp::createView(Plasma::Containment *containment)
     if (KCmdLineArgs::parsedArgs()->isSet("setup")) {
         m_view->enableSetupMode();
         activate();
-    } else if (m_idleOpacity > 0) {
-        m_view->setWindowOpacity(m_idleOpacity);
-        m_view->showView();
+    } else {
+        kDebug() << "checking lockprocess is still around";
+        QDBusInterface lockprocess("org.kde.krunner_lock", "/LockProcess",
+                "org.kde.krunner_lock.LockProcess", QDBusConnection::sessionBus(), this);
+        if (lockprocess.isValid()) {
+            kDebug() << "success!";
+            if (m_idleOpacity > 0) {
+                m_view->setWindowOpacity(m_idleOpacity);
+                m_view->showView();
+                emit viewCreated(m_view->effectiveWinId()); //FIXME this won't work for invisible-idle and is Wrong anyways
+            }
+        } else {
+            kDebug() << "bailing out";
+            qApp->quit();
+        }
     }
-    emit viewCreated(m_view->effectiveWinId());
 }
 
 bool PlasmaApp::eventFilter(QObject *obj, QEvent *event)
