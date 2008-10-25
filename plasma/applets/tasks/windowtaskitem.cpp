067d8b184aafc015e25dc466007829e3eb0e8718
Laurent Montel <montel@kde.org>
2008-10-25 11:14:32 +0000

    add missing i18n
    svn path=/trunk/KDE/kdebase/workspace/; revision=875715

--- plasma/applets/tasks/windowtaskitem.cpp
+++ plasma/applets/tasks/windowtaskitem.cpp
@@ -260,7 +260,7 @@ void WindowTaskItem::contextMenuEvent(QGraphicsSceneContextMenuEvent *e)
     QList <QAction*> actionList;
     QAction *a(0);
     if (m_task->isGrouped()) {
-        a = new QAction("Collapse Parent Group", this);
+        a = new QAction(i18n("Collapse Parent Group"), this);
         actionList.append(a);
         connect(a, SIGNAL(triggered()), m_applet->groupItem(m_task->parentGroup()), SLOT(collapse()));
     }
