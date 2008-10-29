11b9c504b8abf8a77da349e87bb4337511888cad
Rafael Fernández López <ereslibre@kde.org>
2008-10-29 16:52:38 +0000

    Use the inline preview instead of the preview widget, slightly better
    svn path=/trunk/KDE/kdebase/workspace/; revision=877483

--- plasma/wallpapers/image/image.cpp
+++ plasma/wallpapers/image/image.cpp
@@ -408,9 +408,8 @@ void Image::showFileDialog()
 {
     if (!m_dialog) {
         m_dialog = new KFileDialog(KUrl(), "*.png *.jpeg *.jpg *.svg *.svgz", m_widget);
-        KImageFilePreview *previewWidget = new KImageFilePreview(m_dialog);
-        m_dialog->setPreviewWidget(previewWidget);
         m_dialog->setOperationMode(KFileDialog::Opening);
+        m_dialog->setInlinePreviewShown(true);
         m_dialog->setCaption(i18n("Select Wallpaper Image File"));
         m_dialog->setModal(false);
     }
