c42bfe9ac736c9ef279e525a7ab2341ace4db1f0
Sebastian Kügler <sebas@kde.org>
2008-11-11 23:57:35 +0000

    Some system update buggered HAL again, no two batteries show up in my lshal output, and hence in Solid, and hence in the Battery applet.
    So quickly fix the battery applet to work with more than one battery
    (I don't have a physical second battery, so it's a great opportunity I
    get now). Thanks to the HAL people and my OSV's QA department for
    letting this slip through -- it'll improve the battery applet in 4.2.
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=883061

--- plasma/applets/battery/battery.cpp
+++ plasma/applets/battery/battery.cpp
@@ -489,30 +489,29 @@ void Battery::updateStatus()
             if (m_numOfBattery == 1) {
                 if (battery_data.value()["Plugged in"].toBool()) {
                     if (state == "NoCharge") {
-                            batteryLabelText.append(i18n("<b>Battery:</b> %1% (fully charged)", battery_data.value()["Percent"].toString()));
+                            batteryLabelText.append(i18n("<b>Battery:</b> %1% (fully charged)<br />", battery_data.value()["Percent"].toString()));
                     } else if (state == "Discharging") {
-                            batteryLabelText.append(i18n("<b>Battery:</b> %1% (discharging)", battery_data.value()["Percent"].toString()));
+                            batteryLabelText.append(i18n("<b>Battery:</b> %1% (discharging)<br />", battery_data.value()["Percent"].toString()));
                     } else {
-                        batteryLabelText.append(i18n("<b>Battery:</b> %1% (charging)", battery_data.value()["Percent"].toString()));
+                        batteryLabelText.append(i18n("<b>Battery:</b> %1% (charging)<br />", battery_data.value()["Percent"].toString()));
                     }
                 } else {
-                    batteryLabelText.append(i18nc("Battery is not plugged in", "<b>Battery:</b> not present"));
+                    batteryLabelText.append(i18nc("Battery is not plugged in", "<b>Battery:</b> not present<br />"));
                 }
             } else {
                 if (state == "NoCharge") {
-                    batteryLabelText.append(i18n("<b>Battery %1:</b> %2% (fully charged)", bnum, battery_data.value()["Percent"].toString()));
+                    batteryLabelText.append(i18n("<b>Battery %1:</b> %2% (fully charged)<br />", bnum, battery_data.value()["Percent"].toString()));
                 } else if (state == "Discharging") {
-                    batteryLabelText.append(i18n("<b>Battery %1:</b> %2% (discharging)", bnum, battery_data.value()["Percent"].toString()));
+                    batteryLabelText.append(i18n("<b>Battery %1:</b> %2% (discharging)<br />", bnum, battery_data.value()["Percent"].toString()));
                 } else {
-                    batteryLabelText.append(i18n("<b>Battery %1:</b> %2% (charging)", bnum, battery_data.value()["Percent"].toString()));
+                    batteryLabelText.append(i18n("<b>Battery %1:</b> %2% (charging)<br />", bnum, battery_data.value()["Percent"].toString()));
                 }
             }
-
-            if (m_acadapter_plugged) {
-                batteryLabelText.append(i18n("<br /><b>AC Adapter:</b> Plugged in"));
-            } else {
-                batteryLabelText.append(i18n("<br /><b>AC Adapter:</b> Not plugged in"));
-            }
+        }
+        if (m_acadapter_plugged) {
+            batteryLabelText.append(i18n("<b>AC Adapter:</b> Plugged in"));
+        } else {
+            batteryLabelText.append(i18n("<b>AC Adapter:</b> Not plugged in"));
         }
         m_batteryLabel->setText(batteryLabelText);
     }
