bb6f55c16ece039ff5f1e3cca250a86ce6b11610
Tiger Dong <idlecat511@gmail.com>
2008-11-17 11:00:01 +0000

    [GGL] correct the Chinese translation of Google gadget.
    svn path=/trunk/KDE/kdebase/workspace/; revision=885428

--- plasma/scriptengines/google_gadgets/ggl_applet_script.cpp
+++ plasma/scriptengines/google_gadgets/ggl_applet_script.cpp
@@ -91,7 +91,7 @@ bool GglAppletScript::init() {
 
   std::string error;
   if (!ggadget::qt::InitGGL(NULL, "ggl-plasma", profile_dir.c_str(),
-                       kGlobalExtensions, 0, false, &error)) {
+                            kGlobalExtensions, 0, false, &error)) {
     kError() << "Failed to init GGL system:"
              << QString::fromUtf8(error.c_str());
     return false;
