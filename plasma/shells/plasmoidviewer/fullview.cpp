461e99a365d8f39af294c2b372bad11e3d756cc5
Alex Merry <kde@randomguy3.me.uk>
2008-11-23 21:57:01 +0000

    Consistency... use qreal.
    svn path=/trunk/KDE/kdebase/workspace/; revision=888158

--- plasma/shells/plasmoidviewer/fullview.cpp
+++ plasma/shells/plasmoidviewer/fullview.cpp
@@ -115,8 +115,8 @@ void FullView::resizeEvent(QResizeEvent *event)
     qreal newHeight = 0;
 
     if (m_applet->aspectRatioMode() == Plasma::KeepAspectRatio) {
-        float ratio = event->oldSize().width() / event->oldSize().height();
-        float newPossibleWidth = size().height() * ratio;
+        qreal ratio = event->oldSize().width() / event->oldSize().height();
+        qreal newPossibleWidth = size().height() * ratio;
         if (newPossibleWidth > size().width()) {
             newHeight = size().width() / ratio;
             newWidth = newHeight * ratio;
