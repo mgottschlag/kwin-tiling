e0deee232ab010d5ba8fcf4d149fb20a0b025002
Aaron J. Seigo <aseigo@kde.org>
2008-12-17 18:04:48 +0000

    allow defining what sort of shutdown dialog to show (reboot, logout, etc)
    svn path=/trunk/KDE/kdebase/workspace/; revision=898203

--- ksmserver/tests/test.cpp
+++ ksmserver/tests/test.cpp
@@ -8,17 +8,36 @@
 int
 main(int argc, char *argv[])
 {
-   KAboutData about("kapptest", 0, ki18n("kapptest"), "version");
-   KCmdLineArgs::init(argc, argv, &about);
+    KAboutData about("kapptest", 0, ki18n("kapptest"), "version");
+    KCmdLineArgs::init(argc, argv, &about);
 
-   KApplication a;
-   KIconLoader::global()->addAppDir("ksmserver");
-   KSMShutdownFeedback::start();
+    KCmdLineOptions options;
+    options.add("t");
+    options.add("type <name>", ki18n("The type of shutdown to emulate: Default, None, Reboot, Halt or Logout"), "None");
+    KCmdLineArgs::addCmdLineOptions(options);
 
-   KWorkSpace::ShutdownType sdtype = KWorkSpace::ShutdownTypeNone;
-   QString bopt;
-   (void)KSMShutdownDlg::confirmShutdown( true, sdtype, bopt );
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+    KApplication a;
+    KIconLoader::global()->addAppDir("ksmserver");
+    KSMShutdownFeedback::start();
+
+    QString sdtypeOption = args->getOption("type").toLower();
+
+    KWorkSpace::ShutdownType sdtype = KWorkSpace::ShutdownTypeNone;
+    if (sdtypeOption == "default") {
+        sdtype = KWorkSpace::ShutdownTypeDefault;
+    } else if (sdtypeOption == "reboot") {
+        sdtype = KWorkSpace::ShutdownTypeReboot;
+    } else if (sdtypeOption == "halt") {
+        sdtype = KWorkSpace::ShutdownTypeReboot;
+    } else if (sdtypeOption == "logout") {
+        sdtype = KWorkSpace::ShutdownTypeReboot;
+    }
+
+    QString bopt;
+    (void)KSMShutdownDlg::confirmShutdown( true, sdtype, bopt );
 /*   (void)KSMShutdownDlg::confirmShutdown( false, sdtype, bopt ); */
 
-   KSMShutdownFeedback::stop();
+    KSMShutdownFeedback::stop();
 }
