d4d6a0ee28f78ff0c815656daa30a5ff202bb618
Frank Karlitschek <karlitschek@kde.org>
2009-01-05 20:02:41 +0000

    new category for kdm 4.x themes
    svn path=/trunk/KDE/kdebase/workspace/; revision=906250

--- kcontrol/kdm/kdm.knsrc.cmake
+++ kcontrol/kdm/kdm.knsrc.cmake
@@ -1,5 +1,5 @@
 [KNewStuff2]
-ProvidersUrl=http://download.kde.org/khotnewstuff/kdmthemes-providers.xml
+ProvidersUrl=http://download.kde.org/khotnewstuff/kdmthemes4-providers.xml
 # becomes $prefix/share/apps/kdm/themes
 AbsoluteInstallPath=@DATA_INSTALL_DIR@/kdm/themes/
 # unpack the downloaded archive
