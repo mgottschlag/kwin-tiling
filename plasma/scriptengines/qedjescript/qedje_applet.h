44b7e65dbfaafa0d71f6f10fa7451d7bc124cc0a
Artur Duque de Souza <asouza@kde.org>
2009-01-14 14:36:32 +0000

    Fix issues found by krazy2 on qedjescript stuff like adding licenses and include own headers first.
    svn path=/trunk/KDE/kdebase/workspace/; revision=910981

--- plasma/scriptengines/qedjescript/qedje_applet.h
+++ plasma/scriptengines/qedjescript/qedje_applet.h
@@ -1,13 +1,34 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Artur Duque de Souza <morpheuz@gmail.com>       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+
 #ifndef QEDJEAPPLETSCRIPT_HEADER
 #define QEDJEAPPLETSCRIPT_HEADER
 
-#include <Plasma/AppletScript>
+#include "ui_qedjeConfig.h"
 
 // Include qedje stuff
 #include <qzion.h>
 #include <qedje.h>
 
-#include "ui_qedjeConfig.h"
+#include <Plasma/AppletScript>
+
 
 // Define our plasma AppletScript
 class QEdjeAppletScript: public Plasma::AppletScript
--- plasma/scriptengines/qedjescript/qedje_package.cpp
+++ plasma/scriptengines/qedjescript/qedje_package.cpp
@@ -1,3 +1,25 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Artur Duque de Souza <morpheuz@gmail.com>       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include <qedje_package.h>
+#include <qedje.h>
+
 #include <QtCore/QDir>
 #include <QtCore/QFile>
 #include <QDir>
@@ -6,9 +28,6 @@
 #include <Plasma/Package>
 #include <Plasma/PackageStructure>
 #include <Plasma/PackageMetadata>
-
-#include <qedje_package.h>
-#include <qedje.h>
 
 K_EXPORT_PLASMA_PACKAGESTRUCTURE(qedjescripts, QEdjePackage)
 
