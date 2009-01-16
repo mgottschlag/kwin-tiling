ab5609fbd2da7083cc09af59f72ded4610007424
Aaron J. Seigo <aseigo@kde.org>
2009-01-16 05:50:48 +0000

    SVN_SILENT: my (c) as requested by shawn
    svn path=/trunk/KDE/kdebase/workspace/; revision=911778

--- plasma/dataengines/weather/weatherengine.cpp
+++ plasma/dataengines/weather/weatherengine.cpp
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2007 by Shawn Starr <shawn.starr@rogers.com>            *
+ *   Copyright (C) 2009 by Aaron Seigo <aseigo@kde.org>                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
