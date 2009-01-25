mgottschlag/kwin-tiling

commit 1df3139f76ff34a0f52d12832a3ef5bf94909a67

Fix a crash trying to acces deleted items.
svn path=/trunk/KDE/kdebase/workspace/; revision=916145

plasma/applets/kickoff/core/applicationmodel.cpp
@@ -172,9 +172,10 @@ void ApplicationModelPrivate::fillNode(const QString &_relPath, AppNode *node)
                     continue;
                 } else {
                     // find and remove the existing entry with the same name
-                    for (int i = 0 ; i < node->children.count() ; i++) {
-                        if (node->children[i]->appName == appName) {
-                            delete node->children.takeAt(i);
+                    for (QList<AppNode*>::iterator i = node->children.begin(); i != node->children.end(); ++i) {
+                        if ((*i)->appName == appName) {
+                            delete *i;
+                            node->children.erase(i);
                         }
                     }
                 }

commit 0ee31fe5c72fe8111bdf0bed8e1dc818b7d400ec

fix for crash bug #181829 which was introduced with r916145 to fix another crash.
svn path=/trunk/KDE/kdebase/workspace/; revision=916341

plasma/applets/kickoff/core/applicationmodel.cpp
@@ -172,10 +172,9 @@ void ApplicationModelPrivate::fillNode(const QString &_relPath, AppNode *node)
                     continue;
                 } else {
                     // find and remove the existing entry with the same name
-                    for (QList<AppNode*>::iterator i = node->children.begin(); i != node->children.end(); ++i) {
-                        if ((*i)->appName == appName) {
-                            delete *i;
-                            node->children.erase(i);
+                    for (int i = node->children.count() - 1; i >= 0; --i) {
+                        if (node->children[i]->appName == appName) {
+                            delete node->children.takeAt(i);
                         }
                     }
                 }