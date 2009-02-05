efab5195958228b22a7595bbaf376bd159a6c435
Robin Pedersen <robinpeder@gmail.com>
2009-02-05 17:05:09 +0000

    backport 921782
    CCBUG: 180440
    CCBUG: 182487
    
    svn path=/branches/KDE/4.2/kdebase/workspace/; revision=921784

--- plasma/applets/pager/pager.cpp
+++ plasma/applets/pager/pager.cpp
@@ -268,12 +268,18 @@ void Pager::recalculateGeometry()
     qreal itemWidth;
 
     if (formFactor() == Plasma::Vertical) {
-        itemWidth = (contentsRect().width() - padding * (columns - 1)) / columns;
+        if (location() == Plasma::Desktop)
+            itemWidth = (contentsRect().width() - padding * (columns - 1)) / columns;
+        else
+            itemWidth = (contentsRect().width() - leftMargin - rightMargin - padding * (columns - 1)) / columns;
         m_widthScaleFactor = itemWidth / Kephal::ScreenUtils::desktopGeometry().width();
         itemHeight = Kephal::ScreenUtils::desktopGeometry().height() * m_widthScaleFactor;
         m_heightScaleFactor = m_widthScaleFactor;
     } else {
-        itemHeight = (contentsRect().height() - padding * (rows - 1)) / rows;
+        if (location() == Plasma::Desktop)
+            itemHeight = (contentsRect().height() - padding * (rows - 1)) / rows;
+        else
+            itemHeight = (contentsRect().height() - topMargin - bottomMargin - padding * (rows - 1)) / rows;
         m_heightScaleFactor = itemHeight / Kephal::ScreenUtils::desktopGeometry().height();
         itemWidth = Kephal::ScreenUtils::desktopGeometry().width() * m_heightScaleFactor;
         if (m_displayedText == Name) {
