72865226f314289e5a71edbb1ac92a6c245753bd
Marco Martin <notmart@gmail.com>
2009-02-10 20:00:11 +0000

    the two provided dbus methods works, a servicerequested signal
    svn path=/trunk/playground/base/plasma/systemtraydaemon/; revision=924440

--- statusnotifierwatcher/systemtraydaemon.cpp
+++ statusnotifierwatcher/systemtraydaemon.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright 2008 by Marco Martin <notmart@gmail.com>                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
 
 #include "systemtraydaemon.h"
 
@@ -27,8 +45,8 @@ SystemTrayDaemon::SystemTrayDaemon(QObject *parent, const QList<QVariant>&)
     //setModuleName("systemtraydaemon");
     new SystemtrayDaemonAdaptor(this);
     QDBusConnection dbus = QDBusConnection::sessionBus();
-    dbus.registerService("org.kde.SystemTrayWatcher");
-    dbus.registerObject("/SystemTrayDaemon", this);
+    dbus.registerService("org.kde.SystemTrayDaemon");
+    dbus.registerObject("/SystemTrayWatcher", this);
 }
 
 SystemTrayDaemon::~SystemTrayDaemon()
