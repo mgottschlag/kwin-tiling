a327bb486422a57044bd288c4eb2821b7b78a6ff
Ben Cooksley <bcooksley@kde.org>
2009-02-27 05:10:11 +0000

    Add two Solid Device Interfaces previously missed, Video and Serial Interface
    svn path=/trunk/kdereview/solid-actions-kcm/; revision=932635

--- solid/solid-actions-kcm/solid-action-data.cpp
+++ solid/solid-actions-kcm/solid-action-data.cpp
@@ -46,6 +46,8 @@
 #include <Solid/OpticalDrive>
 #include <Solid/StorageVolume>
 #include <Solid/OpticalDisc>
+#include <solid/video.h>
+#include <solid/serialinterface.h>
 
 SolidActionData::SolidActionData(bool includeFiles)
 {
@@ -112,12 +114,12 @@ QList<QMetaObject> SolidActionData::fillInterfaceList()
     interfaces.append( Solid::NetworkInterface::staticMetaObject );
     interfaces.append( Solid::PortableMediaPlayer::staticMetaObject );
     interfaces.append( Solid::Processor::staticMetaObject );
-    //interfaces.append( Solid::SerialInterface::staticMetaObject ); // The header does not exist?
+    interfaces.append( Solid::SerialInterface::staticMetaObject ); // The header does not exist?
     interfaces.append( Solid::StorageAccess::staticMetaObject );
     interfaces.append( Solid::StorageDrive::staticMetaObject );
     interfaces.append( Solid::OpticalDrive::staticMetaObject );
     interfaces.append( Solid::StorageVolume::staticMetaObject );
     interfaces.append( Solid::OpticalDisc::staticMetaObject );
-    //interfaces.append( Solid::Video::staticMetaObject ); // The header does not exist?
+    interfaces.append( Solid::Video::staticMetaObject ); // The header does not exist?
     return interfaces;
 }
