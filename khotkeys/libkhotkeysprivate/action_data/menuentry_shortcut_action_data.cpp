58b0dab93cffdfdd2a3585dd1d6478efb8a3ed11
Michael Jansen <kde@michael-jansen.biz>
2009-03-23 23:25:34 +0000

    Coding Style
    svn path=/trunk/KDE/kdebase/workspace/; revision=943513

--- khotkeys/libkhotkeysprivate/action_data/menuentry_shortcut_action_data.cpp
+++ khotkeys/libkhotkeysprivate/action_data/menuentry_shortcut_action_data.cpp
@@ -1,12 +1,21 @@
-/****************************************************************************
-
- KHotKeys
- 
- Copyright (C) 1999-2001 Lubos Lunak <l.lunak@kde.org>
-
- Distributed under the terms of the GNU General Public License version 2.
- 
-****************************************************************************/
+/**
+ * Copyright (C) 1999-2001 Lubos Lunak <l.lunak@kde.org>
+ * Copyright (C) 2009 Michael Jansen <kde@michael-jansen.biz>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include "menuentry_shortcut_action_data.h"
 
@@ -17,57 +26,60 @@
 #include <kconfiggroup.h>
 #include <kdebug.h>
 
-namespace KHotKeys
-{
+namespace KHotKeys {
 
 
 template<> KDE_EXPORT
 void Simple_action_data< ShortcutTrigger, MenuEntryAction >
-    ::cfg_write( KConfigGroup& cfg_P ) const
+    ::cfg_write( KConfigGroup& cfg ) const
     {
-    base::cfg_write( cfg_P );
-    cfg_P.writeEntry( "Type", "MENUENTRY_SHORTCUT_ACTION_DATA" );
+    base::cfg_write( cfg );
+    cfg.writeEntry( "Type", "MENUENTRY_SHORTCUT_ACTION_DATA" );
     }
 
 
 MenuEntryShortcutActionData::MenuEntryShortcutActionData( 
-        ActionDataGroup* parent_P,
-        const QString& name_P,
-        const QString& comment_P,
-        const KShortcut& shortcut_P,
-        const QString& menuentry_P,
-        bool enabled_P )
+        ActionDataGroup* parent,
+        const QString& name,
+        const QString& comment,
+        const KShortcut& shortcut,
+        const QString& menuentry,
+        bool enabled )
     : Simple_action_data< ShortcutTrigger, MenuEntryAction >(
-        parent_P,
-        name_P,
-        comment_P,
-        enabled_P )
+        parent,
+        name,
+        comment,
+        enabled )
     {
-    set_action( new MenuEntryAction( this, menuentry_P ));
-    set_trigger( new ShortcutTrigger( this, shortcut_P ));
+    set_action( new MenuEntryAction( this, menuentry ));
+    set_trigger( new ShortcutTrigger( this, shortcut ));
     }
 
 
-MenuEntryShortcutActionData::MenuEntryShortcutActionData( ActionDataGroup* parent_P,
-    const QString& name_P, const QString& comment_P, bool enabled_P )
-    : Simple_action_data< ShortcutTrigger, MenuEntryAction >( parent_P, name_P,
-        comment_P, enabled_P )
-    {
-    }
+MenuEntryShortcutActionData::MenuEntryShortcutActionData(
+        ActionDataGroup* parent,
+        const QString& name,
+        const QString& comment,
+        bool enabled)
+    :   Simple_action_data< ShortcutTrigger, MenuEntryAction >(
+            parent,
+            name,
+            comment,
+            enabled)
+    {}
 
 
-MenuEntryShortcutActionData::MenuEntryShortcutActionData( KConfigGroup& cfg_P,
-    ActionDataGroup* parent_P )
-    : Simple_action_data< ShortcutTrigger, MenuEntryAction >( cfg_P, parent_P )
-    {
-    }
+MenuEntryShortcutActionData::MenuEntryShortcutActionData(
+        KConfigGroup& cfg,
+        ActionDataGroup* parent)
+    :   Simple_action_data< ShortcutTrigger, MenuEntryAction >(cfg, parent)
+    {}
 
 
 void MenuEntryShortcutActionData::accept(ActionDataVisitor *visitor) const
     {
     visitor->visitMenuentryShortcutActionData(this);
     }
-
 
 
 } // namespace KHotKeys
--- khotkeys/libkhotkeysprivate/action_data/menuentry_shortcut_action_data.h
+++ khotkeys/libkhotkeysprivate/action_data/menuentry_shortcut_action_data.h
@@ -1,15 +1,23 @@
-/****************************************************************************
-
- KHotKeys
-
- Copyright (C) 1999-2001 Lubos Lunak <l.lunak@kde.org>
-
- Distributed under the terms of the GNU General Public License version 2.
-
-****************************************************************************/
-
 #ifndef MENUENTRY_SHORTCUT_ACTION_DATA_H
 #define MENUENTRY_SHORTCUT_ACTION_DATA_H
+/**
+ * Copyright (C) 1999-2001 Lubos Lunak <l.lunak@kde.org>
+ * Copyright (C) 2009 Michael Jansen <kde@michael-jansen.biz>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include "simple_action_data.h"
 #include "triggers/triggers.h"
@@ -22,18 +30,27 @@ class KDE_EXPORT MenuEntryShortcutActionData
     : public Simple_action_data< ShortcutTrigger, MenuEntryAction >
     {
 
-        typedef Simple_action_data< ShortcutTrigger, MenuEntryAction > base;
+    typedef Simple_action_data< ShortcutTrigger, MenuEntryAction > base;
 
     public:
 
-        MenuEntryShortcutActionData( ActionDataGroup* parent_P, const QString& name_P,
-            const QString& comment_P, bool enabled_P = true );
+        MenuEntryShortcutActionData(
+                ActionDataGroup* parent,
+                const QString& name,
+                const QString& comment,
+                bool enabled = true);
 
-        MenuEntryShortcutActionData( ActionDataGroup* parent_P, const QString& name_P,
-            const QString& comment_P, const KShortcut& shortcut_P, const QString& command_url_P,
-            bool enabled_P = true );
+        MenuEntryShortcutActionData(
+                ActionDataGroup* parent,
+                const QString& name,
+                const QString& comment,
+                const KShortcut& shortcut,
+                const QString& command_url,
+                bool enabled = true);
 
-        MenuEntryShortcutActionData( KConfigGroup& cfg_P, ActionDataGroup* parent_P );
+        MenuEntryShortcutActionData(
+                KConfigGroup& cfg,
+                ActionDataGroup* parent);
 
         /**
          * Visitor pattern
@@ -46,4 +63,5 @@ class KDE_EXPORT MenuEntryShortcutActionData
 
 } // namespace KHotKeys
 
-#endif
+
+#endif /* MENUENTRY_SHORTCUT_ACTION_DATA_H */
