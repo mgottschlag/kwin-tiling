1041c0966c6fa804cee9da7f79b39f4bc2979084
Dario Freddi <drf@kde.org>
2009-03-27 17:45:07 +0000

    Refining buildsystem to allow optional compilation of PolicyKit-KDE
    svn path=/trunk/KDE/kdebase/workspace/; revision=945555

--- cmake/modules/FindPolkitQt.cmake
+++ cmake/modules/FindPolkitQt.cmake
@@ -13,6 +13,8 @@
 #
 # Redistribution and use is allowed according to the terms of the GPLv2+ license.
 
+#include (InstallSettings)
+
 IF (POLKITQT_INCLUDE_DIR AND POLKITQT_LIB)
     SET(POLKITQT_FIND_QUIETLY TRUE)
 ENDIF (POLKITQT_INCLUDE_DIR AND POLKITQT_LIB)
@@ -46,4 +48,18 @@ ELSE (POLKITQT_FOUND)
 ENDIF (POLKITQT_FOUND)
 
 MARK_AS_ADVANCED(POLKITQT_INCLUDE_DIR POLKITQT_LIB)
+
+include (PkgConfigGetVar)
+
+macro(dbus_add_activation_system_service _sources)
+    #PKGCONFIG_GETVAR(dbus-1 session_bus_services_dir _install_dir)
+    foreach (_i ${_sources})
+        get_filename_component(_service_file ${_i} ABSOLUTE)
+        string(REGEX REPLACE "\\.service.*$" ".service" _output_file ${_i})
+        set(_target ${CMAKE_CURRENT_BINARY_DIR}/${_output_file})
+        configure_file(${_service_file} ${_target})
+        install(FILES ${_target} DESTINATION ${SHARE_INSTALL_PREFIX}/dbus-1/system-services )
+        #install(FILES ${_target} DESTINATION ${_install_dir})
+    endforeach (_i ${ARGN})
+endmacro(dbus_add_activation_system_service _sources)
 
