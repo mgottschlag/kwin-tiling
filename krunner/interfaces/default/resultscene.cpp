9a169a0407c577a7a7d1c1ce52b3431efe22eb8d
Aaron J. Seigo <aseigo@kde.org>
2009-03-30 20:10:20 +0000

    only resize the items when the width changes
    svn path=/trunk/KDE/kdebase/workspace/; revision=947052

--- krunner/interfaces/default/resultscene.cpp
+++ krunner/interfaces/default/resultscene.cpp
@@ -78,9 +78,13 @@ QSize ResultScene::minimumSizeHint() const
 
 void ResultScene::resize(int width, int height)
 {
+    bool resizeItems = width != sceneRect().width();
     setSceneRect(0.0, 0.0, (qreal)width, (qreal)height);
-    foreach (ResultItem *item, m_items) {
-        item->calculateSize();
+
+    if (resizeItems) {
+        foreach (ResultItem *item, m_items) {
+            item->calculateSize();
+        }
     }
 }
 
