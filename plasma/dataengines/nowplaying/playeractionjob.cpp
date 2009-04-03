ab63275b38214c69d66f1d0f92003ce6de3ac869
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2009-04-03 07:56:49 +0000

    Typographical and grammar corrections
    svn path=/trunk/KDE/kdebase/workspace/; revision=948566

--- plasma/dataengines/nowplaying/playeractionjob.cpp
+++ plasma/dataengines/nowplaying/playeractionjob.cpp
@@ -25,7 +25,7 @@ void PlayerActionJob::start()
 {
     kDebug() << "Trying to perform the action" << operationName();
     if (!m_player) {
-        setErrorText(i18n("The player '%1' cannot be found", destination()));
+        setErrorText(i18n("The player '%1' cannot be found.", destination()));
         setError(-1);
         emitResult();
         return;
@@ -36,35 +36,35 @@ void PlayerActionJob::start()
         if (m_player->canPlay()) {
             m_player->play();
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'play'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'play'.", m_player->name()));
             setError(-1);
         }
     } else if (operation == "pause") {
         if (m_player->canPause()) {
             m_player->pause();
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'pause'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'pause'.", m_player->name()));
             setError(-1);
         }
     } else if (operation == "stop") {
         if (m_player->canStop()) {
             m_player->stop();
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'stop'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'stop'.", m_player->name()));
             setError(-1);
         }
     } else if (operation == "previous") {
         if (m_player->canGoPrevious()) {
             m_player->previous();
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'previous'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'previous'.", m_player->name()));
             setError(-1);
         }
     } else if (operation == "next") {
         if (m_player->canGoNext()) {
             m_player->next();
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'next'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'next'.", m_player->name()));
             setError(-1);
         }
     } else if (operation == "volume") {
@@ -74,15 +74,15 @@ void PlayerActionJob::start()
                 if (volume >= 0.0 && volume <= 1.0) {
                     m_player->setVolume(volume);
                 } else {
-                    setErrorText(i18n("The 'level' argument to the 'volume' command must be between 0 and 1"));
+                    setErrorText(i18n("The 'level' argument to the 'volume' command must be between 0 and 1."));
                     setError(-2);
                 }
             } else {
-                setErrorText(i18n("The 'volume' command requires a 'level' argument"));
+                setErrorText(i18n("The 'volume' command requires a 'level' argument."));
                 setError(-2);
             }
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'volume'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'volume'.", m_player->name()));
             setError(-1);
         }
     } else if (operation == "seek") {
@@ -93,15 +93,15 @@ void PlayerActionJob::start()
                     m_player->seek(time);
                 } else {
                     setErrorText(i18n("The 'seconds' argument to the 'seek' command must be "
-                                      "between 0 and the length of the track"));
+                                      "between 0 and the length of the track".));
                     setError(-2);
                 }
             } else {
-                setErrorText(i18n("The 'seek' command requires a 'seconds' argument"));
+                setErrorText(i18n("The 'seek' command requires a 'seconds' argument."));
                 setError(-2);
             }
         } else {
-            setErrorText(i18n("The player '%1' cannot perform the action 'seek'", m_player->name()));
+            setErrorText(i18n("The player '%1' cannot perform the action 'seek'.", m_player->name()));
             setError(-1);
         }
     }
