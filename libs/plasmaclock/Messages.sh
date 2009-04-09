05210542da9fc38e0a515a20b0e0dc2d7543fc48
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 11:52:32 +0000

    Updated Messages.sh files to get strings from .ui files too.
    svn path=/trunk/KDE/kdebase/workspace/; revision=951447

--- libs/plasmaclock/Messages.sh
+++ libs/plasmaclock/Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
+$EXTRACTRC *.ui >> rc.cpp
 $XGETTEXT *.cpp -o $podir/libplasmaclock.pot
+rm -f rc.cpp
--- solid/kcm/Messages.sh
+++ solid/kcm/Messages.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/env bash
+$EXTRACTRC *.ui >> rc.cpp
 $XGETTEXT *.cpp -o $podir/kcm_solid.pot
+rm -f rc.cpp
+
