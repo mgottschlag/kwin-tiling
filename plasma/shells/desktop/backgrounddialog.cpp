850d69c3595bd763bd86399edf9f22a3362ec396
Frederik Gladhorn <gladhorn@kde.org>
2009-04-19 09:03:23 +0000

    introduce updateButtons() again. It takes care of enabling/disabling the Apply button and was introduced to KConfigDialog recently. Sebas: you need to update kdelibs :) (revert 954588) CCMAIL: sebas@kde.org
    svn path=/trunk/KDE/kdebase/workspace/; revision=956007

--- plasma/shells/desktop/backgrounddialog.cpp
+++ plasma/shells/desktop/backgrounddialog.cpp
@@ -635,6 +635,7 @@ void BackgroundDialog::saveConfig()
 void BackgroundDialog::settingsModified(bool modified)
 {
     m_modified = modified;
+    updateButtons();
 }
 
 bool BackgroundDialog::hasChanged()
