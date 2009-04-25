47979c091c402c1e1ba13722b17090490a44b04f
Oswald Buddenhagen <ossi@kde.org>
2009-04-25 12:45:16 +0000

    remove superfluous includes
    svn path=/trunk/KDE/kdebase/workspace/; revision=959013

--- libs/kdm/kgreet_classic.cpp
+++ libs/kdm/kgreet_classic.cpp
@@ -23,8 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include "kgreet_classic.h"
-#include "themer/kdmthemer.h"
-#include "themer/kdmitem.h"
 
 #include <kglobal.h>
 #include <klocale.h>
--- libs/kdm/kgreet_winbind.cpp
+++ libs/kdm/kgreet_winbind.cpp
@@ -23,8 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include "kgreet_winbind.h"
-#include "themer/kdmthemer.h"
-#include "themer/kdmitem.h"
 
 #include <klocale.h>
 #include <kglobal.h>
