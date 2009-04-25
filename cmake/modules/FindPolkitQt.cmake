4478d911f79380d29825c230acaa6db1fc858f5c
Dario Freddi <drf@kde.org>
2009-04-25 16:06:43 +0000

    A better cmake module for polkit-qt
    svn path=/trunk/KDE/kdebase/workspace/; revision=959111

--- cmake/modules/FindPolkitQt.cmake
+++ cmake/modules/FindPolkitQt.cmake
@@ -10,56 +10,76 @@
 
 # Copyright (c) 2008, Adrien Bustany, <madcat@mymadcat.com>
 # Copyright (c) 2009, Daniel Nicoletti, <dantti85-pk@yahoo.com.br>
+# Copyright (c) 2009, Dario Freddi, <drf54321@gmail.com>
+# Copyright (c) 2009, Michal Malek, <michalm@jabster.pl>
 #
 # Redistribution and use is allowed according to the terms of the GPLv2+ license.
 
-#include (InstallSettings)
+if (POLKITQT_INCLUDE_DIR AND POLKITQT_LIB)
+    set(POLKITQT_FIND_QUIETLY TRUE)
+endif (POLKITQT_INCLUDE_DIR AND POLKITQT_LIB)
 
-IF (POLKITQT_INCLUDE_DIR AND POLKITQT_LIB)
-    SET(POLKITQT_FIND_QUIETLY TRUE)
-ENDIF (POLKITQT_INCLUDE_DIR AND POLKITQT_LIB)
+include(FindPkgConfig)
 
-# FIND_PATH( POLKITQT_INCLUDE_DIR PolicyKit/policykit-qt/Polkit-qt )
-FIND_PATH( POLKITQT_INCLUDE_DIR PolicyKit/polkit-qt/ )
+if (NOT POLKITQT_MIN_VERSION)
+  set(POLKITQT_MIN_VERSION "0.9.0")
+endif (NOT POLKITQT_MIN_VERSION)
 
-FIND_LIBRARY( POLKITQT_CORE_LIBRARY NAMES polkit-qt-core )
-FIND_LIBRARY( POLKITQT_GUI_LIBRARY NAMES polkit-qt-gui )
+pkg_search_module( POLKITQT polkit-qt )
 
-IF (POLKITQT_INCLUDE_DIR AND POLKITQT_CORE_LIBRARY AND POLKITQT_GUI_LIBRARY)
-   SET(POLKITQT_FOUND TRUE)
-ELSE (POLKITQT_INCLUDE_DIR AND POLKITQT_CORE_LIBRARY AND POLKITQT_GUI_LIBRARY)
-   SET(POLKITQT_FOUND FALSE)
-ENDIF (POLKITQT_INCLUDE_DIR AND POLKITQT_CORE_LIBRARY AND POLKITQT_GUI_LIBRARY)
+if (POLKITQT_FOUND)
+    if (POLKITQT_VERSION VERSION_LESS POLKITQT_MIN_VERSION)
+        message(STATUS "Found Polkit-Qt release < ${POLKITQT_MIN_VERSION}")
+        message(STATUS "You need Polkit-Qt version ${POLKITQT_MIN_VERSION} or newer to compile this component")
+        set(POLKITQT_FOUND FALSE)
+        return()
+    else (POLKITQT_VERSION VERSION_LESS POLKITQT_MIN_VERSION)
+        message(STATUS "Found Polkit-Qt release >= ${POLKITQT_MIN_VERSION}")
+    endif (POLKITQT_VERSION VERSION_LESS POLKITQT_MIN_VERSION)
+else (POLKITQT_FOUND)
+    set(POLKITQT_FOUND FALSE)
+    message(STATUS "Cannot find Polkit-Qt library!")
+    return()
+endif (POLKITQT_FOUND)
 
-SET(POLKITQT_LIBRARIES ${POLKITQT_CORE_LIBRARY} ${POLKITQT_GUI_LIBRARY})
 
-SET(POLKITQT_INCLUDE_DIR ${POLKITQT_INCLUDE_DIR}/PolicyKit/polkit-qt ${POLKITQT_INCLUDE_DIR}/PolicyKit/)
+# find_path( POLKITQT_INCLUDE_DIR PolicyKit/policykit-qt/Polkit-qt )
+find_path( POLKITQT_INCLUDE_DIR PolicyKit/polkit-qt/ )
 
-SET(POLICY_FILES_INSTALL_DIR share/PolicyKit/policy/)
+find_library( POLKITQT_CORE_LIBRARY NAMES polkit-qt-core )
+find_library( POLKITQT_GUI_LIBRARY NAMES polkit-qt-gui )
 
-IF (POLKITQT_FOUND)
-  IF (NOT POLKITQT_FIND_QUIETLY)
-    MESSAGE(STATUS "Found Polkit-qt: ${POLKITQT_LIBRARIES}")
-  ENDIF (NOT POLKITQT_FIND_QUIETLY)
-ELSE (POLKITQT_FOUND)
-  IF (POLKITQT_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could NOT find Polkit-qt")
-  ENDIF (POLKITQT_FIND_REQUIRED)
-ENDIF (POLKITQT_FOUND)
+if (POLKITQT_INCLUDE_DIR AND POLKITQT_CORE_LIBRARY AND POLKITQT_GUI_LIBRARY)
+   set(POLKITQT_FOUND TRUE)
+else (POLKITQT_INCLUDE_DIR AND POLKITQT_CORE_LIBRARY AND POLKITQT_GUI_LIBRARY)
+   set(POLKITQT_FOUND FALSE)
+endif (POLKITQT_INCLUDE_DIR AND POLKITQT_CORE_LIBRARY AND POLKITQT_GUI_LIBRARY)
 
-MARK_AS_ADVANCED(POLKITQT_INCLUDE_DIR POLKITQT_LIB)
+set(POLKITQT_LIBRARIES ${POLKITQT_CORE_LIBRARY} ${POLKITQT_GUI_LIBRARY})
 
-include (PkgConfigGetVar)
+set(POLKITQT_INCLUDE_DIR ${POLKITQT_INCLUDE_DIR}/PolicyKit/polkit-qt ${POLKITQT_INCLUDE_DIR}/PolicyKit/)
+
+set(POLICY_FILES_INSTALL_DIR ${POLKITQT_PREFIX}/share/PolicyKit/policy/)
+
+if (POLKITQT_FOUND)
+  if (NOT POLKITQT_FIND_QUIETLY)
+    message(STATUS "Found Polkit-Qt: ${POLKITQT_LIBRARIES}")
+  endif (NOT POLKITQT_FIND_QUIETLY)
+else (POLKITQT_FOUND)
+  if (POLKITQT_FIND_REQUIRED)
+    message(FATAL_ERROR "Could NOT find Polkit-Qt")
+  endif (POLKITQT_FIND_REQUIRED)
+endif (POLKITQT_FOUND)
+
+mark_as_advanced(POLKITQT_INCLUDE_DIR POLKITQT_LIB)
 
 macro(dbus_add_activation_system_service _sources)
-    #PKGCONFIG_GETVAR(dbus-1 session_bus_services_dir _install_dir)
+    pkg_search_module( DBUS dbus-1 )
     foreach (_i ${_sources})
         get_filename_component(_service_file ${_i} ABSOLUTE)
         string(REGEX REPLACE "\\.service.*$" ".service" _output_file ${_i})
         set(_target ${CMAKE_CURRENT_BINARY_DIR}/${_output_file})
         configure_file(${_service_file} ${_target})
-        install(FILES ${_target} DESTINATION ${SHARE_INSTALL_PREFIX}/dbus-1/system-services )
-        #install(FILES ${_target} DESTINATION ${_install_dir})
+        install(FILES ${_target} DESTINATION ${DBUS_PREFIX}/share/dbus-1/system-services )
     endforeach (_i ${ARGN})
 endmacro(dbus_add_activation_system_service _sources)
-
