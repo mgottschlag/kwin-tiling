a17415b26751f95354646848ad6e76a428e9e880
Laurent Montel <montel@kde.org>
2009-05-07 06:32:02 +0000

    Fix cmake
    svn path=/trunk/KDE/kdebase/workspace/; revision=964648

--- plasma/dataengines/geolocation/cmake/modules/Findlibgps.cmake
+++ plasma/dataengines/geolocation/cmake/modules/Findlibgps.cmake
@@ -4,13 +4,13 @@
 
 if(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
   set(LIBGPS_FIND_QUIETLY TRUE)
+else(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
+
+  find_path(LIBGPS_INCLUDE_DIR NAMES gps.h)
+  find_library(LIBGPS_LIBRARIES NAMES gps)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(libgps DEFAULT_MSG LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
+
+  mark_as_advanced(LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
 endif(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
-
-find_path(LIBGPS_INCLUDE_DIR NAMES gps.h)
-find_library(LIBGPS_LIBRARIES NAMES gps)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libgps DEFAULT_MSG LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
-
-mark_as_advanced(LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
-
