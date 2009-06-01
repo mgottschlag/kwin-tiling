642ad1919ff58545fd2e4917b22fafc731f9e38f
Christoph Feck <christoph@maxiom.de>
2009-06-01 01:08:44 +0000

    SVN_SILENT compile
    svn path=/trunk/KDE/kdebase/workspace/; revision=976133

--- libs/taskmanager/taskmanager.cpp
+++ libs/taskmanager/taskmanager.cpp
@@ -33,7 +33,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <KConfig>
 #include <KConfigGroup>
 #include <KDebug>
-#include <KDirWatch>
+#include <KIO/KDirWatch>
 #include <KGlobal>
 #include <KLocale>
 #include <KStandardDirs>
