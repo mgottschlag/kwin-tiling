e9c12d76121e2009d884a9a07e2abb503356cc93
Christoph Feck <christoph@maxiom.de>
2009-06-01 01:31:36 +0000

    SVN_SILENT oops, I cannot tell link from compile errors...
    svn path=/trunk/KDE/kdebase/workspace/; revision=976137

--- libs/taskmanager/taskmanager.cpp
+++ libs/taskmanager/taskmanager.cpp
@@ -33,7 +33,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <KConfig>
 #include <KConfigGroup>
 #include <KDebug>
-#include <KIO/KDirWatch>
+#include <KDirWatch>
 #include <KGlobal>
 #include <KLocale>
 #include <KStandardDirs>
