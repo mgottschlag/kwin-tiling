bc3b98b94ee025a00be3501341e1b1d32d688cc4
Aaron J. Seigo <aseigo@kde.org>
2009-06-10 16:33:56 +0000

    SVN_SILENT: more code style fixyness
    svn path=/trunk/KDE/kdebase/workspace/; revision=979873

--- plasma/runners/nepomuksearch/queryclientwrapper.cpp
+++ plasma/runners/nepomuksearch/queryclientwrapper.cpp
@@ -44,15 +44,15 @@ Q_DECLARE_METATYPE(Nepomuk::Resource)
 
 static const int s_maxResults = 10;
 
-Nepomuk::QueryClientWrapper::QueryClientWrapper( SearchRunner* runner, Plasma::RunnerContext* context )
+Nepomuk::QueryClientWrapper::QueryClientWrapper(SearchRunner* runner, Plasma::RunnerContext* context)
     : QObject(),
-      m_runner( runner ),
-      m_runnerContext( context )
+      m_runner(runner),
+      m_runnerContext(context)
 {
     // initialize the query client
-    m_queryServiceClient = new Nepomuk::Search::QueryServiceClient( this );
-    connect( m_queryServiceClient, SIGNAL(newEntries( const QList<Nepomuk::Search::Result>& )),
-             this, SLOT(slotNewEntries( const QList<Nepomuk::Search::Result>& )) );
+    m_queryServiceClient = new Nepomuk::Search::QueryServiceClient(this);
+    connect(m_queryServiceClient, SIGNAL(newEntries(const QList<Nepomuk::Search::Result>&)),
+             this, SLOT(slotNewEntries(const QList<Nepomuk::Search::Result>&)));
 }
 
 
@@ -63,59 +63,58 @@ Nepomuk::QueryClientWrapper::~QueryClientWrapper()
 
 void Nepomuk::QueryClientWrapper::runQuery()
 {
-    kDebug() << m_runnerContext->query();
+    //kDebug() << m_runnerContext->query();
 
     // add a timeout in case something goes wrong (no user wants to wait more than 30 seconds)
-    QTimer::singleShot( 30000, m_queryServiceClient, SLOT(close()) );
+    QTimer::singleShot(30000, m_queryServiceClient, SLOT(close()));
 
-    Search::Query q = Search::QueryParser::parseQuery( m_runnerContext->query() );
-    q.setLimit( s_maxResults );
-    m_queryServiceClient->blockingQuery( q );
+    Search::Query q = Search::QueryParser::parseQuery(m_runnerContext->query());
+    q.setLimit(s_maxResults);
+    m_queryServiceClient->blockingQuery(q);
 
-    kDebug() << m_runnerContext->query() << "done";
+    //kDebug() << m_runnerContext->query() << "done";
 }
 
 
 namespace {
-qreal normalizeScore( double score ) {
+qreal normalizeScore(double score) {
     // no search result is ever a perfect match, NEVER. And mostly, when typing a URL
     // the users wants to open that url instead of using the search result. Thus, all
     // search results need to have a lower score than URLs which can drop to 0.5
     // And in the end, for 10 results, the score is not that important at the moment.
     // This can be improved in the future.
     // We go the easy way here and simply cut the score at 0.4
-    return qMin( 0.4, score );
+    return qMin(0.4, score);
 }
 }
 
-void Nepomuk::QueryClientWrapper::slotNewEntries( const QList<Nepomuk::Search::Result>& results )
+void Nepomuk::QueryClientWrapper::slotNewEntries(const QList<Nepomuk::Search::Result>& results)
 {
-    foreach( const Search::Result& result, results ) {
-        Plasma::QueryMatch match( m_runner );
-        match.setType( Plasma::QueryMatch::PossibleMatch );
-        match.setRelevance( normalizeScore( result.score() ) );
+    foreach(const Search::Result& result, results) {
+        Plasma::QueryMatch match(m_runner);
+        match.setType(Plasma::QueryMatch::PossibleMatch);
+        match.setRelevance(normalizeScore(result.score()));
 
-        Nepomuk::Resource res( result.resourceUri() );
+        Nepomuk::Resource res(result.resourceUri());
 
         QString type;
-        if( res.hasType( Soprano::Vocabulary::Xesam::File() ) ||
-            res.resourceUri().scheme() == "file" ) {
-            type = KMimeType::findByUrl( res.resourceUri() )->comment();
-        }
-        else {
-            type = Nepomuk::Types::Class( res.resourceType() ).label();
+        if (res.hasType(Soprano::Vocabulary::Xesam::File()) ||
+            res.resourceUri().scheme() == "file") {
+            type = KMimeType::findByUrl(res.resourceUri())->comment();
+        } else {
+            type = Nepomuk::Types::Class(res.resourceType()).label();
         }
 
-        match.setText( i18nc( "@action file/resource to be opened from KRunner. %1 is the name and %2 the type",
-                              "Open %1 (%2)",
-                              res.genericLabel(),
-                              type ) );
+        match.setText(i18nc("@action file/resource to be opened from KRunner. %1 is the name and %2 the type",
+                            "Open %1 (%2)",
+                            res.genericLabel(),
+                            type));
         QString s = res.genericIcon();
-        match.setIcon( KIcon( s.isEmpty() ? QString("nepomuk") : s ) );
+        match.setIcon(KIcon(s.isEmpty() ? QString("nepomuk") : s));
 
-        match.setData( qVariantFromValue( res ) );
+        match.setData(qVariantFromValue(res));
         match.setId(res.resourceUri().toString());
-        m_runnerContext->addMatch( m_runnerContext->query(), match );
+        m_runnerContext->addMatch(m_runnerContext->query(), match);
     }
 }
 
