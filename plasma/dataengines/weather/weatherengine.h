b7c398cd8e1781b9690fb1d601c7ced98712814c
Shawn Starr <shawn.starr@rogers.com>
2009-06-12 15:58:04 +0000

    krazy2 cleanups, Remove _ from header guards
    svn path=/trunk/KDE/kdebase/workspace/; revision=980839

--- plasma/dataengines/weather/weatherengine.h
+++ plasma/dataengines/weather/weatherengine.h
@@ -17,8 +17,8 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA          *
  ***************************************************************************/
 
-#ifndef _WEATHER_ENGINE_H_
-#define _WEATHER_ENGINE_H_
+#ifndef WEATHERENGINE_H
+#define WEATHERENGINE_H
 
 #include <KService>
 #include <KGenericFactory>
