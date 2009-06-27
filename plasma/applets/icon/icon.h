a91aaed708d50327cd82141226ba2801d69587f4
Aaron J. Seigo <aseigo@kde.org>
2009-06-27 23:04:03 +0000

    backport: preserve the size of icons when dragging between different form factors. bonus points: it saves this state between launches of plasma as well. (forgot to mention in my original commit that this also removes a number of lines of code that were written for kde 4.0 but which have been obsolete for some time now; nice that it all still worked :P)
    CCBUG:179662
    
    svn path=/branches/KDE/4.3/kdebase/workspace/; revision=988371

--- plasma/applets/icon/icon.h
+++ plasma/applets/icon/icon.h
@@ -75,7 +75,8 @@ class IconApplet : public Plasma::Applet
         KPropertiesDialog *m_dialog;
         KMimeType::Ptr m_mimetype;
         KUrl m_url;
-	KDirWatch *m_watchDestopFile;
+        KDirWatch *m_watchDestopFile;
+        QSize m_lastFreeSize;
 };
 
 K_EXPORT_PLASMA_APPLET(icon, IconApplet)
