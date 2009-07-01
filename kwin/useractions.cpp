mgottschlag/kwin-tiling

commit f28186ea3cbbead52537b90b5e9f7a185f131c48

make the context menus in the taskbar and the window manager consistent again; put Advanced at the bottom of the list since it is, well, advanced, implying that the other options are more common BUG:190823
svn path=/trunk/KDE/kdebase/workspace/; revision=959979

kwin/useractions.cpp
@@ -116,9 +116,6 @@ QMenu* Workspace::clientPopup()
         action->setIcon( KIcon( "wizard" ) );
         action->setData( Options::ApplicationRulesOp );
 
-        action = popup->addMenu( advanced_popup );
-        action->setText( i18n("Ad&vanced") );
-
         trans_popup = 0;
         if (compositing()){
             trans_popup = new QMenu( popup );
@@ -169,6 +166,9 @@ QMenu* Workspace::clientPopup()
             mShadeOpAction->setShortcut( kaction->globalShortcut().primary() );
         mShadeOpAction->setCheckable( true );
         mShadeOpAction->setData( Options::ShadeOp );
+
+        action = popup->addMenu( advanced_popup );
+        action->setText( i18n("Ad&vanced") );
 
         popup->addSeparator();
 
@@ -266,7 +266,7 @@ void Workspace::initDesktopPopup()
              this, SLOT( desktopPopupAboutToShow() ) );
 
     QAction *action = desk_popup->menuAction();
-    popup->insertAction(advanced_popup->menuAction(), action);
+    popup->insertAction(mMoveOpAction, action);
     action->setText( i18n("To &Desktop") );
     }
 

libs/taskmanager/taskactions.cpp
@@ -315,7 +315,6 @@ BasicMenu::BasicMenu(QWidget *parent, TaskItem* item, GroupManager *strategy, QL
 
     setTitle(item->name());
     setIcon(item->icon());
-    addMenu(new AdvancedMenu(this, item));
 
     if (TaskManager::self()->numberOfDesktops() > 1) {
         addMenu(new DesktopsMenu(this, item));
@@ -327,6 +326,8 @@ BasicMenu::BasicMenu(QWidget *parent, TaskItem* item, GroupManager *strategy, QL
     addAction(new MinimizeActionImpl(this, item));
     addAction(new MaximizeActionImpl(this, item));
     addAction(new ShadeActionImpl(this, item));
+
+    addMenu(new AdvancedMenu(this, item));
 
     if (strategy->taskGrouper()) {
         QList<QAction*> groupingStrategyActions = strategy->taskGrouper()->strategyActions(this, item);
@@ -363,7 +364,6 @@ BasicMenu::BasicMenu(QWidget *parent, TaskGroup* group, GroupManager *strategy, 
         }
     }
     addSeparator();
-    addMenu(new AdvancedMenu(this, group));
 
     if (TaskManager::self()->numberOfDesktops() > 1) {
         addMenu(new DesktopsMenu(this, group));
@@ -373,6 +373,8 @@ BasicMenu::BasicMenu(QWidget *parent, TaskGroup* group, GroupManager *strategy, 
     addAction(new MinimizeActionImpl(this, group));
     addAction(new MaximizeActionImpl(this, group));
     addAction(new ShadeActionImpl(this, group));
+
+    addMenu(new AdvancedMenu(this, group));
 
     if (strategy->taskGrouper()) {
         QList<QAction*> groupingStrategyActions = strategy->taskGrouper()->strategyActions(this, group);

commit 08e8e663f6b596374e04b383cf444e73cb5d0d61

Backport. Fix change caused by r959979 - the opacity popup should not be before desktop popup.
svn path=/branches/KDE/4.3/kdebase/workspace/; revision=990061

kwin/useractions.cpp
@@ -266,7 +266,8 @@ void Workspace::initDesktopPopup()
              this, SLOT( desktopPopupAboutToShow() ) );
 
     QAction *action = desk_popup->menuAction();
-    popup->insertAction(mMoveOpAction, action);
+    // set it as the first item
+    popup->insertAction( trans_popup ? trans_popup->menuAction() : mMoveOpAction, action);
     action->setText( i18n("To &Desktop") );
     }
 