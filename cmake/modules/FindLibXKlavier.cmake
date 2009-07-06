2564280480634f7f5e15c75428c54f5ba00f26cd
Andriy Rysin <arysin@gmail.com>
2009-07-06 03:28:33 +0000

    fix for libxklavier 4.0 api change using FindPkgConfig
    svn path=/trunk/KDE/kdebase/workspace/; revision=991943

--- cmake/modules/FindLibXKlavier.cmake
+++ cmake/modules/FindLibXKlavier.cmake
@@ -2,49 +2,28 @@
 # Once done this will define
 #
 #  LIBXKLAVIER_FOUND - system has LibXKlavier
-#  LIBXKLAVIER_LIBRARIES - the libraries needed to use LibXKlavier
-#  LIBXKLAVIER_DEFINITIONS - Compiler switches required for using LibXKlavier
+#  LIBXKLAVIER_LDFLAGS - the libraries needed to use LibXKlavier
+#  LIBXKLAVIER_CFLAGS - Compiler switches required for using LibXKlavier
+#  LIBXKLAVIER_VERSION - Version of LibXKlavier
 
-if (LIBXKLAVIER_DEFINITIONS AND LIBXKLAVIER_LIBRARIES)
+if (LIBXKLAVIER_CFLAGS AND LIBXKLAVIER_LDFLAGS)
 
     # in cache already
     SET(LIBXKLAVIER_FOUND TRUE)
-  
-else (LIBXKLAVIER_DEFINITIONS AND LIBXKLAVIER_LIBRARIES)
+
+else (LIBXKLAVIER_CFLAGS AND LIBXKLAVIER_LDFLASG)
 
     IF (NOT WIN32)
         # use pkg-config to get the directories and then use these values
         # in the FIND_PATH() and FIND_LIBRARY() calls
         INCLUDE(UsePkgConfig)
-        PKGCONFIG(libxklavier _LibXKlavierIncDir _LibXKlavierLinkDir _LibXKlavierLinkFlags _LibXKlavierCflags)
 
-	if(_LibXKlavierLinkFlags)
-		# find again pkg-config, to query it about libxklavier version
-		FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config )
-
-		# query pkg-config asking for a libxklavier >= 3.0
-		EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=3.0 libxklavier RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
-		if(_return_VALUE STREQUAL "0")
-        		set(LIBXKLAVIER_DEFINITIONS ${_LibXKlavierCflags})
-		endif(_return_VALUE STREQUAL "0")
-	endif(_LibXKlavierLinkFlags)
-
+	pkg_check_modules(LIBXKLAVIER libxklavier>=3.0)
     ENDIF (NOT WIN32)
 
-#    FIND_PATH(LIBXKLAVIER_INCLUDE_DIR libxklavier/xklavier.h
-#      PATHS
-#      ${_LibXKlavierIncDir}
-#      PATH_SUFFIXES libxklavier
-#    )
-
-    FIND_LIBRARY(LIBXKLAVIER_LIBRARIES NAMES xklavier libxklavier
-      PATHS
-      ${_LibXKlavierLinkDir}
-    )
-
     include(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibXKlavier DEFAULT_MSG LIBXKLAVIER_DEFINITIONS LIBXKLAVIER_LIBRARIES )
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibXKlavier DEFAULT_MSG LIBXKLAVIER_CFLAGS LIBXKLAVIER_LDFLAGS)
 
-    MARK_AS_ADVANCED(LIBXKLAVIER_DEFINITIONS LIBXKLAVIER_LIBRARIES)
+    MARK_AS_ADVANCED(LIBXKLAVIER_CFLAGS LIBXKLAVIER_LDFLAGS LIBXKLAVIER_VERSION)
 
-endif (LIBXKLAVIER_DEFINITIONS AND LIBXKLAVIER_LIBRARIES)
+endif (LIBXKLAVIER_CFLAGS AND LIBXKLAVIER_LDFLAGS)
