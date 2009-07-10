b4bf3d7818d36d3a7eac2061005806110fa6a803
Burkhard Lück <lueck@hube-lueck.de>
2009-07-10 19:41:04 +0000

    revert the changes
    svn path=/trunk/KDE/kdebase/workspace/; revision=994585

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,5 +1,2 @@
 #! /usr/bin/env bash
-sed -e 's/\([A-Za-z].*\)/i18nc("weather condition", "\1");/' ions/data/bbcukmet_i18n.dat >>rc.cpp
-sed -e 's/\([A-Za-z].*\)/i18nc("weather condition", "\1");/' ions/data/envcan_i18n.dat >>rc.cpp
-sed -e 's/\([A-Za-z].*\)/i18nc("weather condition", "\1");/' ions/data/noaa_i18n.dat >>rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
