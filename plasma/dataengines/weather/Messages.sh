fe144378c8fc7d4d234884c5547c3b0247d1a507
Shawn Starr <shawn.starr@rogers.com>
2009-07-10 21:32:52 +0000

    prepare script but keep it commented for i18n folks need to verify
    svn path=/trunk/KDE/kdebase/workspace/; revision=994618

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,2 +1,19 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
+#grep "weather condition" envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "weather forecast" envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "wind direction" envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "wind speed" envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+
+#grep "weather condition" noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "weather forecast" noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "wind direction" noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "wind speed" envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+
+
+#grep "weather condition" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "weather forecast" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "wind direcption" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+#grep "wind speed" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+
+
+#$XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
