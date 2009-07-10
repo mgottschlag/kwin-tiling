0706b8233120be0716c43b322c5093b704e3d710
Shawn Starr <shawn.starr@rogers.com>
2009-07-10 21:34:07 +0000

    Remove extra line. bbc has no 'calm' string
    svn path=/trunk/KDE/kdebase/workspace/; revision=994619

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -9,11 +9,9 @@
 #grep "wind direction" noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 #grep "wind speed" envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 
-
 #grep "weather condition" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 #grep "weather forecast" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 #grep "wind direcption" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-#grep "wind speed" bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 
 
 #$XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
