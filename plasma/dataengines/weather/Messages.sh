04e0fb1049990c0dad6b1c8a34d9b18f5c2453d3
Burkhard Lück <lueck@hube-lueck.de>
2009-07-11 14:53:10 +0000

    i18n fix: add strings to make the weather applet translatable CCMAIL:kde-i18n-doc@kde.org
    svn path=/branches/KDE/4.3/kdebase/workspace/; revision=994846

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,2 +1,17 @@
 #! /usr/bin/env bash
+grep "weather condition" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "weather forecast" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "wind direction" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "wind speed" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+
+grep "weather condition" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "weather forecast" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "wind direction" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "wind speed" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+
+grep "weather condition" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "weather forecast" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+grep "wind direction" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+
+
 $XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
