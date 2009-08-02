0835a6e9d2081e04fb4cc4d2d75bfdebd83f6110
Marco Martin <notmart@gmail.com>
2009-08-02 12:09:45 +0000

    use the same borders as in newspaper
    svn path=/trunk/playground/base/plasma/netbook/containments/; revision=1005858

--- plasma/netbook/containments/sal/sal.cpp
+++ plasma/netbook/containments/sal/sal.cpp
@@ -38,6 +38,7 @@
 #include <Plasma/RunnerManager>
 #include <Plasma/QueryMatch>
 #include <Plasma/ScrollWidget>
+#include <Plasma/SvgWidget>
 
 
 SearchLaunch::SearchLaunch(QObject *parent, const QVariantList &args)
@@ -75,6 +76,21 @@ void SearchLaunch::init()
     m_relayoutTimer = new QTimer(this);
     m_relayoutTimer->setSingleShot(true);
     connect(m_relayoutTimer, SIGNAL(timeout()), this, SLOT(relayout()));
+
+    Plasma::Svg *borderSvg = new Plasma::Svg(this);
+    borderSvg->setImagePath("newspaper/border");
+
+    m_topBorder = new Plasma::SvgWidget(this);
+    m_topBorder->setSvg(borderSvg);
+    m_topBorder->setElementID("top");
+    m_topBorder->resize(m_topBorder->effectiveSizeHint(Qt::PreferredSize));
+    m_topBorder->setZValue(900);
+
+    m_bottomBorder = new Plasma::SvgWidget(this);
+    m_bottomBorder->setSvg(borderSvg);
+    m_bottomBorder->setElementID("bottom");
+    m_bottomBorder->resize(m_bottomBorder->effectiveSizeHint(Qt::PreferredSize));
+    m_bottomBorder->setZValue(900);
 
     m_background = new Plasma::FrameSvg(this);
     m_background->setImagePath("widgets/translucentbackground");
@@ -248,6 +264,8 @@ void SearchLaunch::relayout()
     }
     m_viewMainWidget->resize(0,0);
 
+    m_topBorder->setVisible(m_viewMainWidget->size().height() > m_gridScroll->size().height());
+    m_bottomBorder->setVisible(m_topBorder->isVisible());
 }
 
 void SearchLaunch::launch()
@@ -403,6 +421,15 @@ void SearchLaunch::constraintsEvent(Plasma::Constraints constraints)
 
     if (constraints & Plasma::SizeConstraint) {
         m_relayoutTimer->start();
+
+        if (m_gridScroll) {
+            //FIXME: remove the hardcoded 2
+            m_topBorder->resize(m_gridScroll->size().width(), m_topBorder->size().height());
+            m_topBorder->setPos(m_gridScroll->pos() + QPoint(0, 2));
+
+            m_bottomBorder->resize(m_gridScroll->size().width(), m_bottomBorder->size().height());
+            m_bottomBorder->setPos(m_gridScroll->geometry().bottomLeft() - QPoint(0,  m_bottomBorder->size().height() + 2));
+        }
     }
 
 }
