24e693d9a055eedd9bbdd96a43d0d76b19c60264
Petri Damstén <petri.damsten@kdemail.net>
2009-08-14 07:50:46 +0000

    Usage types are translated. BUG: 192970
    svn path=/trunk/KDE/kdebase/workspace/; revision=1011262

--- plasma/applets/system-monitor/hdd.cpp
+++ plasma/applets/system-monitor/hdd.cpp
@@ -260,8 +260,8 @@ bool Hdd::isValidDevice(const QString& uuid, Plasma::DataEngine::Data* data)
                             data->value("File System Type").toString() <<
                             data->value("Size").toString();
         */
-        if ((data->value("Usage").toString() != "File System" &&
-             data->value("Usage").toString() != "Raid") ||
+        if ((data->value("Usage").toString() != i18n("File System") &&
+             data->value("Usage").toString() != i18n("Raid")) ||
             data->value("File System Type").toString() == "swap") {
             QStringList list = items();
             list.removeAll(uuid);
