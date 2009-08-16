51471ebf07b5636369d31669cd893838a9d93027
Karl Ove Hufthammer <karl@huftis.org>
2009-08-16 20:25:48 +0000

    Write information about which weather provider each string comes from, and greatly simplify and generalise the extraction code.
    svn path=/branches/KDE/4.3/kdebase/workspace/; revision=1012114

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,20 +1,11 @@
 #! /usr/bin/env bash
-grep "weather condition" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "weather forecast" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind direction" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind speed" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "pressure tendency" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+rm -f rc.cpp
+for file in ions/data/*.dat
+do
+  grep '|' $file | awk -F'|' '{ print "// i18n: file: '`basename $file`'"; printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+done
 
-grep "weather condition" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "weather forecast" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind direction" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind speed" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 
-grep "weather condition" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "weather forecast" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind direction" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "pressure tendency" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "visibility" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 
 
 
