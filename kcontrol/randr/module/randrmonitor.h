3dd4a79f2cdecf6acb43b457976412fcfa141839
Luboš Luňák <l.lunak@kde.org>
2009-08-17 17:29:15 +0000

    This is not ksplashx.
    svn path=/branches/work/~seli/randr43/; revision=1012455

--- kcontrol/randr/module/randrmonitor.h
+++ kcontrol/randr/module/randrmonitor.h
@@ -2,8 +2,6 @@
 
 Copyright (C) 2008 Lubos Lunak <l.lunak@suse.cz>
 
-Please see file LICENSE for the licensing terms of ksplashx as a whole.
-
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
