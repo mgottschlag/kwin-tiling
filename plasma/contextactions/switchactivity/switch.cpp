cdf100a9e62fd90bddcf4464f7c169c13ec89823
Chani Armitage <chanika@gmail.com>
2009-08-17 22:34:43 +0000

    Switch Activity plugin for button or wheel
    there's also an unfinished zoom plugin that got put on hold until we
    figure out what's happening to plasma zoom.
    not sure I should include it but whatever
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=1012658

---
+++ plasma/contextactions/switchactivity/switch.cpp
@@ -0,0 +1,143 @@
+/*
+ *   Copyright 2009 by Chani Armitage <chani@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "switch.h"
+
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsSceneWheelEvent>
+
+#include <KDebug>
+#include <KMenu>
+#include <KWindowSystem>
+
+#include <Plasma/Containment>
+#include <Plasma/Corona>
+
+Q_DECLARE_METATYPE(QPointer<Plasma::Containment>)
+
+SwitchActivity::SwitchActivity(QObject *parent, const QVariantList &args)
+    : Plasma::ContextAction(parent, args)
+{
+}
+
+void SwitchActivity::contextEvent(QEvent *event)
+{
+    switch (event->type()) {
+        case QEvent::GraphicsSceneMouseRelease:
+            contextEvent(dynamic_cast<QGraphicsSceneMouseEvent*>(event));
+            break;
+        case QEvent::GraphicsSceneWheel:
+            wheelEvent(dynamic_cast<QGraphicsSceneWheelEvent*>(event));
+            break;
+        default:
+            break;
+    }
+}
+
+void SwitchActivity::contextEvent(QGraphicsSceneMouseEvent *event)
+{
+    Plasma::Containment *myCtmt = containment();
+    if (!myCtmt) {
+        return;
+    }
+    Plasma::Corona *c = myCtmt->corona();
+    if (!c) {
+        return;
+    }
+
+    KMenu desktopMenu;
+
+    desktopMenu.addTitle(i18n("Activities"));
+
+    QList<Plasma::Containment*> containments = c->containments();
+    foreach (Plasma::Containment *ctmt, containments) {
+        if (ctmt->containmentType() == Plasma::Containment::PanelContainment ||
+                ctmt->containmentType() == Plasma::Containment::CustomPanelContainment ||
+                c->offscreenWidgets().contains(ctmt)) {
+            continue;
+        }
+
+        QString name = ctmt->activity();
+        if (name.isEmpty()) {
+            name = ctmt->name();
+        }
+        QAction *action = desktopMenu.addAction(name);
+        action->setData(QVariant::fromValue<QPointer<Plasma::Containment> >(QPointer<Plasma::Containment>(ctmt)));
+
+        //WARNING this assumes the plugin will only ever be set on activities, not panels!
+        if (ctmt==myCtmt) {
+            action->setEnabled(false);
+        }
+    }
+
+    connect(&desktopMenu, SIGNAL(triggered(QAction*)), this, SLOT(switchTo(QAction*)));
+    desktopMenu.exec(event->screenPos());
+}
+
+void SwitchActivity::switchTo(QAction *action)
+{
+    QPointer<Plasma::Containment> ctmt = action->data().value<QPointer<Plasma::Containment> >();
+    if (!ctmt) {
+        return;
+    }
+    Plasma::Containment *myCtmt = containment();
+    if (!myCtmt) {
+        return;
+    }
+
+    ctmt->setScreen(myCtmt->screen(), myCtmt->desktop());
+}
+
+void SwitchActivity::wheelEvent(QGraphicsSceneWheelEvent *event)
+{
+    Plasma::Containment *myCtmt = containment();
+    if (!myCtmt) {
+        return;
+    }
+    Plasma::Corona *c = myCtmt->corona();
+    if (!c) {
+        return;
+    }
+
+    QList<Plasma::Containment*> containments = c->containments();
+    int start = containments.indexOf(myCtmt);
+    int step = (event->delta() < 0) ? 1 : -1;
+    int i = (start + step + containments.size()) % containments.size();
+
+    //FIXME we *really* need a proper way to cycle through activities
+    while (i != start) {
+        Plasma::Containment *ctmt = containments.at(i);
+        if (ctmt->containmentType() == Plasma::Containment::PanelContainment ||
+                ctmt->containmentType() == Plasma::Containment::CustomPanelContainment ||
+                c->offscreenWidgets().contains(ctmt)) {
+            //keep looking
+            i = (i + step + containments.size()) % containments.size();
+        } else {
+            break;
+        }
+    }
+
+    Plasma::Containment *ctmt = containments.at(i);
+    if (ctmt && ctmt != myCtmt) {
+        ctmt->setScreen(myCtmt->screen(), myCtmt->desktop());
+    }
+}
+
+
+#include "switch.moc"
---
+++ plasma/contextactions/zoom/zoom.h
@@ -0,0 +1,44 @@
+/*
+ *   Copyright 2009 by Chani Armitage <chani@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef ZOOM_HEADER
+#define ZOOM_HEADER
+
+#include <plasma/contextaction.h>
+
+class QAction;
+
+class Zoom : public Plasma::ContextAction
+{
+    Q_OBJECT
+    public:
+        Zoom(QObject* parent, const QVariantList& args);
+
+        void contextEvent(QEvent *event);
+        void contextEvent(QGraphicsSceneMouseEvent *event);
+        void wheelEvent(QGraphicsSceneWheelEvent *event);
+        QList<QAction*> contextualActions();
+
+    public slots:
+        void switchTo(QAction *action);
+};
+
+K_EXPORT_PLASMA_CONTEXTACTION(zoom, Zoom)
+
+#endif
