7981fddf6820f8eea9945ad8df37fd2bf917214b
Chani Armitage <chanika@gmail.com>
2009-08-17 22:34:12 +0000

    A complete plugin for switching virtual desktops
    holy crap that was easy. :)
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=1012652

---
+++ plasma/contextactions/switchdesktop/desktop.cpp
@@ -0,0 +1,75 @@
+/*
+ *   Copyright 2009 by Chani Armitage <chani@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "desktop.h"
+
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsSceneWheelEvent>
+
+#include <KDebug>
+#include <KMenu>
+#include <KWindowSystem>
+
+SwitchDesktop::SwitchDesktop(QObject *parent, const QVariantList &args)
+    : Plasma::ContextAction(parent, args)
+{
+}
+
+void SwitchDesktop::contextEvent(QGraphicsSceneMouseEvent *event)
+{
+    int numDesktops = KWindowSystem::numberOfDesktops();
+    int currentDesktop = KWindowSystem::currentDesktop();
+    KMenu desktopMenu;
+
+    desktopMenu.addTitle(i18n("Virtual Desktops"));
+
+    for (int i=1; i<=numDesktops; ++i) {
+        QString name = KWindowSystem::desktopName(i);
+        QAction *action = desktopMenu.addAction(QString("%1: %2").arg(i).arg(name));
+        action->setData(i);
+        if (i==currentDesktop) {
+            action->setEnabled(false);
+        }
+    }
+
+    connect(&desktopMenu, SIGNAL(triggered(QAction*)), this, SLOT(switchTo(QAction*)));
+    desktopMenu.exec(event->screenPos());
+}
+
+void SwitchDesktop::switchTo(QAction *action)
+{
+    int desktop = action->data().toInt();
+    KWindowSystem::setCurrentDesktop(desktop);
+}
+
+void SwitchDesktop::wheelEvent(QGraphicsSceneWheelEvent *event)
+{
+    kDebug() << event->orientation() << event->delta();
+    int numDesktops = KWindowSystem::numberOfDesktops();
+    int currentDesktop = KWindowSystem::currentDesktop();
+
+    if (event->delta() < 0) {
+        KWindowSystem::setCurrentDesktop(currentDesktop % numDesktops + 1);
+    } else {
+        KWindowSystem::setCurrentDesktop((numDesktops + currentDesktop - 2) % numDesktops + 1);
+    }
+}
+
+
+#include "desktop.moc"
---
+++ plasma/contextactions/switchdesktop/desktop.h
@@ -0,0 +1,42 @@
+/*
+ *   Copyright 2009 by Chani Armitage <chani@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SWITCHDESKTOP_HEADER
+#define SWITCHDESKTOP_HEADER
+
+#include <plasma/contextaction.h>
+
+class QAction;
+
+class SwitchDesktop : public Plasma::ContextAction
+{
+    Q_OBJECT
+    public:
+        SwitchDesktop(QObject* parent, const QVariantList& args);
+
+        void contextEvent(QGraphicsSceneMouseEvent *event);
+        void wheelEvent(QGraphicsSceneWheelEvent *event);
+
+    public slots:
+        void switchTo(QAction *action);
+};
+
+K_EXPORT_PLASMA_CONTEXTACTION(switchdesktop, SwitchDesktop)
+
+#endif
