bd29a1c0362aa9ec4ff2b09732c9ba0312d10350
Chani Armitage <chanika@gmail.com>
2009-08-17 22:35:10 +0000

    A kmenu plugin and a dataengine to support it
    svn path=/trunk/KDE/kdebase/workspace/; revision=1012663

---
+++ plasma/contextactions/applauncher/launch.cpp
@@ -0,0 +1,126 @@
+/*
+ *   Copyright 2009 by Chani Armitage <chani@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "launch.h"
+
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsSceneWheelEvent>
+
+#include <KDebug>
+#include <KIcon>
+#include <KMenu>
+
+#include <Plasma/DataEngine>
+#include <Plasma/Containment>
+#include <Plasma/Service>
+
+AppLauncher::AppLauncher(QObject *parent, const QVariantList &args)
+    : Plasma::ContextAction(parent, args)
+{
+}
+
+void AppLauncher::init(const KConfigGroup &config)
+{
+}
+
+void AppLauncher::contextEvent(QEvent *event)
+{
+    QPoint screenPos;
+    switch (event->type()) {
+        case QEvent::GraphicsSceneMouseRelease:
+            screenPos = (dynamic_cast<QGraphicsSceneMouseEvent*>(event))->screenPos();
+            break;
+        case QEvent::GraphicsSceneWheel:
+            screenPos = (dynamic_cast<QGraphicsSceneWheelEvent*>(event))->screenPos();
+            break;
+        default:
+            kDebug() << "unexpected event type" << event->type();
+            return;
+    }
+
+
+    Plasma::DataEngine *apps = dataEngine("apps");
+    if (! apps->isValid()) {
+        return;
+    }
+
+    KMenu desktopMenu;
+
+    //add the whole kmenu
+    Plasma::DataEngine::Data app = dataEngine("apps")->query("/");
+    QStringList sources = app.value("entries").toStringList();
+    foreach (const QString &source, sources) {
+        addApp(&desktopMenu, source);
+    }
+
+    connect(&desktopMenu, SIGNAL(triggered(QAction*)), this, SLOT(switchTo(QAction*)));
+    desktopMenu.exec(screenPos);
+}
+
+bool AppLauncher::addApp(QMenu *menu, const QString &source)
+{
+    if (source == "---") {
+        menu->addSeparator();
+        return false;
+    }
+
+    Plasma::DataEngine::Data app = dataEngine("apps")->query(source);
+
+    if (!app.value("display").toBool()) {
+        kDebug() << "hidden entry" << source;
+        return false;
+    }
+    QString name = app.value("name").toString();
+    if (name.isEmpty()) {
+        kDebug() << "failed source" << source;
+        return false;
+    }
+
+    name.replace("&", "&&"); //escaping
+    KIcon icon(app.value("iconName").toString());
+
+    if (app.value("isApp").toBool()) {
+        QAction *action = menu->addAction(icon, name);
+        action->setData(source);
+    } else { //ooh, it's actually a group!
+        QMenu *subMenu = menu->addMenu(icon, name);
+        bool hasEntries = false;
+        foreach (const QString &source, app.value("entries").toStringList()) {
+            hasEntries = addApp(subMenu, source) || hasEntries;
+        }
+
+        if (!hasEntries) {
+            delete subMenu;
+            return false;
+        }
+    }
+    return true;
+}
+
+void AppLauncher::switchTo(QAction *action)
+{
+    QString source = action->data().toString();
+    kDebug() << source;
+    Plasma::Service *service = dataEngine("apps")->serviceForSource(source);
+    if (service) {
+        service->startOperationCall(service->operationDescription("launch"));
+    }
+}
+
+#include "launch.moc"
---
+++ plasma/dataengines/apps/appsengine.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2009 Chani Armitage <chani@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "appsengine.h"
+#include "appsource.h"
+
+AppsEngine::AppsEngine(QObject *parent, const QVariantList &args) :
+    Plasma::DataEngine(parent, args)
+{
+    Q_UNUSED(args);
+}
+
+AppsEngine::~AppsEngine()
+{
+}
+
+Plasma::Service *AppsEngine::serviceForSource(const QString &name)
+{
+    AppSource *source = dynamic_cast<AppSource*>(containerForSource(name));
+    // if source does not exist, return null service
+    if (!source) {
+        return Plasma::DataEngine::serviceForSource(name);
+    }
+    // if source represents a group or something, return null service
+    if (!source->isApp()) {
+        return Plasma::DataEngine::serviceForSource(name);
+    }
+    // if source represent a proper app, return real service
+    Plasma::Service *service = source->createService();
+    service->setParent(this);
+    return service;
+}
+
+void AppsEngine::init()
+{
+    addGroup(KServiceGroup::root());
+    //TODO find a way to listen for changes
+}
+
+void AppsEngine::addGroup(KServiceGroup::Ptr group)
+{
+    if (!(group && group->isValid())) {
+        return;
+    }
+    AppSource *appSource = new AppSource(group, this);
+    //TODO listen for changes
+    addSource(appSource);
+    //do children
+    foreach (const KServiceGroup::Ptr subGroup, group->groupEntries(KServiceGroup::NoOptions)) {
+        addGroup(subGroup);
+    }
+    foreach (const KService::Ptr app, group->serviceEntries(KServiceGroup::NoOptions)) {
+        addApp(app);
+    }
+}
+
+void AppsEngine::addApp(KService::Ptr app)
+{
+    AppSource *appSource = new AppSource(app, this);
+    //TODO listen for changes
+    addSource(appSource);
+}
+
+K_EXPORT_PLASMA_DATAENGINE(apps, AppsEngine)
+
+#include "appsengine.moc"
