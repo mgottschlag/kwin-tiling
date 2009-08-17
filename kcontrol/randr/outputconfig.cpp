025db2b2eb49dd9016843766f0e82188aab5e08c
Luboš Luňák <l.lunak@kde.org>
2009-08-17 18:20:27 +0000

    revert r883822 - I got it to work
    svn path=/branches/work/~seli/randr43/; revision=1012501

--- kcontrol/randr/outputconfig.cpp
+++ kcontrol/randr/outputconfig.cpp
@@ -338,13 +338,6 @@ void OutputConfig::updatePositionListDelayed()
 		if(index != -1)
 			positionOutputCombo->setCurrentIndex(index);
 	}*/
-
-	// FIXME: RandRScreen::applyProposed() has a bit of code for positioning
-	// outputs, but it's commented out and might not work as is. Until someone
-	// gets this to work, it's probably better not to show these controls.
-	positionLabel->setVisible(false);
-	positionCombo->setVisible(false);
-	positionOutputCombo->setVisible(false);
 }
 
 void OutputConfig::updateRotationList(void)
