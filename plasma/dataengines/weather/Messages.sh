08f6b42881d63c0f7442fbb880c80e38be662afb
Karl Ove Hufthammer <karl@huftis.org>
2009-08-17 08:03:13 +0000

    Updated Messages.sh from stable branch.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1012233

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,22 +1,7 @@
 #! /usr/bin/env bash
-grep "weather condition" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "weather forecast" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind direction" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind speed" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "pressure tendency" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-
-grep "weather condition" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "weather forecast" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind direction" ions/data/noaa_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind speed" ions/data/envcan_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-
-grep "weather condition" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "weather forecast" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "wind direction" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "pressure tendency" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-grep "visibility" ions/data/bbcukmet_i18n.dat | awk -F'|' '{ printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
-
-
-
+for file in ions/data/*.dat
+do
+  grep '|' $file | awk -F'|' '{ print "// i18n: file: '`basename $file`'"; printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+done
 
 $XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
