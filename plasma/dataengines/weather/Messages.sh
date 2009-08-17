10af2744c3c3f234525049034e2e5d82f05a6984
Karl Ove Hufthammer <karl@huftis.org>
2009-08-17 08:01:42 +0000

    Removed file removal and empty lines.
    svn path=/branches/KDE/4.3/kdebase/workspace/; revision=1012232

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,13 +1,7 @@
 #! /usr/bin/env bash
-rm -f rc.cpp
 for file in ions/data/*.dat
 do
   grep '|' $file | awk -F'|' '{ print "// i18n: file: '`basename $file`'"; printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
 done
-
-
-
-
-
 
 $XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
