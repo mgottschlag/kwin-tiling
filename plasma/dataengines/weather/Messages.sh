9d5d0a67b9fc9cbabf5930e0477085a9394f9a83
Karl Ove Hufthammer <karl@huftis.org>
2009-08-18 11:48:48 +0000

    Add line numbers to source references.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1012918

--- plasma/dataengines/weather/Messages.sh
+++ plasma/dataengines/weather/Messages.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 for file in ions/data/*.dat
 do
-  grep '|' $file | awk -F'|' '{ print "// i18n: file: '`basename $file`'"; printf("i18nc(\"%s\", \"%s\");\n", $1, $2) }' >> rc.cpp
+  awk -F'|' '$0 ~ /\|/ {
+                         print "// i18n: file: '`basename $file`':"NR;
+                         printf("i18nc(\"%s\", \"%s\");\n", $1, $2)
+                       }' $file >> rc.cpp
 done
 
 $XGETTEXT `find . -name \*.cpp` -o $podir/plasma_engine_weather.pot
