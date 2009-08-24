7b8edbe8105dd3ef48363817f8412deb21b9bdbb
Aaron J. Seigo <aseigo@kde.org>
2009-08-24 12:51:57 +0000

    better guard :)
    svn path=/trunk/KDE/kdebase/workspace/; revision=1015080

--- plasma/runners/plasma-desktop/plasma-desktop-runner.h
+++ plasma/runners/plasma-desktop/plasma-desktop-runner.h
@@ -16,8 +16,8 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef SHELLRUNNER_H
-#define SHELLRUNNER_H
+#ifndef PLASMADESKTOPRUNNER_H
+#define PLASMADESKTOPRUNNER_H
 
 #include <Plasma/AbstractRunner>
 
