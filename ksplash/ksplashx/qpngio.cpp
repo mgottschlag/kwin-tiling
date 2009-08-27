f5c16a4c2f662c4716974bd25f31b514d93d80b7
Bernhard Rosenkraenzer <bero@arklinux.org>
2009-08-27 13:56:43 +0000

    Compile fix for libpng 1.4 betas
    svn path=/trunk/KDE/kdebase/workspace/; revision=1016309

--- ksplash/ksplashx/qpngio.cpp
+++ ksplash/ksplashx/qpngio.cpp
@@ -183,7 +183,11 @@ void setup_qt( QImage& image, png_structp png_ptr, png_infop info_ptr, float scr
 		    info_ptr->palette[i].red,
 		    info_ptr->palette[i].green,
 		    info_ptr->palette[i].blue,
+#if PNG_LIBPNG_VER < 10400
 		    info_ptr->trans[i]
+#else
+		    info_ptr->trans_alpha[i]
+#endif
 		    )
 		);
 		i++;
