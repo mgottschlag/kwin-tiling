2ee6c9277bda5f3efd7dc222549e233eef79b666
Aaron J. Seigo <aseigo@kde.org>
2009-08-28 16:43:39 +0000

    use the app's corona, don't rely on the containment being in a good state BUG:205504
    svn path=/trunk/KDE/kdebase/workspace/; revision=1016719

--- plasma/shells/desktop/desktopview.cpp
+++ plasma/shells/desktop/desktopview.cpp
@@ -90,7 +90,7 @@ DesktopView::DesktopView(Plasma::Containment *containment, int id, QWidget *pare
     if (containment) {
         containment->enableAction("zoom in", false);
         containment->enableAction("add sibling containment", false);
-        connect(containment->corona(), SIGNAL(containmentAdded(Plasma::Containment *)), this, SLOT(containmentAdded(Plasma::Containment *)));
+        connect(PlasmaApp::self()->corona(), SIGNAL(containmentAdded(Plasma::Containment *)), this, SLOT(containmentAdded(Plasma::Containment *)));
     }
 
     m_actions->addAssociatedWidget(this);
@@ -214,7 +214,7 @@ Plasma::Containment *DesktopView::dashboardContainment() const
     int containmentId = cg.readEntry("DashboardContainment", 0);
 
     if (containmentId > 0) {
-        foreach (Plasma::Containment *c, containment()->corona()->containments()) {
+        foreach (Plasma::Containment *c, PlasmaApp::self()->corona()->containments()) {
             if ((int)c->id() == containmentId) {
                 dc = c;
                 break;
@@ -430,7 +430,7 @@ void DesktopView::zoomIn(Plasma::ZoomLevel zoomLevel)
         scale(factor, factor);
         if (containment()) {
             //disconnect from other containments
-            Plasma::Corona *corona = containment()->corona();
+            Plasma::Corona *corona = PlasmaApp::self()->corona();
             if (corona) {
                 QList<Plasma::Containment*> containments = corona->containments();
                 foreach (Plasma::Containment *c, containments) {
@@ -552,7 +552,7 @@ void DesktopView::screenOwnerChanged(int wasScreen, int isScreen, Plasma::Contai
 
 void DesktopView::nextContainment()
 {
-    QList<Plasma::Containment*> containments = containment()->corona()->containments();
+    QList<Plasma::Containment*> containments = PlasmaApp::self()->corona()->containments();
     int start = containments.indexOf(containment());
     int i = (start + 1) % containments.size();
     //FIXME this is a *horrible* way of choosing a "next" containment.
@@ -571,7 +571,7 @@ void DesktopView::nextContainment()
 
 void DesktopView::previousContainment()
 {
-    QList<Plasma::Containment*> containments = containment()->corona()->containments();
+    QList<Plasma::Containment*> containments = PlasmaApp::self()->corona()->containments();
     int start = containments.indexOf(containment());
     //fun fact: in c++, (-1 % foo) == -1
     int i = start - 1;
