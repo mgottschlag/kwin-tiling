eee25d5b9109be68da4cad7b79b1cabbbc8f4962
Ivan Čukić <ivan.cukic@kde.org>
2009-09-01 16:45:22 +0000

    adding forgotten files
    svn path=/trunk/KDE/kdebase/workspace/; revision=1018392

---
+++ plasma/applets/kickoff/core/krunnermodel.cpp
@@ -0,0 +1,211 @@
+/*
+    Copyright 2009 Ivan Cukic <ivan.cukic+kde@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+// Own
+#include "core/krunnermodel.h"
+
+// Qt
+#include <QBasicTimer>
+#include <QDebug>
+#include <QList>
+#include <QMimeData>
+#include <QString>
+#include <QTimerEvent>
+
+// KDE
+#include <Plasma/AbstractRunner>
+#include <Plasma/RunnerManager>
+
+#define DELAY_TIME 500
+
+using namespace Kickoff;
+
+Plasma::RunnerManager * _runnerManager = NULL;
+Plasma::RunnerManager * runnerManager() {
+    if (_runnerManager == NULL) {
+        _runnerManager = new Plasma::RunnerManager();
+    }
+    return _runnerManager;
+}
+
+class ResultItem {
+public:
+    ResultItem()
+    {
+    }
+
+    ResultItem(QIcon _icon, QString _name, QString _description, QVariant _data)
+        : icon(_icon),
+          name(_name),
+          description(_description),
+          data(_data)
+    {
+    }
+
+    QIcon icon;
+    QString name, description;
+    QVariant data;
+};
+
+bool KRunnerItemHandler::openUrl(const KUrl& url)
+{
+    QString runner = url.host();
+    QString id = url.path();
+    if (id.startsWith("/")) {
+        id = id.remove(0, 1);
+    }
+    qDebug() << "KRunnerItemHandler:openUrl " << runner << " " << id;
+
+    runnerManager()->run(id);
+    return true;
+}
+
+class KRunnerModel::Private {
+public:
+    Private()
+    {
+    }
+
+    ~Private()
+    {
+    }
+
+    QBasicTimer searchDelay;
+    QString searchQuery;
+    DisplayOrder displayOrder;
+};
+
+KRunnerModel::KRunnerModel(QObject *parent)
+        : KickoffModel(parent)
+        , d(new Private())
+{
+    connect(runnerManager(),
+            SIGNAL(matchesChanged (const QList< Plasma::QueryMatch > & )),
+            this,
+            SLOT(matchesChanged (const QList< Plasma::QueryMatch > & )));
+}
+
+KRunnerModel::~KRunnerModel()
+{
+    delete d;
+}
+
+void KRunnerModel::setQuery(const QString& query)
+{
+    qDebug() << "KRunnerModel::setQuery: " << query;
+    runnerManager()->reset();
+    clear();
+
+    d->searchQuery = query.trimmed();
+
+    if (d->searchQuery.isEmpty()) {
+        return;
+    }
+
+    d->searchDelay.start(DELAY_TIME, this);
+}
+
+void KRunnerModel::timerEvent(QTimerEvent * event)
+{
+    qDebug() << "KRunnerModel::timerEvent";
+    KickoffModel::timerEvent(event);
+
+    if (event->timerId() == d->searchDelay.timerId()) {
+        d->searchDelay.stop();
+        runnerManager()->launchQuery(d->searchQuery);
+    };
+}
+
+void KRunnerModel::setNameDisplayOrder(DisplayOrder displayOrder)
+{
+    d->displayOrder = displayOrder;
+}
+
+DisplayOrder KRunnerModel::nameDisplayOrder() const
+{
+   return d->displayOrder;
+}
+
+void KRunnerModel::matchesChanged(const QList< Plasma::QueryMatch > & m)
+{
+    QList< Plasma::QueryMatch > matches = m;
+
+    qSort(matches.begin(), matches.end());
+
+    while (matches.size()) {
+        Plasma::QueryMatch match = matches.takeLast();
+
+        qDebug() << "KRunnerModel::matchesChanged: "
+            << match.icon()
+            << match.text()
+            << match.subtext()
+            << match.data()
+            ;
+        appendRow(
+            StandardItemFactory::createItem(
+                match.icon(),
+                match.text(),
+                match.subtext(),
+                QString("krunner://") + match.runner()->id() + "/" + match.id()
+                )
+            );
+    }
+}
+
+Qt::ItemFlags KRunnerModel::flags(const QModelIndex &index) const
+{
+    Qt::ItemFlags flags = KickoffModel::flags(index);
+
+    if (index.isValid()) {
+        KUrl url = data(index, UrlRole).toString();
+        QString host = url.host();
+        if (host != "services") {
+            flags &= ~ ( Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled );
+        }
+    } else {
+        flags = 0;
+    }
+
+    return flags;
+}
+
+QMimeData * KRunnerModel::mimeData(const QModelIndexList &indexes) const
+{
+    KUrl::List urls;
+
+    foreach (QModelIndex index, indexes) {
+        KUrl url = data(index, UrlRole).toString();
+        QString id = url.path();
+        if (id.startsWith("/")) {
+            id = id.remove(0, 1);
+        }
+        urls << KUrl(id);
+    }
+
+    QMimeData *mimeData = new QMimeData();
+
+    if (!urls.isEmpty()) {
+        urls.populateMimeData(mimeData);
+    }
+
+    return mimeData;
+
+}
+
+#include "krunnermodel.moc"
---
+++ plasma/applets/kickoff/core/krunnermodel.h
@@ -0,0 +1,73 @@
+/*
+    Copyright 2009 Ivan Cukic <ivan.cukic+kde@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KRUNNERMODEL_H
+#define KRUNNERMODEL_H
+
+#include "core/models.h"
+#include "core/kickoff_export.h"
+#include "core/kickoffmodel.h"
+#include "core/urlitemlauncher.h"
+
+#include <Plasma/QueryMatch>
+
+namespace Kickoff {
+
+class KICKOFF_EXPORT KRunnerItemHandler : public UrlItemHandler {
+public:
+    virtual bool openUrl(const KUrl& url);
+};
+
+class KICKOFF_EXPORT KRunnerModel : public KickoffModel
+{
+    Q_OBJECT
+
+public:
+    KRunnerModel(QObject *parent);
+    virtual ~KRunnerModel();
+    void setNameDisplayOrder(DisplayOrder displayOrder);
+    DisplayOrder nameDisplayOrder() const;
+
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+    virtual QMimeData *mimeData(const QModelIndexList &indexes) const;
+    // virtual QStringList mimeTypes() const;
+    // virtual Qt::DropActions supportedDropActions() const;
+    // virtual Qt::DropActions supportedDragActions() const;
+
+private:
+    void timerEvent(QTimerEvent * event);
+
+public Q_SLOTS:
+    void setQuery(const QString& query);
+
+private Q_SLOTS:
+    void matchesChanged(const QList< Plasma::QueryMatch > & matches);
+
+Q_SIGNALS:
+    void resultsAvailable();
+
+private:
+    class Private;
+    Private * const d;
+};
+
+}
+
+#endif // KRUNNERMODEL_H
+
