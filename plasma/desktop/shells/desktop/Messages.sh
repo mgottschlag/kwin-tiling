abf348038e6d237a45b737c22db42a67ec1c748e
Laurent Montel <montel@kde.org>
2009-09-15 07:19:30 +0000

    Extract i18n from scripting too
    svn path=/trunk/KDE/kdebase/workspace/; revision=1023660

--- plasma/desktop/shells/desktop/Messages.sh
+++ plasma/desktop/shells/desktop/Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT *.cpp -o $podir/plasma-desktop.pot
+$XGETTEXT *.cpp scripting/*.cpp -o $podir/plasma-desktop.pot
 rm -f rc.cpp
