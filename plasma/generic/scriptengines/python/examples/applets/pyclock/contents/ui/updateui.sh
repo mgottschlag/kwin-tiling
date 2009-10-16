e50a256339987d798fdb9a0a50606d40ff72c118
Aaron J. Seigo <aseigo@kde.org>
2009-10-16 20:10:03 +0000

    move the examples to kdeexamples
    svn path=/trunk/KDE/kdebase/workspace/; revision=1036206

--- plasma/generic/scriptengines/python/examples/applets/pyclock/contents/ui/updateui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pykdeuic4 analog_clock_config.ui > ../code/analog_clock_config_ui.py
-pykdeuic4 calendar.ui > ../code/calendar_ui.py
-pykdeuic4 timezonesConfig.ui > ../code/timezonesConfig_ui.py
