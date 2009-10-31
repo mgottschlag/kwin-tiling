3940e79d7fe93204eec26ccddf7ff14b342a9f80
Chusslove Illich <caslav.ilic@gmx.net>
2009-10-31 08:12:07 +0000

    More general specification of stuff to extract.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1042943

--- plasma/generic/applets/systemtray/Messages.sh
+++ plasma/generic/applets/systemtray/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC ui/*.ui >> rc.cpp
-$XGETTEXT notificationitemwatcher/*.cpp core/*.cpp rc.cpp ui/*.cpp -o $podir/plasma_applet_systemtray.pot
+$XGETTEXT `find -iname \*.cpp` -o $podir/plasma_applet_systemtray.pot
 rm rc.cpp
