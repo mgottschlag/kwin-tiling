bc2972a4af2123e87cc9b580517a786379f82807
Anne-Marie Mahfouf <annma@kde.org>
2009-11-05 13:01:32 +0000

    put icons in the tabs
    svn path=/trunk/KDE/kdebase/workspace/; revision=1045207

--- libs/plasmagenericshell/backgrounddialog.cpp
+++ libs/plasmagenericshell/backgrounddialog.cpp
@@ -216,7 +216,7 @@ BackgroundDialog::BackgroundDialog(const QSize& res, Plasma::Containment *c, Pla
     
     QWidget *activity = new QWidget(this);
     d->activityUi.setupUi(activity);
-    d->activityItem = addPage(activity, i18n("Activity"), "activity");
+    d->activityItem = addPage(activity, i18n("Activity"), "plasma");
     
     qreal previewRatio = (qreal)res.width() / (qreal)res.height();
     QSize monitorSize(200, int(200 * previewRatio));
@@ -244,7 +244,7 @@ BackgroundDialog::BackgroundDialog(const QSize& res, Plasma::Containment *c, Pla
 
     MousePlugins *m = new MousePlugins(d->containment, this);
     connect(m, SIGNAL(modified(bool)), this, SLOT(settingsModified(bool)));
-    d->mouseItem = addPage(m, i18n("Mouse Plugins"), "mouseplugins");
+    d->mouseItem = addPage(m, i18n("Mouse Plugins"), "input-mouse");
 
     if (d->containment && d->containment->hasConfigurationInterface()) {
         d->containment->createConfigurationInterface(this);
