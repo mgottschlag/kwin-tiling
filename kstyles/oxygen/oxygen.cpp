3c3276d9e59e37fadf1a8a64917f43cb82ff2c8d
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2009-11-23 23:46:20 +0000

    changed frameWidth from 1 to 0. This makes scrollbars look closer to frames, and reduces pixel wasting
    svn path=/trunk/KDE/kdebase/runtime/kstyles/; revision=1053389

--- kstyles/oxygen/oxygen.cpp
+++ kstyles/oxygen/oxygen.cpp
@@ -114,8 +114,7 @@ OxygenStyle::OxygenStyle() :
     // call the slot directly; this initial call will set up things that also
     // need to be reset when the system palette changes
     globalSettingsChange(KGlobalSettings::PaletteChanged, 0);
-
-    setWidgetLayoutProp(WT_Generic, Generic::DefaultFrameWidth, 1);
+    setWidgetLayoutProp(WT_Generic, Generic::DefaultFrameWidth, 0);
 
     // TODO: change this when double buttons are implemented
     setWidgetLayoutProp(WT_ScrollBar, ScrollBar::DoubleBotButton, true);
@@ -280,7 +279,8 @@ bool OxygenStyle::drawGroupBoxComplexControl( const QStyleOptionComplex *option,
             painter->setFont(oldFont);
             return true;
         }
-    }
+
+        }
 
     return false;
 }
@@ -3007,7 +3007,7 @@ bool OxygenStyle::drawGenericPrimitive(
 
             } else if(widgetType == WT_Generic && (flags & State_Raised)) {
 
-                renderSlab(p, r.adjusted(-2, -2, 2, 2), pal.color(QPalette::Background), NoFill);
+                renderSlab(p, r.adjusted(-1, -1, 1, 1), pal.color(QPalette::Background), NoFill);
 
             }
 
