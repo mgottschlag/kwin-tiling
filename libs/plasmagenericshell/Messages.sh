3e203e6da891d12f005af5ce2f2e3182aa92566d
Burkhard Lück <lueck@hube-lueck.de>
2009-12-08 19:41:52 +0000

    extract msgs from subdir widgetsExplorer as well, -> 25 new messages CCMAIL:kde-i18n-doc@kde.org
    svn path=/trunk/KDE/kdebase/workspace/; revision=1060397

--- libs/plasmagenericshell/Messages.sh
+++ libs/plasmagenericshell/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/plasmagenericshell.pot
+$XGETTEXT *.cpp widgetsExplorer/*.cpp -o $podir/plasmagenericshell.pot
 rm -f rc.cpp
