a9699d60d27a3b2cad507c19717bc9f1bf1af381
Aaron J. Seigo <aseigo@kde.org>
2010-01-20 00:40:59 +0000

    use the proper enum
    svn path=/trunk/KDE/kdebase/workspace/; revision=1077353

--- libs/plasmagenericshell/widgetsExplorer/appletslist.cpp
+++ libs/plasmagenericshell/widgetsExplorer/appletslist.cpp
@@ -506,7 +506,7 @@ void AppletsListWidget::onLeftArrowPress()
 
 void AppletsListWidget::scroll(ScrollPolicy side, ScrollPolicy how)
 {
-    int step = (how == AppletsListWidget::Wheel) ? wheelStep : arrowClickStep;
+    const int step = (how == AppletsListWidget::Wheel) ? wheelStep : arrowClickStep;
 
     m_toolTip->setVisible(false);
 
@@ -528,6 +528,7 @@ void AppletsListWidget::scrollDownRight(int step)
     if (nextFirstItemIndex > m_currentAppearingAppletsOnList.count() - 1) {
         nextFirstItemIndex = m_currentAppearingAppletsOnList.count() - 1;
     }
+
     if (nextFirstItemIndex < 0) {
         return;
     }
@@ -550,16 +551,16 @@ void AppletsListWidget::scrollDownRight(int step)
         }
     }
 
-    qreal move = startPosition - visibleStartPosition();
-
+    const qreal move = startPosition - visibleStartPosition();
     m_firstItemIndex = nextFirstItemIndex;
 
     m_slide->stop();
     if (m_orientation == Qt::Horizontal) {
-        m_slide->setProperty("movementDirection", Plasma::Animation::Left);
+        m_slide->setProperty("movementDirection", Plasma::Animation::MoveLeft);
     } else {
-        m_slide->setProperty("movementDirection", Plasma::Animation::Up);
+        m_slide->setProperty("movementDirection", Plasma::Animation::MoveUp);
     }
+
     m_slide->setProperty("distance", move);
     m_slide->start();
 
@@ -583,9 +584,9 @@ void AppletsListWidget::scrollUpLeft(int step)
 
     m_slide->stop();
     if (m_orientation == Qt::Horizontal) {
-        m_slide->setProperty("movementDirection", Plasma::Animation::Left);
+        m_slide->setProperty("movementDirection", Plasma::Animation::MoveLeft);
     } else {
-        m_slide->setProperty("movementDirection", Plasma::Animation::Up);
+        m_slide->setProperty("movementDirection", Plasma::Animation::MoveUp);
     }
     m_slide->setProperty("distance", move);
     m_slide->start();
