9776e8542519ce65968cdb94c4895cd9eb7ed465
Roman Shtylman <shtylman@gmail.com>
2010-02-12 05:28:03 +0000

    fixed hide icon behavior when coming out of 'sleep'
    The hide icon would appear when resuming because some of the
    systemtray icons have autohidden themselves and then add themselves
    in an unhidden state at the end. This was not handled properly by
    the existing hidden icon toggle code.
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=1088999

--- plasma/generic/applets/systemtray/ui/taskarea.cpp
+++ plasma/generic/applets/systemtray/ui/taskarea.cpp
@@ -68,6 +68,7 @@ public:
 
     QSet<QString> hiddenTypes;
     QSet<QString> alwaysShownTypes;
+    QSet<SystemTray::Task*> hiddenTasks;
     bool showingHidden : 1;
     bool hasHiddenTasks : 1;
     bool hasTasksThatCanHide : 1;
@@ -202,9 +203,20 @@ void TaskArea::addWidgetForTask(SystemTray::Task *task)
         }
         return;
     }
-
-    d->hasTasksThatCanHide = d->hasTasksThatCanHide || (task->hidden() != Task::NotHidden);
-
+    
+    // keep track of the hidden tasks
+    // needs to be done because if a tasks is added multiple times (like when coming out of sleep)
+    // it may be autohidden for a while until the final one which will not be hidden
+    // therefore we need a way to track the hidden tasks
+    // if the task appears in the hidden list, then we know there are hidden tasks
+    if (task->hidden() != Task::NotHidden) {
+        d->hiddenTasks << task;
+    } else {
+        d->hiddenTasks.remove(task);
+    }
+    
+    d->hasTasksThatCanHide = !d->hiddenTasks.isEmpty();
+    
     if (!d->showingHidden && task->hidden() != Task::NotHidden && !d->alwaysShownTypes.contains(task->typeId())) {
         //kDebug() << "is a hidden type";
         d->hasHiddenTasks = true;
@@ -251,6 +263,9 @@ void TaskArea::addWidgetForTask(SystemTray::Task *task)
 
 void TaskArea::removeTask(Task *task)
 {
+    d->hiddenTasks.remove(task);
+    d->hasTasksThatCanHide = d->hiddenTasks.count() > 0;
+    
     QGraphicsWidget *widget = task->widget(d->host, false);
 
     if (widget) {
@@ -260,7 +275,7 @@ void TaskArea::removeTask(Task *task)
         d->lastTasksLayout->removeItem(widget);
         relayout();
     } else {
-        relayout();relayout();
+        relayout();
         QTimer::singleShot(200, this, SLOT(relayout()));
     }
 }
