294e8652df6eeae886a7028101bc32af8c39a9fa
Darío Andrés Rodríguez <andresbajotierra@gmail.com>
2010-02-21 18:32:37 +0000

    - Revert port to KHNS3 (Klassroom in course)
    svn path=/trunk/KDE/kdebase/workspace/; revision=1093929

--- ksplash/kcm/installer.cpp
+++ ksplash/kcm/installer.cpp
@@ -38,7 +38,7 @@
 #include <ktar.h>
 #include <kservicetypetrader.h>
 #include <kio/netaccess.h>
-#include <knewstuff3/downloaddialog.h>
+#include <knewstuff2/engine.h>
 
 ThemeListBox::ThemeListBox(QWidget *parent)
   : KListWidget(parent)
@@ -477,10 +477,11 @@ void SplashInstaller::slotSetTheme(int id)
 //-----------------------------------------------------------------------------
 void SplashInstaller::slotNew()
 {
-  KNS3::DownloadDialog dialog("ksplash.knsrc", this);
-  dialog.exec();
-  if (!dialog.changedEntries().isEmpty()) {
-    readThemesList();
+  KNS::Engine engine(this);
+  if (engine.init("ksplash.knsrc")) {
+    const KNS::Entry::List entries = engine.downloadDialogModal(this);
+    if ( !entries.isEmpty() )
+        readThemesList();
   }
 }
 
