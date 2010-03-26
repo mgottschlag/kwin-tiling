6e3bd3750073e241c6277c34606cfa86a464eb30
Aaron J. Seigo <aseigo@kde.org>
2010-03-26 15:58:42 +0000

    proper version with license headers and all.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1107719

--- statusnotifierwatcher/systemtraytypedefs.h
+++ statusnotifierwatcher/systemtraytypedefs.h
@@ -1,3 +1,28 @@
+/***************************************************************************
+ *                                                                         *
+ *   Copyright (C) 2009 Marco Martin <notmart@gmail.com>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef SYSTEMTRAYTYPEDEFS_H
+#define SYSTEMTRAYTYPEDEFS_H
+
+#include <QByteArray>
+#include <QString>
 #include <QVector>
 
 struct KDbusImageStruct {
@@ -6,11 +31,7 @@ struct KDbusImageStruct {
     QByteArray data;
 };
 
-Q_DECLARE_METATYPE(KDbusImageStruct)
-
 typedef QVector<KDbusImageStruct> KDbusImageVector;
-
-Q_DECLARE_METATYPE(KDbusImageVector)
 
 struct KDbusToolTipStruct {
     QString icon;
@@ -19,4 +40,9 @@ struct KDbusToolTipStruct {
     QString subTitle;
 };
 
+Q_DECLARE_METATYPE(KDbusImageStruct)
+Q_DECLARE_METATYPE(KDbusImageVector)
 Q_DECLARE_METATYPE(KDbusToolTipStruct)
+
+#endif
+
