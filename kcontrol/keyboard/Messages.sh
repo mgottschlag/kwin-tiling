c84690ff7225cfa7e70317db2cefe61353b8e9d9
Albert Astals Cid <tsdgeos@terra.es>
2010-04-26 18:08:42 +0000

    add the rc.cpp when extracting the first catalog CCMAIL: arysin@gmail.com
    svn path=/trunk/kdereview/keyboard2/; revision=1119166

--- kcontrol/keyboard/Messages.sh
+++ kcontrol/keyboard/Messages.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 $EXTRACTRC kcmmiscwidget.ui >> rc.cpp
-$XGETTEXT kcmmisc.cpp -o $podir/kcmkeyboard.pot
+$XGETTEXT rc.cpp kcmmisc.cpp -o $podir/kcmkeyboard.pot
 rm -f rc.cpp
 $EXTRACTRC kcm_*.ui >> rc.cpp
 $XGETTEXT kcm_*.cpp keyboard_*.cpp -o $podir/kxkb.pot
