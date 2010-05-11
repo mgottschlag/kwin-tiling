a0823766ba3a5967761a9f30ad78e7c330ae452d
Andriy Rysin <arysin@gmail.com>
2010-05-11 01:51:37 +0000

    move new keyboard module from kdereview
    svn path=/trunk/KDE/kdebase/workspace/; revision=1125243

--- kcontrol/keyboard/Messages.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC kcmmiscwidget.ui >> rc.cpp
-$XGETTEXT rc.cpp kcmmisc.cpp -o $podir/kcmkeyboard.pot
-rm -f rc.cpp
-$EXTRACTRC kcm_*.ui >> rc.cpp
-$XGETTEXT kcm_*.cpp keyboard_*.cpp -o $podir/kxkb.pot
-rm -f rc.cpp
--- kcontrol/keyboard/bindings.cpp
+++
@@ -1,9 +0,0 @@
-# define DEF( name, key ) \
-   QAction* a__ = actionCollection->addAction( name );   \
-   a = qobject_cast<KAction*>(a__);                       \
-   a->setText( i18n(name) );                              \
-   qobject_cast<KAction*>( a) ->setGlobalShortcut(KShortcut(key));                  
-
-    DEF( I18N_NOOP("Switch to Next Keyboard Layout"), Qt::ALT+Qt::CTRL+Qt::Key_K);
-
-#undef DEF
