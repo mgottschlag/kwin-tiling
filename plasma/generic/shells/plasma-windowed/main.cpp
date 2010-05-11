b91f940398d03c4e299c146f71f747ae730d95f0
Marco Martin <notmart@gmail.com>
2010-05-11 20:33:10 +0000

    move in kdebase
    svn path=/trunk/KDE/kdebase/workspace/; revision=1125617

---
+++ plasma/generic/shells/plasma-windowed/main.cpp
@@ -0,0 +1,60 @@
+/*
+ *   Copyright 2006-2008 Aaron Seigo <aseigo@kde.org>
+ *   Copyright 2009 Marco Martin <notmart@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as
+ *   published by the Free Software Foundation; either version 2,
+ *   or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KLocale>
+#include <KIcon>
+
+#include "plasmaapp.h"
+
+static const char description[] = I18N_NOOP( "Shell used to load Plasma widgets as stand-alone applications." );
+static const char version[] = "0.1";
+
+extern "C"
+KDE_EXPORT int kdemain(int argc, char **argv)
+{
+    KAboutData aboutData("plasma-windowed", 0, ki18n("Plasma Widgets shell"),
+                         version, ki18n(description), KAboutData::License_GPL,
+                         ki18n("Copyright 2006-2009, The KDE Team"));
+    aboutData.addAuthor(ki18n("Marco Martin"),
+                        ki18n("Author and maintainer"),
+                        "notmart@gmail.com");
+
+    KCmdLineArgs::init(argc, argv, &aboutData);
+
+    KCmdLineOptions options;
+    options.add("b", ki18n("Show window decorations around the widget"));
+    options.add("noborder", ki18n("Show window decorations around the widget"));
+    options.add("f");
+    options.add("fullscreen", ki18n("Display the widget fullscreen"));
+    options.add("+applet", ki18n("Name of applet to view; may refer to the plugin name or be a path "
+                                "(absolute or relative) to a package. If not provided, then an "
+                                "attempt is made to load a package from the current directory."));
+    options.add("+[args]", ki18n("Optional arguments for the applet to add"));
+    KCmdLineArgs::addCmdLineOptions(options);
+
+    PlasmaApp *app = PlasmaApp::self();
+    QApplication::setWindowIcon(KIcon("plasma"));
+    int rc = app->exec();
+    delete app;
+    return rc;
+}
+
---
+++ plasma/generic/shells/plasma-windowed/plasmaapp.cpp
@@ -0,0 +1,214 @@
+/*
+ *   Copyright 2006-2008 Aaron Seigo <aseigo@kde.org>
+ *   Copyright 2009 Marco Martin <notmart@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "plasmaapp.h"
+
+#include <unistd.h>
+
+#include <QPixmapCache>
+#include <QtDBus/QtDBus>
+
+#include <KCrash>
+#include <KColorUtils>
+#include <KDebug>
+#include <KCmdLineArgs>
+#include <KWindowSystem>
+
+#include <Plasma/Containment>
+#include <Plasma/Theme>
+#include <Plasma/Corona>
+#include <Plasma/Applet>
+#include <Plasma/Wallpaper>
+#include <Plasma/WindowEffects>
+
+#include "singleview.h"
+
+
+PlasmaApp* PlasmaApp::self()
+{
+    if (!kapp) {
+        return new PlasmaApp();
+    }
+
+    return qobject_cast<PlasmaApp*>(kapp);
+}
+
+PlasmaApp::PlasmaApp()
+    : KUniqueApplication(),
+      m_corona(0),
+      m_maxId(0)
+{
+    KGlobal::locale()->insertCatalog("plasma-standaloneplasmoids");
+    KCrash::setFlags(KCrash::AutoRestart);
+
+    KConfigGroup cg(KGlobal::config(), "General");
+    Plasma::Theme::defaultTheme()->setFont(cg.readEntry("desktopFont", font()));
+
+    corona();
+    m_containment = m_corona->addContainment("null");
+
+    KConfigGroup containmentConfig = m_containment->config();
+    KConfigGroup applets(&containmentConfig, "Applets");
+
+    foreach (const QString &group, applets.groupList()) {
+        KConfigGroup appletGroup(&applets, group);
+
+        int id = appletGroup.name().toInt();
+        QString pluginName = appletGroup.readEntry("plugin", QString());
+        if (id != 0 && !pluginName.isEmpty()) {
+            m_storedApplets.insert(pluginName, id);
+            m_maxId = qMax(id, m_maxId);
+        }
+    }
+
+    //newInstance();
+    connect(this, SIGNAL(aboutToQuit()), this, SLOT(cleanup()));
+}
+
+PlasmaApp::~PlasmaApp()
+{
+}
+
+int  PlasmaApp::newInstance()
+{
+    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
+
+    if (args->count() == 0) {
+        KCmdLineArgs::usage();
+        return 0;
+    }
+
+    QString pluginName;
+    if (args->count() > 0) {
+        pluginName = args->arg(0);
+    }
+
+    QVariantList appletArgs;
+    for (int i = 1; i < args->count(); ++i) {
+        appletArgs << args->arg(i);
+    }
+
+    int appletId;
+    if (m_storedApplets.contains(pluginName)) {
+        appletId = m_storedApplets.values(pluginName).first();
+        m_storedApplets.remove(pluginName, appletId);
+    } else {
+        appletId = ++m_maxId;
+    }
+
+    SingleView *view = new SingleView(m_corona, m_containment, pluginName, appletId, appletArgs);
+
+    if (!view->applet()) {
+        delete view;
+        return 0;
+    }
+
+    connect(view, SIGNAL(storeApplet(Applet*)), this, SLOT(storeApplet(Applet*)));
+    connect(view, SIGNAL(destroyed(QObject*)), this, SLOT(viewDestroyed(QObject*)));
+
+    if (args->isSet("border")) {
+        view->setBackgroundBrush(KColorUtils::mix(Plasma::Theme::defaultTheme()->color(Plasma::Theme::BackgroundColor), Plasma::Theme::defaultTheme()->color(Plasma::Theme::TextColor), 0.15));
+        connect(Plasma::Theme::defaultTheme(), SIGNAL(themeChanged()), SLOT(themeChanged()));
+        view->applet()->setBackgroundHints(Plasma::Applet::NoBackground);
+    } else {
+        view->setWindowFlags(Qt::FramelessWindowHint);
+        view->setAttribute(Qt::WA_TranslucentBackground);
+        view->setAutoFillBackground(false);
+        view->viewport()->setAutoFillBackground(false);
+        view->setAttribute(Qt::WA_NoSystemBackground);
+        view->viewport()->setAttribute(Qt::WA_NoSystemBackground);
+        Plasma::WindowEffects::overrideShadow(view->winId(), true);
+    }
+
+    if (args->isSet("fullscreen")) {
+        view->setWindowState(Qt::WindowFullScreen);
+    }
+
+    args->clear();
+
+    m_views.append(view);
+    KWindowSystem::setOnDesktop(view->winId(), KWindowSystem::currentDesktop());
+    view->show();
+    view->raise();
+
+    return 0;
+}
+
+
+void PlasmaApp::cleanup()
+{
+    if (m_corona) {
+        m_corona->saveLayout();
+    }
+
+    qDeleteAll(m_views);
+
+    delete m_corona;
+    m_corona = 0;
+
+    //TODO: This manual sync() should not be necessary?
+    syncConfig();
+}
+
+void PlasmaApp::syncConfig()
+{
+    KGlobal::config()->sync();
+}
+
+void PlasmaApp::themeChanged()
+{
+    foreach(SingleView *view, m_views) {
+        if (view->autoFillBackground()) {
+            view->setBackgroundBrush(KColorUtils::mix(Plasma::Theme::defaultTheme()->color(Plasma::Theme::BackgroundColor), Plasma::Theme::defaultTheme()->color(Plasma::Theme::TextColor), 0.15));
+        }
+    }
+}
+
+Plasma::Corona* PlasmaApp::corona()
+{
+    if (!m_corona) {
+        m_corona = new Plasma::Corona(this);
+        connect(m_corona, SIGNAL(configSynced()), this, SLOT(syncConfig()));
+
+
+        m_corona->setItemIndexMethod(QGraphicsScene::NoIndex);
+        //m_corona->initializeLayout();
+    }
+
+    return m_corona;
+}
+
+bool PlasmaApp::hasComposite()
+{
+    return KWindowSystem::compositingActive();
+}
+
+
+void PlasmaApp::storeApplet(Plasma::Applet *applet)
+{
+    m_storedApplets.insert(applet->name(), applet->id());
+}
+
+void PlasmaApp::viewDestroyed(QObject *view)
+{
+    m_views.removeAll(static_cast<SingleView *>(view));
+}
+
+#include "plasmaapp.moc"
