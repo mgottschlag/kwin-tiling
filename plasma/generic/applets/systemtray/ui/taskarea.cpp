78463da978b364a51aaaedc89d9ae3e6e9f92d79
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2010-05-13 15:04:45 +0000

    fix the name of statusitems displayed in the popup of hidden tasks by adding a slot in the HiddenTaskLabel class connected to the changed signal of the task move the class in a private header because I could not compile while keeping the class in the taskarea.cpp file
    svn path=/trunk/KDE/kdebase/workspace/; revision=1126290

--- plasma/generic/applets/systemtray/ui/taskarea.cpp
+++ plasma/generic/applets/systemtray/ui/taskarea.cpp
@@ -44,114 +44,10 @@
 
 #include "applet.h"
 #include "compactlayout.h"
+#include "taskarea_p.h"
 
 namespace SystemTray
 {
-
-class HiddenTaskLabel : public Plasma::Label
-{
-public:
-    HiddenTaskLabel(QGraphicsWidget *taskIcon, const QString &label, Plasma::ItemBackground *itemBackground, Plasma::Applet *applet, QGraphicsWidget *parent = 0)
-        : Plasma::Label(parent),
-          m_taskIcon(taskIcon),
-          m_itemBackground(itemBackground),
-          m_applet(applet)
-    {
-        taskIcon->setMaximumHeight(48);
-        taskIcon->setMinimumHeight(24);
-        taskIcon->setMinimumWidth(24);
-
-        nativeWidget()->setIndent(6);
-        setContentsMargins(6, 0, 0, 0);
-
-        setWordWrap(false);
-        setText(label);
-        if (!itemBackground->scene()) {
-            scene()->addItem(itemBackground);
-            takeItemBackgroundOwnership();
-        }
-    }
-
-    ~HiddenTaskLabel()
-    {
-    }
-
-protected:
-    void takeItemBackgroundOwnership()
-    {
-        if (m_taskIcon) {
-            QRectF totalRect = geometry().united(m_taskIcon.data()->geometry());
-            totalRect.moveTopLeft(QPoint(0,0));
-            totalRect = m_taskIcon.data()->mapToScene(totalRect).boundingRect();
-            qreal left, top, right, bottom;
-            m_itemBackground->getContentsMargins(&left, &top, &right, &bottom);
-            totalRect.adjust(-left/2, -top/2, right/2, bottom/2);
-            m_itemBackground->setTarget(totalRect);
-        }
-    }
-
-    template<class T> void forwardEvent(T *event)
-    {
-        if (m_taskIcon) {
-            QGraphicsWidget *item = m_taskIcon.data();
-            QPointF delta = item->sceneBoundingRect().center() - event->scenePos();
-            event->setScenePos(item->sceneBoundingRect().center());
-            event->setScreenPos((event->screenPos() + delta).toPoint());
-            if (dynamic_cast<QGraphicsSceneContextMenuEvent *>(event) &&
-                qobject_cast<Plasma::Applet*>(item) &&
-                m_applet->containment()) {
-                event->setPos(m_applet->containment()->mapFromScene(event->scenePos()));
-                scene()->sendEvent(m_applet->containment(), event);
-            } else {
-            if (dynamic_cast<QGraphicsSceneContextMenuEvent *>(event)) 
-                event->setPos(item->boundingRect().center());
-                scene()->sendEvent(item, event);
-            }
-        }
-    }
-
-    void mousePressEvent(QGraphicsSceneMouseEvent *event)
-    {
-        forwardEvent(event);
-    }
-
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
-    {
-        forwardEvent(event);
-    }
-
-    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
-    {
-        forwardEvent(event);
-    }
-
-    void wheelEvent(QGraphicsSceneWheelEvent *event)
-    {
-        forwardEvent(event);
-    }
-
-    void hoverEnterEvent(QGraphicsSceneHoverEvent * event)
-    {
-        takeItemBackgroundOwnership();
-        forwardEvent(event);
-    }
-
-    void hoverLeaveEvent(QGraphicsSceneHoverEvent * event)
-    {
-        forwardEvent(event);
-    }
-
-    void hoverMoveEvent(QGraphicsSceneHoverEvent * event)
-    {
-        forwardEvent(event);
-    }
-
-private:
-    QWeakPointer<QGraphicsWidget> m_taskIcon;
-    Plasma::ItemBackground *m_itemBackground;
-    Plasma::Applet *m_applet;
-};
-
 
 class TaskArea::Private
 {
@@ -381,12 +277,14 @@ void TaskArea::addWidgetForTask(SystemTray::Task *task)
                     break;
                 }
             }
+            disconnect(task, 0, d->hiddenTasks.value(task), 0);
             d->hiddenTasks.value(task)->deleteLater();
             d->hiddenTasks.remove(task);
             d->hiddenRelayoutTimer->start(250);
         }
     } else if (!d->hiddenTasks.contains(task)) {
         HiddenTaskLabel *hiddenWidget = new HiddenTaskLabel(widget, task->name(), d->itemBackground, d->host, d->hiddenTasksWidget);
+        connect(task, SIGNAL(changed(SystemTray::Task*)), hiddenWidget, SLOT(taskChanged(SystemTray::Task*)));
         d->hiddenTasks.insert(task, hiddenWidget);
 
         if (widget) {
@@ -677,3 +575,4 @@ void TaskArea::setLocation(Plasma::Location location)
 }
 
 #include "taskarea.moc"
+#include "taskarea_p.moc"
---
+++ plasma/generic/applets/systemtray/ui/taskarea_p.h
@@ -0,0 +1,142 @@
+/***************************************************************************
+ *   taskarea.cpp                                                          *
+ *                                                                         *
+ *   Copyright (C) 2008 Jason Stubbs <jasonbstubbs@gmail.com>              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef TASKAREA_P_H
+#define TASKAREA_P_H
+
+#include <Plasma/Label>
+
+namespace SystemTray
+{
+
+class HiddenTaskLabel : public Plasma::Label
+{
+
+    Q_OBJECT
+
+public:
+    HiddenTaskLabel(QGraphicsWidget *taskIcon, const QString &label, Plasma::ItemBackground *itemBackground, Plasma::Applet *applet, QGraphicsWidget *parent = 0)
+        : Plasma::Label(parent),
+          m_taskIcon(taskIcon),
+          m_itemBackground(itemBackground),
+          m_applet(applet)
+    {
+        taskIcon->setMaximumHeight(48);
+        taskIcon->setMinimumHeight(24);
+        taskIcon->setMinimumWidth(24);
+
+        nativeWidget()->setIndent(6);
+        setContentsMargins(6, 0, 0, 0);
+
+        setWordWrap(false);
+        setText(label);
+        if (!itemBackground->scene()) {
+            scene()->addItem(itemBackground);
+            takeItemBackgroundOwnership();
+        }
+    }
+
+public slots:
+  
+    void taskChanged(SystemTray::Task *task)
+    {
+        setText(task->name());
+    }
+
+protected:
+    void takeItemBackgroundOwnership()
+    {
+        if (m_taskIcon) {
+            QRectF totalRect = geometry().united(m_taskIcon.data()->geometry());
+            totalRect.moveTopLeft(QPoint(0,0));
+            totalRect = m_taskIcon.data()->mapToScene(totalRect).boundingRect();
+            qreal left, top, right, bottom;
+            m_itemBackground->getContentsMargins(&left, &top, &right, &bottom);
+            totalRect.adjust(-left/2, -top/2, right/2, bottom/2);
+            m_itemBackground->setTarget(totalRect);
+        }
+    }
+
+    template<class T> void forwardEvent(T *event)
+    {
+        if (m_taskIcon) {
+            QGraphicsWidget *item = m_taskIcon.data();
+            QPointF delta = item->sceneBoundingRect().center() - event->scenePos();
+            event->setScenePos(item->sceneBoundingRect().center());
+            event->setScreenPos((event->screenPos() + delta).toPoint());
+            if (dynamic_cast<QGraphicsSceneContextMenuEvent *>(event) &&
+                qobject_cast<Plasma::Applet*>(item) &&
+                m_applet->containment()) {
+                event->setPos(m_applet->containment()->mapFromScene(event->scenePos()));
+                scene()->sendEvent(m_applet->containment(), event);
+            } else {
+            if (dynamic_cast<QGraphicsSceneContextMenuEvent *>(event)) 
+                event->setPos(item->boundingRect().center());
+                scene()->sendEvent(item, event);
+            }
+        }
+    }
+
+    void mousePressEvent(QGraphicsSceneMouseEvent *event)
+    {
+        forwardEvent(event);
+    }
+
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+    {
+        forwardEvent(event);
+    }
+
+    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+    {
+        forwardEvent(event);
+    }
+
+    void wheelEvent(QGraphicsSceneWheelEvent *event)
+    {
+        forwardEvent(event);
+    }
+
+    void hoverEnterEvent(QGraphicsSceneHoverEvent * event)
+    {
+        takeItemBackgroundOwnership();
+        forwardEvent(event);
+    }
+
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent * event)
+    {
+        forwardEvent(event);
+    }
+
+    void hoverMoveEvent(QGraphicsSceneHoverEvent * event)
+    {
+        forwardEvent(event);
+    }
+
+private:
+    QWeakPointer<QGraphicsWidget> m_taskIcon;
+    Plasma::ItemBackground *m_itemBackground;
+    Plasma::Applet *m_applet;
+};
+
+}
+
+#endif
