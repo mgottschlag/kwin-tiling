dec612c0603b7018e00bef8f23420bd49fc4419d
Aaron J. Seigo <aseigo@kde.org>
2010-05-21 02:42:47 +0000

    march the size down to 26px
    svn path=/trunk/KDE/kdebase/workspace/; revision=1129024

--- plasma/desktop/shell/data/layouts/org.kde.plasma-desktop.defaultPanel/contents/layout.js
+++ plasma/desktop/shell/data/layouts/org.kde.plasma-desktop.defaultPanel/contents/layout.js
@@ -3,7 +3,7 @@ if (panelIds.length == 1) {
     // we are the only panel, so set the location for the user
     panel.location = 'bottom'
 }
-panel.height = 28
+panel.height = 26
 panel.addWidget("launcher")
 panel.addWidget("pager")
 panel.addWidget("tasks")
