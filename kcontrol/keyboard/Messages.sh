cc819c06eac87608448cb4da1fb5c52dbe822b16
Andriy Rysin <arysin@gmail.com>
2010-06-29 19:18:04 +0000

    fix missing i18n messages in the pot file
    svn path=/trunk/KDE/kdebase/workspace/; revision=1144378

--- kcontrol/keyboard/Messages.sh
+++ kcontrol/keyboard/Messages.sh
@@ -3,5 +3,5 @@ $EXTRACTRC kcmmiscwidget.ui >> rc.cpp
 $XGETTEXT rc.cpp kcmmisc.cpp -o $podir/kcmkeyboard.pot
 rm -f rc.cpp
 $EXTRACTRC kcm_*.ui >> rc.cpp
-$XGETTEXT kcm_*.cpp keyboard_*.cpp -o $podir/kxkb.pot
+$XGETTEXT kcm_*.cpp keyboard_*.cpp layout_widget.cpp flags.cpp -o $podir/kxkb.pot
 rm -f rc.cpp
