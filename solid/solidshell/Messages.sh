b7eec72537cf4622bfda733a30501671577b4d3b
Albert Astals Cid <tsdgeos@terra.es>
2010-07-09 18:40:56 +0000

    Fix Messages.sh CCMAIL: alex@eyeos.org
    svn path=/trunk/KDE/kdebase/workspace/; revision=1148105

--- solid/solidshell/Messages.sh
+++ solid/solidshell/Messages.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
-$XGETTEXT solid-bluetooth.cpp -o $podir/solid-bluetooth.pot
 $XGETTEXT solid-network.cpp -o $podir/solid-network.pot
 $XGETTEXT solid-powermanagement.cpp -o $podir/solid-powermanagement.pot
