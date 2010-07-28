a0706203a0e7301c6c38d6517281c59adbad6393
Marco Martin <notmart@gmail.com>
2010-07-28 17:30:02 +0000

    map from global the available screen region, to have proper margins in non fullscreen views and multi monitor
    svn path=/trunk/KDE/kdebase/workspace/; revision=1156191

--- plasma/netbook/containments/newspaper/newspaper.cpp
+++ plasma/netbook/containments/newspaper/newspaper.cpp
@@ -589,11 +589,11 @@ void Newspaper::availableScreenRegionChanged()
 
     QGraphicsView *ownView = view();
     
-    if (ownView) {
-        kWarning()<<"AAAA"<<ownView->pos()<<maxRect;
-        maxRect.moveTopLeft(maxRect.topLeft() - ownView->mapToGlobal(QPoint(0,0)));
+    //FIXME: the second check is a workaround to a qt bug: when a qwidget has just been created, maptoglobal and mapfromglobal aren't symmetryc. remove as soon as the bug is fixed
+    if (ownView && (ownView->mapFromGlobal(QPoint(0,0)) == -ownView->mapToGlobal(QPoint(0,0)))) {
+        maxRect.moveTopLeft(ownView->mapFromGlobal(maxRect.topLeft()));
     }
-kWarning()<<"FFFF"<<maxRect;
+
     setContentsMargins(maxRect.left(), maxRect.top(), qMax((qreal)0.0, size().width() - maxRect.right()), qMax((qreal)0.0, size().height() - maxRect.bottom()));
 }
 
