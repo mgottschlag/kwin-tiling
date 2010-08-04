4022598fcd8aa602ba0ca005122bfb2ccf2d5802
Aaron J. Seigo <aseigo@kde.org>
2010-08-04 19:39:41 +0000

    no actions? no menu.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1159224

--- plasma/generic/containmentactions/minimalcontextmenu/menu.cpp
+++ plasma/generic/containmentactions/minimalcontextmenu/menu.cpp
@@ -59,8 +59,13 @@ void ContextMenu::contextEvent(QEvent *event)
             return;
     }
 
+    QList<QAction*> actions = contextualActions();
+    if (actions.isEmpty()) {
+        return;
+    }
+
     KMenu desktopMenu;
-    desktopMenu.addActions(contextualActions());
+    desktopMenu.addActions(actions);
     desktopMenu.exec(screenPos);
 }
 
