525908184c3cbdebab6f9d7997d050a065f88e1f
Aaron J. Seigo <aseigo@kde.org>
2010-09-15 18:09:32 +0000

    remove the access to kwininfo, that's the important bit, actually CCBUG:251131
    svn path=/trunk/KDE/kdebase/workspace/; revision=1175732

--- plasma/desktop/applets/pager/pager.cpp
+++ plasma/desktop/applets/pager/pager.cpp
@@ -668,8 +668,6 @@ void Pager::windowRemoved(WId id)
 {
     Q_UNUSED(id)
 
-    KWindowInfo info = KWindowSystem::windowInfo(id, NET::WMGeometry | NET::WMFrameExtents | NET::WMWindowType | NET::WMDesktop | NET::WMState | NET::XAWMState);
-
     if (!m_timer->isActive()) {
         m_timer->start(FAST_UPDATE_DELAY);
     }
@@ -678,8 +676,6 @@ void Pager::windowRemoved(WId id)
 void Pager::activeWindowChanged(WId id)
 {
     Q_UNUSED(id)
-
-    KWindowInfo info = KWindowSystem::windowInfo(id, NET::WMGeometry | NET::WMFrameExtents | NET::WMWindowType | NET::WMDesktop | NET::WMState | NET::XAWMState);
 
     if (!m_timer->isActive()) {
         m_timer->start(FAST_UPDATE_DELAY);
