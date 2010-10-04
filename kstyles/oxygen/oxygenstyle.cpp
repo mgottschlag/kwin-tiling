4e5f0ce414cb9bd349615d80758ed2af54ccaf55
Jonathan Riddell <jr@jriddell.org>
2010-10-04 12:00:48 +0000

    https://bugs.launchpad.net/ubuntu/+source/kdebase-workspace/+bug/602389 fixes regression, dolphin wasn't able to show free space in places for removable devices. patch by Alessandro Ghersi <alessandro-ghersi@kubuntu.org>
    svn path=/trunk/KDE/kdebase/workspace/; revision=1182390

--- kstyles/oxygen/oxygenstyle.cpp
+++ kstyles/oxygen/oxygenstyle.cpp
@@ -4146,6 +4146,9 @@ namespace Oxygen
 
         // draw bar
         sub_opt.rect = subElementRect( QStyle::SE_ProgressBarContents, cbOption, widget );
+        if (sub_opt.rect.height() < 3) {
+            sub_opt.rect.adjust(0, 0, 0, 2);
+        }
         drawProgressBarContentsControl( &sub_opt, painter, widget );
 
         // draw label
