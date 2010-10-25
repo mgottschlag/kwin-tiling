7852f8b501531791451a8b13ec5662037960f18d
Dario Freddi <drf@kde.org>
2010-10-25 19:04:21 +0000

    CCMAIL: kde-core-devel@kde.org
    Removing PolicyKit-kde from kdebase, as anticipated quite a long time ago on k-c-d. The destiny of PolicyKit-kde will end up, together with polkit-qt, in
    Gitorious here: http://gitorious.org/policykit-kde . I'm not willing to keep the code in KDE's repository anymore for the sole reason that those two components
    should be treated as deprecated - they won't see any feature and hardly any bug fixing, since now it's time for polkit-1. A last release will be made after the
    move for interested parties. If you don't agree with this, start a discussion.
    
    So long PolicyKit-kde, you've been a good friend after all.
    
    svn path=/trunk/KDE/kdebase/workspace/; revision=1189750

--- PolicyKit-kde/authorization/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-$EXTRACTRC `find -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/polkit-kde-authorization.pot
-rm -f rc.cpp
