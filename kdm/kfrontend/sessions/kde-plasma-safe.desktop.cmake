7f788353bdd871e1e9721285398ea386fb02973b
Jonathan Riddell <jr@jriddell.org>
2010-10-28 15:57:43 +0000

    Update for new branding.  Plasma is now a brand for the whole workspace.  KDE is the community which makes it.
    svn path=/trunk/KDE/kdebase/workspace/; revision=1190697

--- kdm/kfrontend/sessions/kde-plasma-safe.desktop.cmake
+++ kdm/kfrontend/sessions/kde-plasma-safe.desktop.cmake
@@ -3,5 +3,5 @@ Encoding=UTF-8
 Type=XSession
 Exec=${KDE4_BIN_INSTALL_DIR}/startkde --failsafe
 TryExec=${KDE4_BIN_INSTALL_DIR}/startkde
-Name=KDE (failsafe session)
-Comment=The K Desktop Environment. A powerful Open Source graphical desktop environment (failsafe session)
+Name=KDE Plasma Workspace (failsafe session)
+Comment=The desktop made by KDE (failsafe session)
