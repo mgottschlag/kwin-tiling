afa21c8edd2e124a8a5bd6422a4606026f9ab20a
Pino Toscano <pino@kde.org>
2010-11-01 15:19:34 +0000

    do not extract messages from sources in 'tests' subdirectories
    svn path=/trunk/KDE/kdebase/workspace/; revision=1191967

--- libs/plasmagenericshell/Messages.sh
+++ libs/plasmagenericshell/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/plasmagenericshell.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/plasmagenericshell.pot
 rm -f rc.cpp
