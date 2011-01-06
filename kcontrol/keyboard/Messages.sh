7dabbead4dc34ee412ae6b36bd11d55e65e68ebf
Burkhard Lück <lueck@hube-lueck.de>
2011-01-06 10:54:17 +0000

    catch all kcm ui files for message extraction
    svn path=/trunk/KDE/kdebase/workspace/; revision=1212259

--- kcontrol/keyboard/Messages.sh
+++ kcontrol/keyboard/Messages.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
-$EXTRACTRC kcmmiscwidget.ui >> rc.cpp
+$EXTRACTRC kcm*.ui >> rc.cpp
 $XGETTEXT rc.cpp kcmmisc.cpp -o $podir/kcmkeyboard.pot
-rm -f rc.cpp
-$EXTRACTRC kcm_*.ui >> rc.cpp
 $XGETTEXT kcm_*.cpp keyboard_*.cpp layout_widget.cpp flags.cpp -o $podir/kxkb.pot
 rm -f rc.cpp
