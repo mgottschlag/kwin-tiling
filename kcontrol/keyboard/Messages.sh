89b946b4c03750536de539af1aa218413e4388ca
Lukáš Tinkl <lukas@kde.org>
2011-01-28 16:33:16 +0000

    fix message extraction
    svn path=/branches/KDE/4.6/kdebase/workspace/; revision=1217784

--- kcontrol/keyboard/Messages.sh
+++ kcontrol/keyboard/Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC kcm*.ui >> rc.cpp
 $XGETTEXT rc.cpp kcmmisc.cpp -o $podir/kcmkeyboard.pot
-$XGETTEXT kcm_*.cpp keyboard_*.cpp layout_widget.cpp flags.cpp -o $podir/kxkb.pot
+$XGETTEXT kcm_*.cpp keyboard_*.cpp layout_*.cpp flags.cpp layouts_menu.cpp -o $podir/kxkb.pot
 rm -f rc.cpp
