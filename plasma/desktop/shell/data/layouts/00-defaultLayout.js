70e746d3fae467c52ab91cb7e8b5b10f2519248b
Kevin Ottens <ervin@kde.org>
2011-04-27 00:52:27 +0200

    No more default widget in the default containment

--- plasma/desktop/shell/data/layouts/00-defaultLayout.js
+++ plasma/desktop/shell/data/layouts/00-defaultLayout.js
@@ -1,29 +1,3 @@
-function widgetExists(name)
-{
-    var widgets = knownWidgetTypes;
-    for (i in widgets) {
-        if (widgets[i] == name) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-var topLeftScreen = 0
-var rect = screenGeometry(0)
-
-// find our "top left" screen to put the folderview on it
-for (var i = 1; i < screenCount; ++i) {
-    var g = screenGeometry(i)
-
-    if (g.x <= rect.x && g.top >= rect.top) {
-        rect = g
-        topLeftScreen = i
-    }
-}
-
-var hasFolderview = widgetExists("folderview");
 
 for (var i = 0; i < screenCount; ++i) {
     var desktop = new Activity
@@ -31,11 +5,6 @@ for (var i = 0; i < screenCount; ++i) {
     desktop.screen = i
     desktop.wallpaperPlugin = 'image'
     desktop.wallpaperMode = 'SingleImage'
-
-    if (hasFolderview && i == topLeftScreen) {
-        var folderview = desktop.addWidget("folderview")
-        folderview.writeConfig("url", "desktop:/")
-    }
 }
 
 loadTemplate("org.kde.plasma-desktop.defaultPanel")
