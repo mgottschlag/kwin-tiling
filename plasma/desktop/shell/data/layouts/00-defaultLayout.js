5b05f01dfdf37135cabd83a67c2d32c110e0b52d
Aaron Seigo <aseigo@kde.org>
2011-04-26 12:12:25 +0200

    i suppose we don't need debug output here ;)

--- plasma/desktop/shell/data/layouts/00-defaultLayout.js
+++ plasma/desktop/shell/data/layouts/00-defaultLayout.js
@@ -24,7 +24,6 @@ for (var i = 1; i < screenCount; ++i) {
 }
 
 var hasFolderview = widgetExists("folderview");
-print("HAS FOLDERVIEW?!!!!!!!!!!!!!! " + hasFolderview);
 
 for (var i = 0; i < screenCount; ++i) {
     var desktop = new Activity
