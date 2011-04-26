4bd206a8b5c9f64b6bbf26a4b753c4170e69cf38
Kevin Ottens <ervin@kde.org>
2011-04-27 00:25:28 +0200

    Provide a proper name to the default activity

--- plasma/desktop/shell/data/layouts/00-defaultLayout.js
+++ plasma/desktop/shell/data/layouts/00-defaultLayout.js
@@ -27,6 +27,7 @@ var hasFolderview = widgetExists("folderview");
 
 for (var i = 0; i < screenCount; ++i) {
     var desktop = new Activity
+    desktop.name = i18n("Desktop")
     desktop.screen = i
     desktop.wallpaperPlugin = 'image'
     desktop.wallpaperMode = 'SingleImage'
