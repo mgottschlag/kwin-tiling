95825fb034b70f329d1f3e8b821c966c77e41d99
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-05-02 23:35:09 +0200

    moved drawOuterGlow to public, cause its needed by Oxygen::Style for scrollbar handles.

--- libs/oxygen/oxygenhelper.h
+++ libs/oxygen/oxygenhelper.h
@@ -335,6 +335,9 @@ namespace Oxygen
 
         //@}
 
+        //! generic outer glow (to be stored in tilesets)
+        virtual void drawOuterGlow( QPainter&, const QColor&, int size );
+
         protected:
 
         //! generic slab painting (to be stored in tilesets)
@@ -342,9 +345,6 @@ namespace Oxygen
 
         //! generic outer shadow (to be stored in tilesets)
         virtual void drawShadow( QPainter&, const QColor&, int size );
-
-        //! generic outer glow (to be stored in tilesets)
-        virtual void drawOuterGlow( QPainter&, const QColor&, int size );
 
         //! return background adjusted color matching relative vertical position in window
         const QColor& backgroundColor( const QColor&, qreal ratio );
