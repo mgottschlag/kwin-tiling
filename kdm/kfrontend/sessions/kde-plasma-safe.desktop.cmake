d6a44afd961733b66332bfa4057b855c5c71873e
Script Kiddy <scripty@kde.org>
2011-07-11 10:56:30 +0200

    SVN_SILENT made messages (.desktop file)

--- kdm/kfrontend/sessions/kde-plasma-safe.desktop.cmake
+++ kdm/kfrontend/sessions/kde-plasma-safe.desktop.cmake
@@ -4,4 +4,6 @@ Type=XSession
 Exec=${KDE4_BIN_INSTALL_DIR}/startkde --failsafe
 TryExec=${KDE4_BIN_INSTALL_DIR}/startkde
 Name=KDE Plasma Workspace (failsafe session)
+Name[he]=סביבת עבודה של KDE Plasma (הפעלה בטוחה)
 Comment=The desktop made by KDE (failsafe session)
+Comment[he]=שולחן העבודה של KDE (הפעלה בטוחה)
