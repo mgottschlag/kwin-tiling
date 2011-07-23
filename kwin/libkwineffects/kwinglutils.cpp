mgottschlag/kwin-tiling

commit 909b4efa802aaa29738eca5833d6556f8c0ffd50

Add a define KWIN_SHADER_DEBUG to all shaders
If the environment variable KWIN_GL_DEBUG is set to 1
the define KWIN_SHADER_DEBUG is added to the glsl
source code allowing to add some custom ifdefed
visual debug handling.

As an example it's added to scene-fragment.glsl to
paint everything in a greenish way.

kwin/libkwineffects/kwinglutils.cpp
@@ -295,6 +295,9 @@ bool GLShader::compile(GLuint program, GLenum shaderType, const QByteArray &sour
 #ifdef KWIN_HAVE_OPENGLES
     ba.append("#ifdef GL_ES\nprecision highp float;\n#endif\n");
 #endif
+    if (ShaderManager::instance()->isShaderDebug()) {
+        ba.append("#define KWIN_SHADER_DEBUG 1\n");
+    }
     ba.append(source);
 
     const char* src = ba.constData();
@@ -615,6 +618,7 @@ ShaderManager::ShaderManager()
     , m_inited(false)
     , m_valid(false)
 {
+    m_debug = qstrcmp(qgetenv("KWIN_GL_DEBUG"), "1") == 0;
     initShaders();
     m_inited = true;
 }
@@ -646,6 +650,11 @@ bool ShaderManager::isShaderBound() const
 bool ShaderManager::isValid() const
 {
     return m_valid;
+}
+
+bool ShaderManager::isShaderDebug() const
+{
+    return m_debug;
 }
 
 GLShader *ShaderManager::pushShader(ShaderType type, bool reset)
@@ -840,7 +849,6 @@ void ShaderManager::resetShader(ShaderType type)
         break;
     }
 
-    //shader->setUniform("debug", 0);
     shader->setUniform("sampler", 0);
 
     shader->setUniform(GLShader::ProjectionMatrix,     projection);

kwin/libkwineffects/kwinglutils.h
@@ -289,6 +289,13 @@ public:
      * @return @c true if the built-in shaders are valid, @c false otherwise
      **/
     bool isValid() const;
+    /**
+     * Is @c true if the environment variable KWIN_GL_DEBUG is set to 1.
+     * In that case shaders are compiled with KWIN_SHADER_DEBUG defined.
+     * @returns @c true if shaders are compiled with debug information
+     * @since 4.8
+     **/
+    bool isShaderDebug() const;
 
     /**
      * Binds the shader of specified @p type.
@@ -363,6 +370,7 @@ private:
     GLShader *m_colorShader;
     bool m_inited;
     bool m_valid;
+    bool m_debug;
     static ShaderManager *s_shaderManager;
 };
 

kwin/scene-fragment.glsl
@@ -1,7 +1,6 @@
 uniform sampler2D sampler;
 uniform vec4 modulation;
 uniform float saturation;
-uniform int debug;
 uniform int u_forceAlpha;
 
 varying vec2 varyingTexCoords;
@@ -23,9 +22,9 @@ void main() {
 
     tex *= modulation;
 
-    /*if (debug != 0) {
-        tex.g += 0.5;
-    }*/
+#ifdef KWIN_SHADER_DEBUG
+    tex.g = min(tex.g + 0.5, 1.0);
+#endif
 
     gl_FragColor = tex;
 }

commit 52b2e677d8bdf56a5df25cedddd498c830b10c4e

Fix regression introduced with 909b4efa
I still don't understand how I actually tested my patch
before committing. It was working, sigh. That were
several hours of annoyance including a git bisect presenting
my own commit I assumed to be correct. Sigh.

kwin/libkwineffects/kwinglutils.cpp
@@ -601,6 +601,8 @@ ShaderManager *ShaderManager::instance()
 {
     if (!s_shaderManager) {
         s_shaderManager = new ShaderManager();
+        s_shaderManager->initShaders();
+        s_shaderManager->m_inited = true;
     }
     return s_shaderManager;
 }
@@ -619,8 +621,6 @@ ShaderManager::ShaderManager()
     , m_valid(false)
 {
     m_debug = qstrcmp(qgetenv("KWIN_GL_DEBUG"), "1") == 0;
-    initShaders();
-    m_inited = true;
 }
 
 ShaderManager::~ShaderManager()