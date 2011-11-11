dd2ef261589ba1df794343ba99e35e88855b5b50
Andriy Rysin <arysin@gmail.com>
2011-11-10 23:32:06 -0500

    unit test fixes

--- kcontrol/keyboard/tests/flags_test.cpp
+++ kcontrol/keyboard/tests/flags_test.cpp
@@ -78,9 +78,9 @@ private Q_SLOTS:
         QCOMPARE( flags->getShortText(layoutUnit2, keyboardConfig), QString("us") );
 
         const Rules* rules = Rules::readRules(Rules::NO_EXTRAS);
-        QCOMPARE( flags->getLongText(layoutUnit, rules), QString("USA") );
-        QVERIFY( flags->getLongText(layoutUnit1, rules).startsWith("USA - International") );
-        QCOMPARE( flags->getLongText(layoutUnit2, rules), QString("USA - other") );
+        QCOMPARE( flags->getLongText(layoutUnit, rules), QString("English (US)") );
+        QVERIFY( flags->getLongText(layoutUnit1, rules).startsWith("English (US, international with dead keys)") );
+        QCOMPARE( flags->getLongText(layoutUnit2, rules), QString("other") );
 
         rules = NULL; // when no rules found
         QCOMPARE( flags->getLongText(layoutUnit1, rules), QString("us - intl") );
