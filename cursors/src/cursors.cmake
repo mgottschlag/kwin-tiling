53ee0ea0397e6f08b0a9ab2580a434e46b408c22
Lukas Sommer <sommerluk@gmail.com>
2011-11-11 16:25:49 +0100

    Make Oxygen mouse cursors (pseudo) scalable
    This patch makes the mouse cursor themes that KDE provides pseudo
    scalable. This means that it removes the "big" themes. Instead of this,
    it provides only 1 theme per color. This theme is a multi-size theme.
    It contains coursers in 24 (old "normal" size), 48 (old "big" size)
    and 72 (new size).
    
    At the same time, it substitutes the duplicate curser images (there
    were many of them) by symbolic links to save memory. This way, the
    file size of the themes is (even with adding the "72" size) decreased.
    
    The patch modifies kde-workspace/cursors/src which provides the sources
    that produce the binary image files.
    
    The patch also modifies the (binary) themes in kde-workspace/cursors
    directly.
    
    I hope that GIT manages well all the binary stuff and the symbolic
    links ...
    BUG: 273936
    FIXED-IN: 4.8
    REVIEW: 102524

--- cursors/src/cursors.cmake
+++ cursors/src/cursors.cmake
@@ -1,4 +1,8 @@
+# This macro produces a "themed" version of the SVG, using the spcified colors.
+# Than, it renders this themed SVG to PNG files.
+# You can specify one or more dpi values when you call this macro.
 macro(add_cursor cursor color theme dpi)
+    # Produce a "themed" SVG
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
                        DEPENDS ${MAKE_SVG} ${CMAKE_CURRENT_SOURCE_DIR}/colors.in ${SVGDIR}/${cursor}.svg
                        COMMAND ${CMAKE_COMMAND} -Dconfig=${CMAKE_CURRENT_SOURCE_DIR}/colors.in
@@ -6,26 +10,45 @@ macro(add_cursor cursor color theme dpi)
                                                 -Doutput=${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
                                                 -P ${MAKE_SVG}
                       )
-    add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${cursor}.png
-                       DEPENDS ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
-                       COMMAND ${INKSCAPE} --without-gui --export-dpi=${dpi}
-                                           --export-png=${CMAKE_BINARY_DIR}/oxy-${theme}/png/${cursor}.png
-                                           ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
-                      )
+    # Prepare a list of resolutions in dpi
+    set(resolutions ${ARGV})
+    list(REMOVE_AT resolutions 0)
+    list(REMOVE_AT resolutions 0)
+    list(REMOVE_AT resolutions 0)
+    # Render the SVG
+    foreach(resolution ${resolutions})
+        add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${resolution}/${cursor}.png
+                           DEPENDS ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
+                           COMMAND ${INKSCAPE} --without-gui --export-dpi=${resolution}
+                                               --export-png=${CMAKE_BINARY_DIR}/oxy-${theme}/png/${resolution}/${cursor}.png
+                                               ${CMAKE_BINARY_DIR}/oxy-${theme}/svg/${cursor}.svg
+                          )
+    endforeach(resolution)
 endmacro(add_cursor)
 
 macro(add_x_cursor theme cursor dpi)
+    # Prepare a list of resolutions in dpi
+    set(resolutions ${ARGV})
+    list(REMOVE_AT resolutions 0)
+    list(REMOVE_AT resolutions 0)
+    # Prepare a list of the png files that are necessary
     set(inputs)
     foreach(png ${${cursor}_inputs})
-        list(APPEND inputs ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${png})
+        foreach(resolution ${resolutions})
+            list(APPEND inputs ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${resolution}/${png})
+        endforeach(resolution)
     endforeach(png)
+    # Make a coma-separated list (normal lists can't be passed as argument)
+    string(REPLACE ";" "," resolutions_coma_separated "${resolutions}")
+    # Adopt the x cursor config file to the requested resolutions
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
                        DEPENDS ${MAKE_CONFIG} ${CONFIGDIR}/${cursor}.in
                        COMMAND ${CMAKE_COMMAND} -Dconfig=${CONFIGDIR}/${cursor}.in
                                                 -Doutput=${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
-                                                -Ddpi=${dpi}
+                                                -Dresolutions=${resolutions_coma_separated}
                                                 -P ${MAKE_CONFIG}
                       )
+    # Use the adopted x cursor config file and the png files to produce the cursor file
     if(NOT WIN32)
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors/${cursor}
                        DEPENDS ${inputs} ${CMAKE_BINARY_DIR}/oxy-${theme}/config/${cursor}.in
@@ -45,8 +68,17 @@ macro(add_x_cursor theme cursor dpi)
 endmacro(add_x_cursor)
 
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/packages)
+
+# Macro that adds a theme. You can specify more than one dpi value (overloaded macro).
 macro(add_theme color theme dpi)
-    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/png)
+    # Prepare a list of resolutions in dpi
+    set(resolutions ${ARGV})
+    list(REMOVE_AT resolutions 0)
+    list(REMOVE_AT resolutions 0)
+    # Make missing directories
+    foreach (resolution ${resolutions})
+        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/png/${resolution})
+    endforeach(resolution)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/svg)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/config)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors)
@@ -54,19 +86,26 @@ macro(add_theme color theme dpi)
     file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/wincursors/oxy-${theme})
     endif(WIN32)
     set(${theme}_cursors)
+    # render SVG to PNG files
     foreach(svg ${SVGS})
-        string(REGEX REPLACE ".*/" "" cursor ${svg})
-        string(REGEX REPLACE "[.]svg" "" cursor ${cursor})
-        add_cursor(${cursor} ${color} ${theme} ${dpi})
+        string(REGEX REPLACE ".*/" "" cursor ${svg})           # use relative paths
+        string(REGEX REPLACE "[.]svg" "" cursor ${cursor})     # remove ".svg" from the path
+        add_cursor(${cursor} ${color} ${theme} ${resolutions}) # Render a "themed" version of the SVG to PNG files.
     endforeach(svg)
+    # produce cursor files from the png files
     foreach(cursor ${CURSORS})
-        add_x_cursor(${theme} ${cursor} ${dpi})
+        add_x_cursor(${theme} ${cursor} ${resolutions})
         list(APPEND ${theme}_cursors ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors/${cursor})
     endforeach(cursor)
+    # add the symbolic links
+    foreach(symlink ${SYMLINKS_PATHS})
+        file(COPY ${symlink} DESTINATION ${CMAKE_BINARY_DIR}/oxy-${theme}/cursors)
+    endforeach(symlink)
+    # packaging
     add_custom_target(theme-${theme} ALL DEPENDS ${${theme}_cursors})
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/oxy-${theme}/index.theme
                        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/index.theme
-                       COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                       COMMAND ${CMAKE_COMMAND} -E copy
                                                    ${CMAKE_CURRENT_SOURCE_DIR}/index.theme
                                                    ${CMAKE_BINARY_DIR}/oxy-${theme}/index.theme
                       )
@@ -77,5 +116,5 @@ macro(add_theme color theme dpi)
                                       oxy-${theme}/index.theme
                        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                       )
-    add_custom_target(package-${theme} DEPENDS ${CMAKE_BINARY_DIR}/packages/oxy-${theme}.tar.bz2)
+    add_custom_target(package-${theme} ALL DEPENDS ${CMAKE_BINARY_DIR}/packages/oxy-${theme}.tar.bz2)
 endmacro(add_theme)
