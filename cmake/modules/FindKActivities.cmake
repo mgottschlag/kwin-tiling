6171d883eb8b486e854392f8c636161098d378e6
Ivan Čukić <ivan.cukic@kde.org>
2011-12-07 23:36:31 +0100

    Fixed licensing issues

--- cmake/modules/FindKActivities.cmake
+++ cmake/modules/FindKActivities.cmake
@@ -1,24 +1,9 @@
-#   Copyright (C) 2011 Ivan Cukic <ivan.cukic(at)kde.org>
-#
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License version 2,
-#   or (at your option) any later version, as published by the Free
-#   Software Foundation
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details
-#
-#   You should have received a copy of the GNU General Public
-#   License along with this program; if not, write to the
-#   Free Software Foundation, Inc.,
-#   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 # cmake macro to see if we have libKActivities
-
+#
 # KACTIVITIES_INCLUDE_DIRS
 # KACTIVITIES_LIBRARY
+#
+# Copyright (C) 2011 Ivan Cukic <ivan.cukic(at)kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
