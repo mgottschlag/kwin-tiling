1cd35c19378beef1e88fe83244fa6f64d72448d3
Aaron Seigo <aseigo@kde.org>
2011-12-07 15:17:20 +0100

    slide from the correct direction

--- plasma/desktop/shell/plasmaapp.cpp
+++ plasma/desktop/shell/plasmaapp.cpp
@@ -552,7 +552,7 @@ ControllerWindow *PlasmaApp::showController(int screen, Plasma::Containment *con
     }
 
     controller->show();
-    Plasma::WindowEffects::slideWindow(controller, Plasma::BottomEdge);
+    Plasma::WindowEffects::slideWindow(controller, controller->location());
     KWindowSystem::setOnAllDesktops(controller->winId(), true);
     QTimer::singleShot(0, controller, SLOT(activate()));
     KWindowSystem::setState(controller->winId(), NET::SkipTaskbar | NET::SkipPager | NET::Sticky | NET::KeepAbove);
