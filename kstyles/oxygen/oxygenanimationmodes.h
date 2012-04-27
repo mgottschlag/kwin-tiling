0e0d82526c3e07515993655072af5b5c66e1f511
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2012-04-27 17:17:09 +0200

    fixed licenses.

--- kstyles/oxygen/oxygenanimationmodes.h
+++ kstyles/oxygen/oxygenanimationmodes.h
@@ -1,31 +1,31 @@
 #ifndef oxygenanimationmodes_h
 #define oxygenanimationmodes_h
 
-//////////////////////////////////////////////////////////////////////////////
-// oxygenanimationmodes.h
-// animation modes
-// -------------------
-//
-// Copyright (c) 2009 Hugo Pereira Da Costa <hugo.pereira@free.fr>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-//////////////////////////////////////////////////////////////////////////////
+/*
+* oxygenanimationmodes.h
+* animation modes
+* -------------------
+*
+* Copyright (c) 2012 Hugo Pereira Da Costa <hugo@oxygen-icons.org>
+*
+* Largely inspired from Qtcurve style
+* Copyright (C) Craig Drummond, 2003 - 2010 craig.p.drummond@gmail.com
+*
+* This  library is free  software; you can  redistribute it and/or
+* modify it  under  the terms  of the  GNU Lesser  General  Public
+* License  as published  by the Free  Software  Foundation; either
+* version 2 of the License, or( at your option ) any later version.
+*
+* This library is distributed  in the hope that it will be useful,
+* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License  along  with  this library;  if not,  write to  the Free
+* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
 
 #include "QtCore/QFlags"
 
