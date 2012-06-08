5e5de9b5fdc4ad003f666d5d76ab66596c08fde4
Anne-Marie Mahfouf <annma@kde.org>
2012-06-08 21:20:24 +0200

    Extract qml files strings

--- libs/plasmagenericshell/Messages.sh
+++ libs/plasmagenericshell/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/plasmagenericshell.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml | grep -v '/tests/'` -o $podir/plasmagenericshell.pot
 rm -f rc.cpp
