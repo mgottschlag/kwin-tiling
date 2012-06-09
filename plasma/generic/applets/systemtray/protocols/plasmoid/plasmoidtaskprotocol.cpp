mgottschlag/kwin-tiling

commit 6c7cf24592ed9185a59ebe527db633de7e866878

index the plasmoid tasks by host. this makes possible to have two systrays, could also help with CCBUG: 235066
svn path=/trunk/KDE/kdebase/workspace/; revision=1117944

plasma/generic/applets/systemtray/protocols/plasmoid/plasmoidtaskprotocol.cpp
@@ -48,33 +48,39 @@ void PlasmoidProtocol::init()
 
 void PlasmoidProtocol::forwardConstraintsEvent(Plasma::Constraints constraints)
 {
-    foreach (PlasmoidTask *task, m_tasks) {
-        task->forwardConstraintsEvent(constraints);
+    typedef QHash<QString, PlasmoidTask*> TfoType;
+    foreach (TfoType tasksForHost, m_tasks) {
+        foreach (PlasmoidTask *task, tasksForHost) {
+            task->forwardConstraintsEvent(constraints);
+        }
     }
 }
 
 void PlasmoidProtocol::loadFromConfig(Plasma::Applet *parent)
 {
     KConfigGroup cg = parent->config();
-    QHash<QString, PlasmoidTask*> existingTasks = m_tasks;
-    m_tasks.clear();
+    QHash<QString, PlasmoidTask*> existingTasks = m_tasks.value(parent);
+
+    if (m_tasks.contains(parent)) {
+        m_tasks[parent].clear();
+    }
 
     KConfigGroup appletGroup(&cg, "Applets");
     foreach (const QString &groupName, appletGroup.groupList()) {
         KConfigGroup childGroup(&appletGroup, groupName);
         QString appletName = childGroup.readEntry("plugin", QString());
 
-        if (m_tasks.contains(appletName)) {
+        if (m_tasks.contains(parent) && m_tasks.value(parent).contains(appletName)) {
             continue;
         }
 
         if (existingTasks.contains(appletName)) {
-            m_tasks.insert(appletName, existingTasks.value(appletName));
+            m_tasks[parent].insert(appletName, existingTasks.value(appletName));
             existingTasks.remove(appletName);
             continue;
         }
 
-        addApplet(appletName, parent);
+        addApplet(appletName, groupName.toInt(), parent);
 
         existingTasks.remove(appletName);
     }
@@ -89,11 +95,11 @@ void PlasmoidProtocol::loadFromConfig(Plasma::Applet *parent)
     }
 }
 
-void PlasmoidProtocol::addApplet(const QString appletName, Plasma::Applet *parent)
+void PlasmoidProtocol::addApplet(const QString appletName, const int id, Plasma::Applet *parent)
 {
     kDebug() << "Registering task with the manager" << appletName;
 
-    PlasmoidTask *task = new PlasmoidTask(appletName, 0, this, parent);
+    PlasmoidTask *task = new PlasmoidTask(appletName, id, this, parent);
 
     if (!task->isValid()) {
         // we failed to load our applet *sob*
@@ -101,36 +107,49 @@ void PlasmoidProtocol::addApplet(const QString appletName, Plasma::Applet *paren
         return;
     }
 
-    m_tasks.insert(appletName, task);
-    connect(task, SIGNAL(taskDeleted(QString)), this, SLOT(cleanupTask(QString)));
+
+    m_tasks[parent].insert(appletName, task);
+
+    connect(task, SIGNAL(taskDeleted(Plasma::Applet *, const QString &)), this, SLOT(cleanupTask(plasma::Applet *, const QString &)));
     emit taskCreated(task);
 }
 
 void PlasmoidProtocol::removeApplet(const QString appletName, Plasma::Applet *parent)
 {
-    Plasma::Applet *applet = qobject_cast<Plasma::Applet *>(m_tasks[appletName]->widget(parent, true));
+    if (!m_tasks.contains(parent) || !m_tasks.value(parent).contains(appletName)) {
+        return;
+    }
+
+    Plasma::Applet *applet = qobject_cast<Plasma::Applet *>(m_tasks.value(parent).value(appletName)->widget(parent, true));
 
     if (applet) {
         applet->destroy();
     }
 }
 
-void PlasmoidProtocol::cleanupTask(QString typeId)
+void PlasmoidProtocol::cleanupTask(Plasma::Applet *host, const QString &typeId)
 {
     kDebug() << "task with typeId" << typeId << "removed";
-    m_tasks.remove(typeId);
+    if (m_tasks.contains(host)) {
+        m_tasks[host].remove(typeId);
+        if (m_tasks.value(host).isEmpty()) {
+            m_tasks.remove(host);
+        }
+    }
 }
 
-QStringList PlasmoidProtocol::applets(const Plasma::Applet *host) const
+QStringList PlasmoidProtocol::applets(Plasma::Applet *host) const
 {
     QStringList list;
-    QHashIterator<QString, PlasmoidTask *> i(m_tasks);
+    if (!m_tasks.contains(host)) {
+        return list;
+    }
+
+    QHashIterator<QString, PlasmoidTask *> i(m_tasks.value(host));
 
     while (i.hasNext()) {
         i.next();
-        if (i.value()->host() == host) {
-            list << i.key();
-        }
+        list << i.key();
     }
 
     return list;

plasma/generic/applets/systemtray/core/manager.cpp
@@ -106,7 +106,7 @@ void Manager::loadApplets(Plasma::Applet *parent)
 
 void Manager::addApplet(const QString appletName, Plasma::Applet *parent)
 {
-    d->plasmoidProtocol->addApplet(appletName, parent);
+    d->plasmoidProtocol->addApplet(appletName, 0, parent);
 }
 
 void Manager::removeApplet(const QString appletName, Plasma::Applet *parent)

plasma/generic/applets/systemtray/protocols/plasmoid/plasmoidtask.cpp
@@ -49,7 +49,7 @@ PlasmoidTask::PlasmoidTask(const QString &appletname, int id, QObject *parent, P
 
 PlasmoidTask::~PlasmoidTask()
 {
-    emit taskDeleted(m_typeId);
+    emit taskDeleted(m_host, m_typeId);
 }
 
 

plasma/generic/applets/systemtray/protocols/plasmoid/plasmoidtask.h
@@ -53,7 +53,7 @@ protected Q_SLOTS:
     void newAppletStatus(Plasma::ItemStatus status);
 
 Q_SIGNALS:
-    void taskDeleted(QString typeId);
+    void taskDeleted(Plasma::Applet *host, const QString &typeId);
 
 protected:
     virtual QGraphicsWidget* createWidget(Plasma::Applet *applet);

plasma/generic/applets/systemtray/protocols/plasmoid/plasmoidtaskprotocol.h
@@ -48,16 +48,16 @@ public:
 
     void forwardConstraintsEvent(Plasma::Constraints constraints);
     void loadFromConfig(Plasma::Applet *parent);
-    void addApplet(const QString appletName, Plasma::Applet *parent);
+    void addApplet(const QString appletName, const int id, Plasma::Applet *parent);
     void removeApplet(const QString appletName, Plasma::Applet *parent);
-    QStringList applets(const Plasma::Applet *parent) const;
+    QStringList applets(Plasma::Applet *parent) const;
 
 private slots:
-    void cleanupTask(QString typeId);
+    void cleanupTask(Plasma::Applet *host, const QString &typeId);
 
 private:
     //FIXME: applets must be indicized by name -and- parent
-    QHash<QString, PlasmoidTask*> m_tasks;
+    QHash<Plasma::Applet *, QHash<QString, PlasmoidTask*> > m_tasks;
 };
 
 }

plasma/generic/applets/systemtray/ui/applet.cpp
@@ -594,6 +594,7 @@ void Applet::createConfigurationInterface(KConfigDialog *parent)
     m_visibleItemsSourceModel.data()->appendRow(unknownItem);
 
     QStringList ownApplets = s_manager->applets(this);
+
     foreach (const KPluginInfo &info, Plasma::Applet::listAppletInfo()) {
         KService::Ptr service = info.service();
         if (service->property("X-Plasma-NotificationArea", QVariant::Bool).toBool()) {

commit 6fea7b3cd6bd77195ddd01cc6604bc2eee29d25b

do not clear the applet list since the code relies on that to remove / add entries
fixes a regression introduced in 6c7cf245

plasma/generic/applets/systemtray/protocols/plasmoid/plasmoidtaskprotocol.cpp
@@ -60,10 +60,6 @@ void PlasmoidProtocol::loadFromConfig(Plasma::Applet *parent)
 {
     QHash<QString, PlasmoidTask*> existingTasks = m_tasks.value(parent);
 
-    if (m_tasks.contains(parent)) {
-        m_tasks[parent].clear();
-    }
-
     KConfigGroup appletGroup = parent->config();
     appletGroup = KConfigGroup(&appletGroup, "Applets");
     foreach (const QString &groupName, appletGroup.groupList()) {